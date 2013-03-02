768756396357cf47c9e5fa86a33fe9fd18b19f0e
Arthur Benemann <arthur.benemann@gmail.com>
2013-03-02 18:17:03 -0300

    Enable or disables the tts module in respect to the preference setting.

--- DroidPlanner/src/com/diydrones/droidplanner/helpers/TTS.java
+++ DroidPlanner/src/com/diydrones/droidplanner/helpers/TTS.java
@@ -3,23 +3,33 @@ package com.diydrones.droidplanner.helpers;
 import java.util.Locale;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 
-public class TTS {
+public class TTS implements OnInitListener {
 	TextToSpeech tts;
 
 	public TTS(Context context) {
-		tts = new TextToSpeech(context, new OnInitListener() {
+		if(shouldEnableTTS(context)) {
+			tts = new TextToSpeech(context, this);
+		}
+	}
 
-			@Override
-			public void onInit(int status) {
-				tts.setLanguage(Locale.US);
-			}
-		});
+	@Override
+	public void onInit(int status) {
+		tts.setLanguage(Locale.US);
 	}
 
 	public void speak(String string) {
-		tts.speak(string, TextToSpeech.QUEUE_FLUSH, null);
+		if (tts != null)
+			tts.speak(string, TextToSpeech.QUEUE_FLUSH, null);
+	}
+
+	private boolean shouldEnableTTS(Context context) {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		return prefs.getBoolean("pref_enable_tts", false);
 	}
 }
