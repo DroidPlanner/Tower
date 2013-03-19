e8ba9aee97cbcbeea5cdf2f3074b3a9f87800fac
Arthur Benemann <arthur.benemann@gmail.com>
2013-03-19 18:15:35 -0300

    Adding a flight path indicator.
    Closes #16.

--- DroidPlanner/src/com/diydrones/droidplanner/fragments/FlightMapFragment.java
+++ DroidPlanner/src/com/diydrones/droidplanner/fragments/FlightMapFragment.java
@@ -1,7 +1,10 @@
 package com.diydrones.droidplanner.fragments;
 
+import java.util.List;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,11 +21,14 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class FlightMapFragment extends OfflineMapFragment {
 	private static final int DRONE_MIN_ROTATION = 5;
 	private GoogleMap mMap;
 	private Marker[] DroneMarker;
+	private Polyline flightPath;
 	private boolean hasBeenZoomed = false;
 	private int lastMarker = 0;
 
@@ -31,11 +37,27 @@ public class FlightMapFragment extends OfflineMapFragment {
 			Bundle bundle) {
 		View view = super.onCreateView(inflater, viewGroup, bundle);
 		mMap = getMap();		
-		addDroneMarkersToMap();
+		addDroneMarkersToMap();		
+		addFlightPathToMap();		
 		return view;
 	}
+	
+	public void receiveData(MAVLinkMessage msg) {
+		if(msg.msgid == msg_global_position_int.MAVLINK_MSG_ID_GLOBAL_POSITION_INT) {
+			LatLng position = new LatLng(((msg_global_position_int)msg).lat/1E7, ((msg_global_position_int)msg).lon/1E7);
+			float heading = (0x0000FFFF & ((int)((msg_global_position_int)msg).hdg))/100f; // TODO fix unsigned short read at mavlink library
+			updateDronePosition(heading, position);
+			addFlithPathPoint(position);
+		}
+	}
 
-	public void updateDronePosition(float heading, LatLng coord) {
+	private void addFlithPathPoint(LatLng position) {
+		List<LatLng> oldFlightPath = flightPath.getPoints();
+		oldFlightPath.add(position);
+		flightPath.setPoints(oldFlightPath);	
+	}
+
+	private void updateDronePosition(float heading, LatLng coord) {
 		int index = (int) (heading/DRONE_MIN_ROTATION);
 		
 		DroneMarker[lastMarker].setVisible(false);
@@ -47,6 +69,12 @@ public class FlightMapFragment extends OfflineMapFragment {
 			hasBeenZoomed = true;
 			mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 16));
 		}
+	}
+
+	private void addFlightPathToMap() {
+		PolylineOptions flightPathOptions = new PolylineOptions();
+		flightPathOptions.color(Color.argb(128, 0, 0, 255)).width(2);
+		flightPath = mMap.addPolyline(flightPathOptions);		
 	}
 
 	private void addDroneMarkersToMap() {
@@ -70,14 +98,6 @@ public class FlightMapFragment extends OfflineMapFragment {
 		matrix.postRotate(heading - mMap.getCameraPosition().bearing);
 		return BitmapDescriptorFactory.fromBitmap( Bitmap.createBitmap(planeBitmap, 0, 0, planeBitmap.getWidth(),
 				planeBitmap.getHeight(), matrix, true));
-	}
-
-	public void receiveData(MAVLinkMessage msg) {
-		if(msg.msgid == msg_global_position_int.MAVLINK_MSG_ID_GLOBAL_POSITION_INT) {
-			LatLng position = new LatLng(((msg_global_position_int)msg).lat/1E7, ((msg_global_position_int)msg).lon/1E7);
-			float heading = (0x0000FFFF & ((int)((msg_global_position_int)msg).hdg))/100f; // TODO fix unsigned short read at mavlink library
-			updateDronePosition(heading, position);
-		}
 	}
 
 }
