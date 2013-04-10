b269d5132212e9c3689baea6b716dbb76bc6d49a
Arthur Benemann <arthur.benemann@gmail.com>
2013-04-10 18:57:28 -0300

    Cleaning up the terminal screen.

--- DroidPlanner/src/com/droidplanner/TerminalActivity.java
+++ DroidPlanner/src/com/droidplanner/TerminalActivity.java
@@ -8,18 +8,9 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.ftdi.j2xx.D2xxManager;
-import com.ftdi.j2xx.FT_Device;
-
 public class TerminalActivity extends SuperActivity implements OnClickListener {
-
-	public static D2xxManager ftD2xx = null;
-	
 	TextView terminal;
 	Button sendButton;
-	Menu menu;
-	MenuItem connectButton;
-	FT_Device ftDev;
 	
 	@Override
 	int getNavigationItem() {
@@ -38,68 +29,20 @@ public class TerminalActivity extends SuperActivity implements OnClickListener {
 		    	
 	}
 	
-	@Override
-	protected void onResume() {
-		super.onRestart();
-		//TODO reimplement MAVClient.init();
-	}
-
-
-
-	@Override
-	protected void onStop() {
-		super.onDestroy();
-		//TODO reimplement MAVClient.onDestroy();
-	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_terminal, menu);
-		this.menu = menu;
-		connectButton = menu.findItem(R.id.menu_connect);
-
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.menu_connect:
-			//TODO reimplement MAVClient.sendConnectMessage();
-			return true;
 		default:
 			return super.onMenuItemSelected(featureId, item);
 		}
-	}
-	
-	//TODO reimplement 
-
-
-	/*
-	public MAVLinkClient MAVClient = new MAVLinkClient(this) {
-	
-		
-		@Override
-		public void notifyReceivedData(MAVLinkMessage m) {
-		
-		}
-	
-		@Override
-		public void notifyDisconnected() {
-			connectButton.setTitle(getResources().getString(
-					R.string.menu_connect));
-		}
-	
-		@Override
-		public void notifyConnected() {
-			connectButton.setTitle(getResources().getString(
-					R.string.menu_disconnect));
-		}
-	};
-	
-	*/
-	
-	
+	}	
 
 	@Override
 	public void onClick(View v) {	
