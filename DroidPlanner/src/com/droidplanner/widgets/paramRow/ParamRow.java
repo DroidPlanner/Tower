96ca8fb1b829c3a2b33bcaee8bc5a6a1ed5fdae6
Arthur Benemann <arthur.benemann@gmail.com>
2013-05-03 14:28:28 -0300

    The type view is irrelevant for the user.

--- DroidPlanner/src/com/droidplanner/widgets/paramRow/ParamRow.java
+++ DroidPlanner/src/com/droidplanner/widgets/paramRow/ParamRow.java
@@ -6,7 +6,6 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.widget.EditText;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -16,7 +15,6 @@ import com.droidplanner.MAVLink.parameters.Parameter;
 public class ParamRow extends TableRow implements TextWatcher {
 	private TextView nameView;
 	private EditText valueView;
-	private TextView typeView;
 	private TextView indexView;
 	private Parameter param;
 
@@ -33,7 +31,6 @@ public class ParamRow extends TableRow implements TextWatcher {
 	public void setParam(Parameter param) {
 		this.param = param;
 		nameView.setText(param.name);
-		typeView.setText(Integer.toString(param.type));
 		indexView.setText(Integer.toString(param.index));
 		valueView.setText(param.getValue());
 	}
@@ -41,22 +38,16 @@ public class ParamRow extends TableRow implements TextWatcher {
 	private void createRowViews(Context context) {
 		nameView = new TextView(context);
 		valueView = new EditText(context);
-		typeView = new TextView(context);
 		indexView = new TextView(context);
-
 		valueView.setInputType(InputType.TYPE_CLASS_NUMBER);
-
-		typeView.setGravity(Gravity.RIGHT);
 
 		indexView.setWidth(50);
 		nameView.setWidth(150);
 		valueView.setWidth(100);
-		typeView.setWidth(50);
 
 		addView(indexView);
 		addView(nameView);
 		addView(valueView);
-		addView(typeView);
 
 		valueView.addTextChangedListener(this);
 	}
