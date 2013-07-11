2feace643308352f45c1f3eedd6f0c5f4babff7f
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-11 17:12:06 -0300

    Cleaning RecordMe.
    Renaming functions, removing duplicated code for saving waypoints and removing unused variables.

--- DroidPlanner/src/com/droidplanner/helpers/RecordMe.java
+++ DroidPlanner/src/com/droidplanner/helpers/RecordMe.java
@@ -1,8 +1,7 @@
 package com.droidplanner.helpers;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -13,119 +12,85 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
+import com.MAVLink.waypoint;
+import com.droidplanner.R;
 import com.droidplanner.MAVLink.Drone;
-import com.droidplanner.helpers.file.FileStream;
 import com.droidplanner.service.MAVLinkClient;
+import com.droidplanner.waypoints.MissionWriter;
 
 public class RecordMe implements LocationListener {
 	private static final long MIN_TIME_MS = 2000;
 	private static final float MIN_DISTANCE_M = 0;
-	private Context context;
-	private boolean followMeEnabled = false;
-	private LocationManager locationManager;
-	private Drone drone;
-	private FileOutputStream file;
-	private int count;
 	
-	public RecordMe(MAVLinkClient MAVClient,Context context, Drone drone) {
+	private Context context;
+	private Drone drone;
+	private LocationManager locationManager;
+	private boolean recordMeEnabled = false;
+	private List<waypoint> waypoints = new ArrayList<waypoint>();
+
+	public RecordMe(MAVLinkClient MAVClient, Context context, Drone drone) {
 		this.context = context;
 		this.drone = drone;
-		this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+		this.locationManager = (LocationManager) context
+				.getSystemService(Context.LOCATION_SERVICE);
 	}
 
-	public void toogleFollowMeState() {
+	public void toogleRecordMeState() {
 		if (isEnabledInPreferences()) {
 			if (isEnabled()) {
-				disableFollowMe();
+				finishRecordMe();
 			} else {
-				enableFollowMe();
+				startRecordMe();
 			}
 		} else {
-			disableFollowMe();
-		}
-	}
-	
-	private void enableFollowMe() {
-		Toast.makeText(context, "FollowMe Enabled", Toast.LENGTH_SHORT).show();
-				
-		// Register the listener with the Location Manager to receive location updates
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_MS, MIN_DISTANCE_M, this);
-		count=0;
-		followMeEnabled = true;
-		if(isEnabledInPreferences()){
-			try {
-				file = FileStream.getWaypointFileStream();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}		
-	}
-	
-	private void disableFollowMe() {
-		Toast.makeText(context, "FollowMe Disabled", Toast.LENGTH_SHORT).show();
-		locationManager.removeUpdates(this);
-		followMeEnabled = false;
-		if(isEnabledInPreferences()){
-			try {
-				file.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public boolean isEnabled() {
-		return followMeEnabled;
-	}
-	
-	//record me writes location to waypoint file
-	public void writeFirstLine(Location l){
-		try {
-			file.write("QGC WPL 110\n0\t1\t0\t16\t0\t0\t0\t0\t".getBytes());
-			file.write(String.valueOf(l.getLatitude()).getBytes());
-			file.write("\t".getBytes());
-			file.write(String.valueOf(l.getLongitude()).getBytes());
-			file.write("\t".getBytes());
-			file.write(String.valueOf(drone.defaultAlt).getBytes());
-			file.write("\t1\n".getBytes());
-		} catch (IOException e) {
-		// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public void saveWaypoints(Location l) {
-		try {			
-			file.write(String.valueOf(count).getBytes());
-			file.write("\t0\t0\t16\t0\t0\t0\t0\t".getBytes());
-			file.write(String.valueOf(l.getLatitude()).getBytes());
-			file.write("\t".getBytes());
-			file.write(String.valueOf(l.getLongitude()).getBytes());
-			file.write("\t".getBytes());
-			file.write(String.valueOf(drone.defaultAlt).getBytes());
-			file.write("\t1\n".getBytes());
-				
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void onLocationChanged(Location location) {
-		if(isEnabledInPreferences()){
-			if(count==0){
-				writeFirstLine(location);
-				count++;
-			}else{
-				saveWaypoints(location);
-				count++;
-			}
+			finishRecordMe();
 		}
 	}
 
+	private void startRecordMe() {
+		Toast.makeText(context, "Record Enabled", Toast.LENGTH_SHORT).show();
+
+		// Register the listener with the Location Manager to receive location
+		// updates
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				MIN_TIME_MS, MIN_DISTANCE_M, this);
+		recordMeEnabled = true;
+		saveWaypointsToFile();
+	}
+
+	private void finishRecordMe() {
+		locationManager.removeUpdates(this);
+		recordMeEnabled = false;
+		saveWaypointsToFile();
+	}
+
+	private void saveWaypointsToFile() {
+		if (writeMission()) {
+			Toast.makeText(context, R.string.file_saved, Toast.LENGTH_SHORT)
+					.show();
+		} else {
+			Toast.makeText(context, R.string.error_when_saving,
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private boolean writeMission() {
+		MissionWriter missionWriter = new MissionWriter(drone.home, waypoints);
+		return missionWriter.saveWaypoints();
+	}
+
+	public boolean isEnabled() {
+		return recordMeEnabled;
+	}
+
+	// @Override
+	public void onLocationChanged(Location location) {
+		// TODO find a better way to do the altitude
+		waypoints.add(new waypoint(location.getLatitude(), location.getLongitude(), drone.defaultAlt));
+	}
+
 	@Override
-	public void onProviderDisabled(String provider) {		
+	public void onProviderDisabled(String provider) {
 	}
 
 	@Override
@@ -133,7 +98,7 @@ public class RecordMe implements LocationListener {
 	}
 
 	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {		
+	public void onStatusChanged(String provider, int status, Bundle extras) {
 	}
 
 	private boolean isEnabledInPreferences() {
