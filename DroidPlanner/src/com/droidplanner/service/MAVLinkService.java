d4030c50fe6c42197f5db5b3fca1700c1057c850
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-11 16:22:40 -0300

    Fix imports so we have no warnings

--- DroidPlanner/src/com/droidplanner/service/MAVLinkService.java
+++ DroidPlanner/src/com/droidplanner/service/MAVLinkService.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
