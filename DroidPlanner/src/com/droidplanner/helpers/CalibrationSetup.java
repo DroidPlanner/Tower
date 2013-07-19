56d6356bb486b7a8584c10bda21598fe398d6d23
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-19 16:02:38 -0300

    Refactoring: Extracting another two Mavlink messages from the Calibration Class.

--- DroidPlanner/src/com/droidplanner/helpers/CalibrationSetup.java
+++ DroidPlanner/src/com/droidplanner/helpers/CalibrationSetup.java
@@ -6,11 +6,8 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 
 import com.MAVLink.Messages.MAVLinkMessage;
-import com.MAVLink.Messages.ardupilotmega.msg_command_ack;
-import com.MAVLink.Messages.ardupilotmega.msg_command_long;
 import com.MAVLink.Messages.ardupilotmega.msg_statustext;
-import com.MAVLink.Messages.enums.MAV_CMD;
-import com.MAVLink.Messages.enums.MAV_CMD_ACK;
+import com.droidplanner.MAVLink.MavLinkCalibration;
 import com.droidplanner.service.MAVLinkClient;
 
 public class CalibrationSetup implements OnClickListener {
@@ -25,14 +22,14 @@ public class CalibrationSetup implements OnClickListener {
 
 	public void startCalibration(Context context) {
 		this.context = context;
-		sendStartCalibrationMessage();
+		MavLinkCalibration.sendStartCalibrationMessage(MAV);
 		count = 0;
 	}
 
 	@Override
 	public void onClick(DialogInterface dialog, int id) {
 		count++;
-		sendCalibrationAckMessage(count);
+		MavLinkCalibration.sendCalibrationAckMessage(count, MAV);
 		if (count >= 6) {
 			createDialog("Calibration Done!");
 			count = 0;
@@ -46,30 +43,6 @@ public class CalibrationSetup implements OnClickListener {
 				createDialog(statusMsg.getText());
 			}
 		}
-	}
-
-	private void sendStartCalibrationMessage() {
-		msg_command_long msg = new msg_command_long();
-		msg.target_system = 1;
-		msg.target_component = 1;
-
-		msg.command = MAV_CMD.MAV_CMD_PREFLIGHT_CALIBRATION;
-		msg.param1 = 0;
-		msg.param2 = 0;
-		msg.param3 = 0;
-		msg.param4 = 0;
-		msg.param5 = 1;
-		msg.param6 = 0;
-		msg.param7 = 0;
-		msg.confirmation = 0;
-		MAV.sendMavPacket(msg.pack());
-	}
-
-	private void sendCalibrationAckMessage(int count) {
-		msg_command_ack msg = new msg_command_ack();
-		msg.command = (short) count;
-		msg.result = MAV_CMD_ACK.MAV_CMD_ACK_OK;
-		MAV.sendMavPacket(msg.pack());
 	}
 
 	private void createDialog(String message) {
