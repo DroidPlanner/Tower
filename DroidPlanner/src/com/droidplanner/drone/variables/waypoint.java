4261ebc736ec31243fc16f7612dc94e1c813c8d6
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-19 21:28:53 -0300

    It may be better to warp the mission_item in a field then extending it.

--- DroidPlanner/src/com/droidplanner/drone/variables/waypoint.java
+++ DroidPlanner/src/com/droidplanner/drone/variables/waypoint.java
@@ -3,8 +3,8 @@ package com.droidplanner.drone.variables;
 import com.MAVLink.Messages.ardupilotmega.msg_mission_item;
 import com.google.android.gms.maps.model.LatLng;
 
-public class waypoint extends msg_mission_item {
-	private static final long serialVersionUID = 1L;
+public class waypoint {	
+	public msg_mission_item missionItem = new msg_mission_item();
 
 	public waypoint(LatLng c, Double h) {
 		this(c.latitude, c.longitude, h);
@@ -16,19 +16,19 @@ public class waypoint extends msg_mission_item {
 	}
 
 	public Double getHeight() {
-		return (double) z;
+		return (double) missionItem.z;
 	}
 
 	public LatLng getCoord() {
-		return new LatLng(x, y);
+		return new LatLng(missionItem.x, missionItem.y);
 	}
 
 	public void setHeight(Double height) {
-		z = height.floatValue();
+		missionItem.z = height.floatValue();
 	}
 
 	public void setCoord(LatLng coord) {
-		x = (float) coord.latitude;
-		y = (float) coord.longitude;
+		missionItem.x = (float) coord.latitude;
+		missionItem.y = (float) coord.longitude;
 	}
 }
