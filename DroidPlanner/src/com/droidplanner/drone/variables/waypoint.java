0ccbe2d09dbbc911cf35f2efa0ac0040b953df1b
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-19 21:20:47 -0300

    Change the waypoint class to be a warper of the mavlink message "mission_item".
    This way we have access to all the other fields of the mission_item class without creating a new object. This is the first step in the direction of fixing issue #133.

--- DroidPlanner/src/com/droidplanner/drone/variables/waypoint.java
+++ DroidPlanner/src/com/droidplanner/drone/variables/waypoint.java
@@ -1,15 +1,13 @@
 package com.droidplanner.drone.variables;
 
+import com.MAVLink.Messages.ardupilotmega.msg_mission_item;
 import com.google.android.gms.maps.model.LatLng;
 
-public class waypoint {	
-	
-	private LatLng coord;
-	private Double Height;
+public class waypoint extends msg_mission_item {
+	private static final long serialVersionUID = 1L;
 
 	public waypoint(LatLng c, Double h) {
-		setCoord(c);
-		setHeight(h);
+		this(c.latitude, c.longitude, h);
 	}
 
 	public waypoint(Double Lat, Double Lng, Double h) {
@@ -18,18 +16,19 @@ public class waypoint {
 	}
 
 	public Double getHeight() {
-		return Height;
-	}
-
-	public void setHeight(Double height) {
-		Height = height;
+		return (double) z;
 	}
 
 	public LatLng getCoord() {
-		return coord;
+		return new LatLng(x, y);
+	}
+
+	public void setHeight(Double height) {
+		z = height.floatValue();
 	}
 
 	public void setCoord(LatLng coord) {
-		this.coord = coord;
+		x = (float) coord.latitude;
+		y = (float) coord.longitude;
 	}
 }
