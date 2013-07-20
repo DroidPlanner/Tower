a2bb513d16bc05b4964e8aef378cb4a7072af9e2
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-19 21:40:25 -0300

    Adding other fields from the mission_item object.

--- DroidPlanner/src/com/droidplanner/drone/variables/waypoint.java
+++ DroidPlanner/src/com/droidplanner/drone/variables/waypoint.java
@@ -1,9 +1,10 @@
 package com.droidplanner.drone.variables;
 
 import com.MAVLink.Messages.ardupilotmega.msg_mission_item;
+import com.MAVLink.Messages.enums.MAV_FRAME;
 import com.google.android.gms.maps.model.LatLng;
 
-public class waypoint {	
+public class waypoint {
 	public msg_mission_item missionItem = new msg_mission_item();
 
 	public waypoint(LatLng c, Double h) {
@@ -13,6 +14,15 @@ public class waypoint {
 	public waypoint(Double Lat, Double Lng, Double h) {
 		setCoord(new LatLng(Lat, Lng));
 		setHeight(h);
+		
+		missionItem.current = 0; // TODO use correct parameter for HOME
+		missionItem.frame = MAV_FRAME.MAV_FRAME_GLOBAL;
+		missionItem.command = 16; // TODO use correct parameter
+		missionItem.param1 = 0; // TODO use correct parameter
+		missionItem.param2 = 0; // TODO use correct parameter
+		missionItem.param3 = 0; // TODO use correct parameter
+		missionItem.param4 = 0; // TODO use correct parameter
+		missionItem.autocontinue = 1; // TODO use correct parameter
 	}
 
 	public Double getHeight() {
