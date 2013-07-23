c4ccb5437469cebe5dc6162ed3584100bb349499
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-22 22:48:00 -0300

    Adding the option to change the waypoint altitude.

--- DroidPlanner/src/com/droidplanner/widgets/tableRow/MissionRow.java
+++ DroidPlanner/src/com/droidplanner/widgets/tableRow/MissionRow.java
@@ -3,26 +3,26 @@ package com.droidplanner.widgets.tableRow;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.text.InputType;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.MAVLink.Messages.ApmCommands;
+import com.droidplanner.dialogs.AltitudeDialog;
+import com.droidplanner.dialogs.AltitudeDialog.OnAltitudeChangedListner;
 import com.droidplanner.drone.variables.waypoint;
 import com.droidplanner.fragments.MissionFragment;
 
-public class MissionRow extends TableRow implements OnClickListener {
+public class MissionRow extends TableRow implements OnClickListener, OnAltitudeChangedListner {
 
 	private Context context;
 	private MissionFragment fragment;
 	private waypoint waypoint;
-	
+
 	private TextView nameView;
-	private EditText valueView;
+	private TextView altitudeView;
 	private TextView cmdView;
 	private Button removeButton;
 
@@ -38,55 +38,74 @@ public class MissionRow extends TableRow implements OnClickListener {
 		createRowViews(context);
 		update();
 	}
-	
-	public void update(){
-		valueView.setText(waypoint.getHeight().toString());
+
+	public void update() {
+		altitudeView.setText(waypoint.getHeight().toString() + 'm');
 		nameView.setText("WP " + waypoint.getNumber());
 		cmdView.setText(waypoint.getCmd().getName());
 	}
 
-	
 	private void createRowViews(Context context) {
 		nameView = new TextView(context);
-		valueView = new EditText(context);
-		valueView.setInputType(InputType.TYPE_CLASS_NUMBER);
+		altitudeView = new TextView(context);
 		cmdView = new TextView(context);
 		removeButton = new Button(context);
 		removeButton.setText("X");
 
-		//Sizes
+		// Sizes
 		nameView.setWidth(50);
 		cmdView.setWidth(60);
-		valueView.setWidth(80);
+		altitudeView.setWidth(80);
 
-		//Listeners
-		cmdView.setOnClickListener(this);	
+		// Listeners
+		cmdView.setOnClickListener(this);
+		altitudeView.setOnClickListener(this);
 		removeButton.setOnClickListener(this);
-		
+
 		addView(nameView);
 		addView(cmdView);
-		addView(valueView);
+		addView(altitudeView);
 		addView(removeButton);
 	}
 
 	@Override
 	public void onClick(View v) {
 		if (v.equals(removeButton)) {
-			fragment.onDeleteWaypoint(waypoint);
-		}else{
-		final String[] list = new String[ApmCommands.getNameList().size()];
-		ApmCommands.getNameList().toArray(list);		
-		AlertDialog.Builder dialog = new AlertDialog.Builder(context);
-		dialog.setTitle("Select type");
-		dialog.setItems(list, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface d, int i) {
-						waypoint.setCmd(ApmCommands.getCmd(list[i]));
-						update();
-					}
-				});
-		dialog.create().show();
+			onDeleteButtonClick();
+		} else if (v.equals(altitudeView)) {
+			onAltitudeClick();
+		} else if (v.equals(cmdView)) {
+			onCmdClick();
 		}
 	}
 
+	private void onDeleteButtonClick() {
+		fragment.onDeleteWaypoint(waypoint);
+	}
+
+	private void onAltitudeClick() {
+		AltitudeDialog dialog = new AltitudeDialog(this);
+		dialog.build(waypoint.getHeight(), context);
+	}
+
+	private void onCmdClick() {
+		final String[] list = new String[ApmCommands.getNameList().size()];
+		ApmCommands.getNameList().toArray(list);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+		dialog.setTitle("Select type");
+		dialog.setItems(list, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface d, int i) {
+				waypoint.setCmd(ApmCommands.getCmd(list[i]));
+				update();
+			}
+		});
+		dialog.create().show();
+	}
+
+	@Override
+	public void onAltitudeChanged(double newAltitude) {
+		waypoint.setHeight(newAltitude);
+		update();
+	}
 }
