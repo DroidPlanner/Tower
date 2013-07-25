e48862074b2c430adefcfca3c99e61b59277ba0d
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-25 00:11:49 -0300

    Updates the home marker use the correct icon.

--- DroidPlanner/src/com/droidplanner/fragments/markers/HomeMarker.java
+++ DroidPlanner/src/com/droidplanner/fragments/markers/HomeMarker.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import com.droidplanner.R.drawable;
 import com.droidplanner.drone.Drone;
+import com.droidplanner.drone.variables.Home;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
@@ -50,4 +51,23 @@ public class HomeMarker {
 	public boolean isHomeMarker(Marker marker) {
 		return homeMarker.equals(marker);
 	}
+
+	public static MarkerOptions build(Home home) {
+		return new MarkerOptions()
+				.position(home.getCoord())
+				.draggable(true)
+				.title("Home")
+				.snippet(
+						String.format(Locale.ENGLISH, "%.1fm", home.getHeight()))
+				.anchor((float) 0.5, (float) 0.5)
+				.icon(BitmapDescriptorFactory
+						.fromResource(drawable.ic_menu_home)).title("Home");
+	}
+
+	public static void update(Marker marker, Home home) {
+		marker.setPosition(home.getCoord());
+		marker.setSnippet(String.format(Locale.ENGLISH, "%s %.1fm", home
+				.getCmd().getName(), home.getHeight()));
+	}
+
 }
