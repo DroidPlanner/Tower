ac30f9932b99a6eba73f77291fedf29bd3487bb5
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-26 01:34:55 -0300

    Implementing multiple color waypoints.

--- DroidPlanner/src/com/droidplanner/fragments/markers/WaypointMarker.java
+++ DroidPlanner/src/com/droidplanner/fragments/markers/WaypointMarker.java
@@ -3,6 +3,8 @@ package com.droidplanner.fragments.markers;
 import java.util.Locale;
 
 import com.droidplanner.drone.variables.waypoint;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -15,14 +17,42 @@ public class WaypointMarker {
 				.title("WP" + Integer.toString(wp.getNumber()))
 				.snippet(
 						String.format(Locale.ENGLISH, "%s %.1fm", wp.getCmd()
-								.getName(), wp.getHeight()));// TODO include icons for each waypoint type
+								.getName(), wp.getHeight())).icon(getIcon(wp));
 	}
 
 	public static void update(Marker marker, waypoint wp) {
 		marker.setPosition(wp.getCoord());
 		marker.setTitle("WP" + Integer.toString(wp.getNumber()));
 		marker.setSnippet(String.format(Locale.ENGLISH, "%s %.1fm", wp.getCmd()
-				.getName(), wp.getHeight())); // TODO include icons for each waypoint type
+				.getName(), wp.getHeight()));
+		marker.setIcon(getIcon(wp));
+	}
+
+	private static BitmapDescriptor getIcon(waypoint wp) {
+		switch (wp.getCmd()) {
+		default:
+		case CMD_NAV_WAYPOINT:
+			return BitmapDescriptorFactory.defaultMarker();
+		case CMD_NAV_LOITER_TIME:
+		case CMD_NAV_LOITER_TURNS:
+		case CMD_NAV_LOITER_UNLIM:
+			return BitmapDescriptorFactory
+					.defaultMarker(BitmapDescriptorFactory.HUE_BLUE);
+		case CMD_NAV_RETURN_TO_LAUNCH:
+			return BitmapDescriptorFactory
+					.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+		case CMD_NAV_LAND:
+			return BitmapDescriptorFactory
+					.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+		case CMD_NAV_TAKEOFF:
+			return BitmapDescriptorFactory
+					.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA);
+		case CMD_NAV_PATHPLANNING:
+		case CMD_NAV_ROI:
+			return BitmapDescriptorFactory
+					.defaultMarker(BitmapDescriptorFactory.HUE_AZURE);
+		}
+
 	}
 
 }
