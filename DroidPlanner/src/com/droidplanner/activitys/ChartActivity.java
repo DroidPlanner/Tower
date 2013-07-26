cf41d9fa6c44116c6495fc42ce9b35a4f64149a9
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-26 18:05:38 -0300

    Implementing for fields in the chart.

--- DroidPlanner/src/com/droidplanner/activitys/ChartActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/ChartActivity.java
@@ -13,7 +13,8 @@ public class ChartActivity extends SuperActivity implements HudUpdatedListner {
 
 	public Chart chart;
 	public LinearLayout readoutMenu;
-	public String[] labels = { "Pitch", "Yaw", "Roll" };
+	public String[] labels = { "Pitch", "Roll","Yaw", "Altitude", "Target Alt.", "Latitude",
+			"Longitude","SatCount" ,"Voltage", "Current", "G. Speed", "A. Speed","WP","DistToWp", };
 	public ChartCheckBoxList checkBoxList = new ChartCheckBoxList();
 
 	@Override
@@ -39,6 +40,18 @@ public class ChartActivity extends SuperActivity implements HudUpdatedListner {
 		checkBoxList.updateCheckBox("Pitch", drone.orientation.getPitch());
 		checkBoxList.updateCheckBox("Roll", drone.orientation.getRoll());
 		checkBoxList.updateCheckBox("Yaw", drone.orientation.getYaw());
+		checkBoxList.updateCheckBox("Altitude", drone.altitude.getAltitude());
+		checkBoxList.updateCheckBox("Target Alt.", drone.altitude.getTargetAltitude());
+		checkBoxList.updateCheckBox("Latitude",	drone.GPS.getPosition().latitude);
+		checkBoxList.updateCheckBox("Longitude", drone.GPS.getPosition().longitude);
+		checkBoxList.updateCheckBox("SatCount", drone.GPS.getSatCount());
+		checkBoxList.updateCheckBox("Voltage", drone.battery.getBattVolt());
+		checkBoxList.updateCheckBox("Current", drone.battery.getBattCurrent());
+		checkBoxList.updateCheckBox("G. Speed", drone.speed.getGroundSpeed());
+		checkBoxList.updateCheckBox("A. Speed", drone.speed.getAirSpeed());
+		checkBoxList.updateCheckBox("DistToWp", drone.mission.getDisttowp());
+		checkBoxList.updateCheckBox("WP", drone.mission.getWpno());
+
 		chart.update();
 	}
 
