11ef6ed1d80f1e24124ad023ed6e3b0f65ccb3e7
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-25 23:14:59 -0300

    Refactoring: Extracting some methods.

--- DroidPlanner/src/com/droidplanner/activitys/SuperActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/SuperActivity.java
@@ -150,46 +150,54 @@ public abstract class SuperActivity extends Activity implements
 			connectButton.setTitle(getResources().getString(
 					R.string.menu_disconnect));
 		}
-		if (PreferenceManager.getDefaultSharedPreferences(
-				getApplicationContext()).getBoolean(
-				"pref_lock_screen_orientation", false)) {
-			int rotation = ((WindowManager) getSystemService(WINDOW_SERVICE))
-					.getDefaultDisplay().getRotation();
-			int actualOrientation = getResources().getConfiguration().orientation;
-			boolean naturalOrientationLandscape = (((rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) && actualOrientation == Configuration.ORIENTATION_LANDSCAPE) || ((rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) && actualOrientation == Configuration.ORIENTATION_PORTRAIT));
-			if (naturalOrientationLandscape) {
-				switch (rotation) {
-				case Surface.ROTATION_0:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-					break;
-				case Surface.ROTATION_90:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
-					break;
-				case Surface.ROTATION_180:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
-					break;
-				case Surface.ROTATION_270:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-					break;
-				}
-			} else {
-				switch (rotation) {
-				case Surface.ROTATION_0:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-					break;
-				case Surface.ROTATION_90:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-					break;
-				case Surface.ROTATION_180:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
-					break;
-				case Surface.ROTATION_270:
-					screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
-					break;
-				}
-			}
-			setRequestedOrientation(screenRequestedOrientation);
+		if (isPrefLockOrientationSet()) {
+			lockOrientation();
 		}
+	}
+
+	private boolean isPrefLockOrientationSet() {
+		return PreferenceManager.getDefaultSharedPreferences(
+				getApplicationContext()).getBoolean(
+				"pref_lock_screen_orientation", false);
+	}
+
+	private void lockOrientation() {
+		int rotation = ((WindowManager) getSystemService(WINDOW_SERVICE))
+				.getDefaultDisplay().getRotation();
+		int actualOrientation = getResources().getConfiguration().orientation;
+		boolean naturalOrientationLandscape = (((rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) && actualOrientation == Configuration.ORIENTATION_LANDSCAPE) || ((rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) && actualOrientation == Configuration.ORIENTATION_PORTRAIT));
+		if (naturalOrientationLandscape) {
+			switch (rotation) {
+			case Surface.ROTATION_0:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+				break;
+			case Surface.ROTATION_90:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+				break;
+			case Surface.ROTATION_180:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+				break;
+			case Surface.ROTATION_270:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+				break;
+			}
+		} else {
+			switch (rotation) {
+			case Surface.ROTATION_0:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+				break;
+			case Surface.ROTATION_90:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+				break;
+			case Surface.ROTATION_180:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+				break;
+			case Surface.ROTATION_270:
+				screenRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+				break;
+			}
+		}
+		setRequestedOrientation(screenRequestedOrientation);
 	}
 
 	@Override
