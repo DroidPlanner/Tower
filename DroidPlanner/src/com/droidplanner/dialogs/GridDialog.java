b622d9d668d6e3f759df52bbb9519ac21869d42d
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-03 00:21:47 -0300

    Extracting layout information of Dialog Grid to an xml file.

--- DroidPlanner/src/com/droidplanner/dialogs/GridDialog.java
+++ DroidPlanner/src/com/droidplanner/dialogs/GridDialog.java
@@ -6,9 +6,11 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.widget.LinearLayout;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Toast;
 
+import com.droidplanner.R;
 import com.droidplanner.drone.variables.waypoint;
 import com.droidplanner.polygon.GridBuilder;
 import com.droidplanner.polygon.Polygon;
@@ -49,27 +51,12 @@ public abstract class GridDialog implements DialogInterface.OnClickListener {
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setTitle("Polygon Generator");
 
-		LinearLayout layout = new LinearLayout(context);
-		layout.setOrientation(LinearLayout.VERTICAL);
-
-		angleView = new SeekBarWithText(context);
-		angleView.setMinMaxInc(0, 180, 0.1);
-		angleView.setTitle("Hatch angle:");
-		angleView.setUnit("deg");
-
-		distanceView = new SeekBarWithText(context);
-		distanceView.setMinMaxInc(5, 500, 5);
-		distanceView.setTitle("Distance between lines:");
-		distanceView.setUnit("m");
-		
-		altitudeView = new SeekBarWithText(context);
-		altitudeView.setMinMaxInc(5, 1000, 5);
-		altitudeView.setTitle("Flight Altitude:");
-		altitudeView.setUnit("m");
-
-		layout.addView(angleView);
-		layout.addView(distanceView);
-		layout.addView(altitudeView);
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.dialog_grid, null);
+		angleView = (SeekBarWithText) layout.findViewById(R.id.angleView);
+		distanceView = (SeekBarWithText) layout.findViewById(R.id.distanceView);
+		altitudeView = (SeekBarWithText) layout.findViewById(R.id.altitudeView);
 		builder.setView(layout);
 
 		builder.setNegativeButton("Cancel", this).setPositiveButton("Ok", this);
@@ -81,7 +68,8 @@ public abstract class GridDialog implements DialogInterface.OnClickListener {
 	public void onClick(DialogInterface arg0, int which) {
 		if (which == Dialog.BUTTON_POSITIVE) {
 			GridBuilder grid = new GridBuilder(polygon, angleView.getValue(),
-					distanceView.getValue(), originPoint, altitudeView.getValue());
+					distanceView.getValue(), originPoint,
+					altitudeView.getValue());
 
 			onPolygonGenerated(grid.hatchfill());
 		}
