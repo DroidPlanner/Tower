47c25823e8674b17ca74bf460d565ab507df7c53
Joe DeMarco <joedjoedjoe@gmail.com>
2013-08-07 16:52:38 -0400

    Revise buttons to work with reformatted code Change launch to more of a "start mission" button launch switches to auto mode and gives throttle input

--- DroidPlanner/src/com/droidplanner/activitys/FlightDataActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/FlightDataActivity.java
@@ -3,14 +3,24 @@ package com.droidplanner.activitys;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 
 import com.droidplanner.DroidPlannerApp.OnWaypointUpdateListner;
 import com.droidplanner.R;
 import com.droidplanner.activitys.helpers.SuperFlightActivity;
 import com.droidplanner.fragments.FlightMapFragment;
+import com.droidplanner.helpers.RcOutput;
 
 public class FlightDataActivity extends SuperFlightActivity implements
-		OnWaypointUpdateListner {
+		OnWaypointUpdateListner, OnClickListener, OnTouchListener {
+	
+	private Button arm, disarm, launch, land, rtl, stabilize;
+//	private OnTouchListener armListen;
+	private RcOutput rcOutput;
 
 	@Override
 	public int getNavigationItem() {
@@ -29,7 +39,20 @@ public class FlightDataActivity extends SuperFlightActivity implements
 
 		drone.mission.missionListner = this;
 		drone.setDroneTypeChangedListner(this);
-
+		drone = app.drone;
+		rcOutput = new RcOutput(drone, this);
+		arm = (Button) findViewById(R.id.arm);
+		disarm = (Button) findViewById(R.id.disarm);
+		launch = (Button) findViewById(R.id.launch);
+		land = (Button) findViewById(R.id.land);
+		rtl = (Button) findViewById(R.id.rtl);
+		stabilize = (Button) findViewById(R.id.stabilize);
+		arm.setOnTouchListener(this);
+		disarm.setOnTouchListener(this);
+		launch.setOnTouchListener(this);
+		land.setOnClickListener(this);
+		rtl.setOnClickListener(this);
+		stabilize.setOnClickListener(this);
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,6 +71,41 @@ public class FlightDataActivity extends SuperFlightActivity implements
 			return true;
 		default:
 			return super.onMenuItemSelected(featureId, item);
+		}
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		if (v.equals(launch)) {
+			simulateLaunchEvent(event);
+		} else if (v.equals(arm)) {
+			rcOutput.simulateArmEvent(event);
+		} else if (v.equals(disarm)) {
+			rcOutput.simulateDisarmEvent(event);
+		}
+		return false;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.equals(rtl)) {
+			OnModeSpinnerSelected("RTL");
+		} else if (v.equals(stabilize)) {
+			OnModeSpinnerSelected("Stabilize");
+		} else if (v.equals(land)){
+//			setLandPoint(new waypoint(drone.getPosition(), 0.0));
+		}
+		
+	}
+	
+	private void simulateLaunchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//			setLaunchPoint(new waypoint(drone.getPosition(),drone.defaultAlt));
+			rcOutput.enableRcOverride();
+			rcOutput.setRcChannel(RcOutput.TROTTLE, 1);	
+		}
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			rcOutput.disableRcOverride();
 		}
 	}
 
