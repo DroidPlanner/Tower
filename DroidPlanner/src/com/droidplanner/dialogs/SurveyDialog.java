DroidPlanner/Tower

commit e6dce83d61c9f2a1537331bbf1b81021adf02b6b

Raise an error Toast when there are no camera files in the device.

DroidPlanner/src/com/droidplanner/dialogs/SurveyDialog.java
@@ -73,11 +73,16 @@ public abstract class SurveyDialog implements DialogInterface.OnClickListener,
 	}
 
 	private void updateCameraSpinner(Context context) {
-		avaliableCameras = new ArrayAdapter<CharSequence>(context,
-				android.R.layout.simple_spinner_dropdown_item);
-		avaliableCameras.addAll(FileList.getCameraInfoFileList());
-		cameraSpinner.setAdapter(avaliableCameras);
-		cameraSpinner.setSelection(0);
+		String[] list = FileList.getCameraInfoFileList();
+		if (list.length > 0) {
+			avaliableCameras = new ArrayAdapter<CharSequence>(context,
+					android.R.layout.simple_spinner_dropdown_item);
+			avaliableCameras.addAll();
+			cameraSpinner.setAdapter(avaliableCameras);
+			cameraSpinner.setSelection(0);
+		}else{
+			Toast.makeText(context, context.getString(R.string.no_files), Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override

commit db33322f8814f9e2d77cbfa5bc523bb73de9e5c1

Fix one error introduced in e6dce83d61

DroidPlanner/src/com/droidplanner/dialogs/SurveyDialog.java
@@ -77,7 +77,7 @@ public abstract class SurveyDialog implements DialogInterface.OnClickListener,
 		if (list.length > 0) {
 			avaliableCameras = new ArrayAdapter<CharSequence>(context,
 					android.R.layout.simple_spinner_dropdown_item);
-			avaliableCameras.addAll();
+			avaliableCameras.addAll(list);
 			cameraSpinner.setAdapter(avaliableCameras);
 			cameraSpinner.setSelection(0);
 		}else{