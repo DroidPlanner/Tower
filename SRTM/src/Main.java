802b433dcae660e57b248714758da91913d62312
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-22 01:41:24 -0300

    Removing unused string

--- SRTM/src/Main.java
+++ SRTM/src/Main.java
@@ -4,8 +4,6 @@ public class Main {
 	public static void main(String [ ] args){
 		System.out.println("Start");
 		
-		SRTM.exists(0, 0,"./bin/SRTM");
-		
 		int alt = SRTM.getData(-51.1439127,-29.7026708,"./bin/SRTM"); //Near my house
 		int sea = SRTM.getData(-50.0360209,-29.8055343,"./bin/SRTM"); //Sea level
 		
