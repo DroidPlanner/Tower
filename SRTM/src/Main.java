c7f251cb8ab1f2402b9a1d3a3cdd1ced9863fd09
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-22 14:18:08 -0300

    Huge performance gain by using Buffered streams

--- SRTM/src/Main.java
+++ SRTM/src/Main.java
@@ -1,14 +1,21 @@
 
 public class Main {
 
+	static long time;
 	public static void main(String [ ] args){
 		System.out.println("Start");
+		time = System.currentTimeMillis();
 		
 		int alt = SRTM.getData(-51.1439127,-29.7026708,"./bin/SRTM"); //Near my house
-		int sea = SRTM.getData(-50.0360209,-29.8055343,"./bin/SRTM"); //Sea level
+		System.out.println("Time:"+(System.currentTimeMillis()-time)+"ms");
+		time = System.currentTimeMillis();
+		int sea = SRTM.getData(-50.0360209,-20.8055343,"./bin/SRTM"); //Sea level
+		System.out.println("Time:"+(System.currentTimeMillis()-time)+"ms");
+		time = System.currentTimeMillis();
+		sea = SRTM.getData(-50.0360209,-10.8055343,"./bin/SRTM"); //Sea level
+		System.out.println("Time:"+(System.currentTimeMillis()-time)+"ms");
+		time = System.currentTimeMillis();
 		
 		System.out.println("Altitude:"+alt+" Sea:"+sea);
-		
-		System.out.println("End");
-	}
+		}
 }
