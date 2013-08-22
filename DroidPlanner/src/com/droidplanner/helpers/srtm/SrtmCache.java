97b79a8c4532a9aee2446b079c8294f5d33c2c30
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-22 20:06:55 -0300

    Updating Srtm classes in DP

--- DroidPlanner/src/com/droidplanner/helpers/srtm/SrtmCache.java
+++ DroidPlanner/src/com/droidplanner/helpers/srtm/SrtmCache.java
@@ -15,9 +15,9 @@ public class SrtmCache extends AsyncTask<Integer, Integer, Integer>{
 		Log.d("SRTM","fetching data");
 		for (int i = 0; i < 1000; i++) {
 			
-		alt = SRTM.getData(-51.1439127,-29.7026708,path); //Near my house
-		sea = SRTM.getData(-50.0360209,-29.8055343,path); //Sea level	
-		high = SRTM.getData(-50.0360209,-20.8055343,path); //High place	
+		alt = Srtm.getData(-51.1439127,-29.7026708,path); //Near my house
+		sea = Srtm.getData(-50.0360209,-29.8055343,path); //Sea level	
+		high = Srtm.getData(-50.0360209,-20.8055343,path); //High place	
 		}
 		Log.d("SRTM","Altitude:"+alt+" Sea:"+sea+ " High:"+high);
 		
--- DroidPlanner/src/com/droidplanner/helpers/srtm/SrtmDownloader.java
+++ DroidPlanner/src/com/droidplanner/helpers/srtm/SrtmDownloader.java
@@ -1,88 +1,79 @@
 package com.droidplanner.helpers.srtm;
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class SrtmDownloader {
 
-	static boolean downloadFile(String urlAddress, File output) {
-		URL url1;
-		InputStream inputs;
-		BufferedOutputStream outputs;
-		System.out.println("file:"+output.getName());
-		try {
-			url1 = new URL(urlAddress);
-			inputs = url1.openStream();
-			outputs = new BufferedOutputStream(new FileOutputStream(output));
-			int i = 0;
-			int ch = 0;
-			while (ch >= 0) {
-				ch = inputs.read();
-				if (ch >= 0) {
-					outputs.write(ch);
-				}
-				i++;
-				if (i % 100000 == 0) {
-					System.out.print("-");
-				}
-			}
-			System.out.println("");
-			inputs.close();
-			outputs.close();
-	
-		} catch (MalformedURLException ex) {
-			Logger.getLogger(SRTM.class.getName()).log(Level.SEVERE, "", ex);
-			return false;
-		} catch (FileNotFoundException ex) {
-			return false;
-		} catch (IOException ex) {
-			Logger.getLogger(SRTM.class.getName()).log(Level.SEVERE, "", ex);
-			return false;
-		}
-	
-		return true;
-	}
-
-	static boolean downloadRegionIndex(int region, String srtmPath,
-			String url) {
+	static void downloadRegionIndex(int region, String srtmPath, String url)
+			throws IOException {
 		String regionIndex = SrtmRegions.REGIONS[region] + ".index.html";
 		if (!srtmPath.equals("")) {
 			regionIndex = srtmPath + "/" + regionIndex;
 		}
 		File regionIndexFile = new File(regionIndex);
-		return downloadFile(url + SrtmRegions.REGIONS[region] + "/", regionIndexFile);
+		downloadFile(url + SrtmRegions.REGIONS[region] + "/", regionIndexFile);
 	}
 
-	static boolean download(String fname, String path) {
+	static void downloadSrtmFile(String fname, String path) throws Exception {
 		File output;
 		String region = SrtmRegions.findRegion(fname, path);
-		if (region == null) {
-			return false;
+		output = new File(path + "/" + fname + ".zip");
+		downloadSrtmFile(fname, output, region);
+		UnZip.unZipIt(fname, output);
+		output.delete();
+	}
+
+	private static void downloadSrtmFile(String fname, File output,
+			String region) throws IOException {
+		try {
+			downloadFile(Srtm.url + region + "/" + fname + ".zip", output);
+		} catch (IOException e) {
+			downloadAlternativeSrtmFile(fname, output, region, e);
 		}
-		if (path.equals("")) {
-			output = new File(region + "/" + fname + ".zip");
-		} else {
-			output = new File(path + "/" + region + "/" + fname + ".zip");
-		}
-		boolean result = downloadFile(SRTM.url + region + "/" + fname + ".zip",
-				output);
+	}
+
+	private static void downloadAlternativeSrtmFile(String fname, File output,
+			String region, IOException e) throws IOException {
 		// fix SRTM 2.1 naming problem in North America
-		if ((!result) && fname.startsWith("N5")
+		if (fname.startsWith("N5")
 				&& region.equalsIgnoreCase("North_America")) {
-			if (downloadFile(SRTM.url + region + "/" + fname.replace(".hgt", "hgt")
-					+ ".zip", output)) {
-				return true;
+			downloadFile(
+					Srtm.url + region + "/" + fname.replace(".hgt", "hgt")
+							+ ".zip", output);
+		} else {
+			throw e;
+		}
+	}
+
+	private static void downloadFile(String urlAddress, File output)
+			throws IOException {
+		URL url1;
+		InputStream inputs;
+		BufferedOutputStream outputs;
+		System.out.println("file:" + output.getName());
+		url1 = new URL(urlAddress);
+		inputs = url1.openStream();
+		outputs = new BufferedOutputStream(new FileOutputStream(output));
+		int i = 0;
+		int ch = 0;
+		while (ch >= 0) {
+			ch = inputs.read();
+			if (ch >= 0) {
+				outputs.write(ch);
+			}
+			i++;
+			if (i % 100000 == 0) {
+				System.out.print("-");
 			}
 		}
-		return result;
+		System.out.println("");
+		inputs.close();
+		outputs.close();
+
 	}
 
 }
