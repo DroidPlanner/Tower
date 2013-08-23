ea4d6d1e084b89d83fdb6f238c9b8054b65e8532
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-22 23:39:34 -0300

    Fixing SRTM download function so that the callback shows a percentage

--- Tests/src/srtm/SrtmTest.java
+++ Tests/src/srtm/SrtmTest.java
@@ -1,9 +1,8 @@
 package srtm;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-
-import helpers.TimeLogger;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -14,15 +13,14 @@ import com.srtm.Srtm.OnProgressListner;
 
 public class SrtmTest {
 	private static final String PATH = "./bin/SRTM";
-	TimeLogger log;
 	Srtm srtm;
-	
+	protected int progressCalbacks;
+
 	@Before
-	public void Setup(){
-		log = new TimeLogger();
-		srtm = new Srtm(PATH);		
+	public void Setup() {
+		srtm = new Srtm(PATH);
 	}
-		
+
 	@Test
 	public void testSrtm() {
 		int alt = srtm.getData(-51.1439127, -29.7026708);
@@ -33,19 +31,20 @@ public class SrtmTest {
 		assertEquals(1, sea);
 		assertEquals(406, high);
 	}
-	
-	//@Ignore("Since it needs to fetch a file this can be very slow")
+
+	@Ignore("Since it needs to fetch a file this can be very slow")
 	@Test
 	public void testListner() {
-		new File(PATH+"/S30W052.hgt").delete();
-		srtm.setListner(new OnProgressListner() {			
+		//	new File(PATH + "/S30W052.hgt").delete();
+		
+		srtm.setListner(new OnProgressListner() {
 			@Override
-			public void onProgress(String filename, float percentage) {
-				System.out.println("OnProgress:"+filename+" %:"+percentage);
+			public void onProgress(String filename, int percentage) {
+				System.out.println("OnProgress:" + filename + " %:"+ percentage);
 			}
 		});
-		srtm.getData(-51.1439127, -29.7026708);
-		
+		srtm.getData(-51.1439127, -29.7026708);		
+
 	}
 
 }
