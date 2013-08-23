d3afd7a13d8d5c94236a724cc94f48f0e0979a61
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-22 22:40:08 -0300

    Refactoring Srtm classes to objects

--- SRTM/src/com/srtm/Srtm.java
+++ SRTM/src/com/srtm/Srtm.java
@@ -1,26 +1,27 @@
 package com.srtm;
 public class Srtm {
 	private static final int SRTM_NaN = -32768;
-	public SrtmData srtmData = new SrtmData();
 	
-	Srtm(String dir) {
-		this.srtmData.path = dir;
+	public interface OnProgressListner{
+		public void onProgress(float percentage, String filename);
+	}
+	
+	public SrtmData srtmData;
+	
+	
+	public Srtm(String dir) {
+		srtmData = new SrtmData(dir);
 	}
 
 	/*
 	 * Get SRTM elevation in meters for lon and lat WGS-84 coordinates
 	 */
-	public static int getData(double lon, double lat, String dir) {
+	public int getData(double lon, double lat) {
 		try {
-			return get(lon, lat, dir);
+			return srtmData.load(lon, lat);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return SRTM_NaN; // SRTM NaN
 		}
-	}
-	
-	private static int get(double lon, double lat, String dir) throws Exception {
-		Srtm srtm = new Srtm(dir);
-		return srtm.srtmData.load(srtm, lon, lat);
 	}
 }
--- Tests/src/srtm/SrtmTest.java
+++ Tests/src/srtm/SrtmTest.java
@@ -11,13 +11,14 @@ public class SrtmTest {
 	@Test
 	public void testSrtm() {
 		TimeLogger log = new TimeLogger();
+		Srtm srtm = new Srtm("./bin/SRTM"); 
 
 		System.out.println("Start");
-		int alt = Srtm.getData(-51.1439127, -29.7026708, "./bin/SRTM");
+		int alt = srtm.getData(-51.1439127, -29.7026708);
 		log.tick();
-		int sea = Srtm.getData(-50.0360209, -29.8055343, "./bin/SRTM");
+		int sea = srtm.getData(-50.0360209, -29.8055343);
 		log.tick();
-		int high = Srtm.getData(-50.0360209, -20.8055343, "./bin/SRTM");
+		int high = srtm.getData(-50.0360209, -20.8055343);
 		log.tick();
 		System.out.println("Altitude:" + alt + " Sea:" + sea + " High:" + high);
 
