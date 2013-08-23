44f8e222dd0a07495d37eaa6fab7725f91a73818
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-23 00:35:29 -0300

    Fixing the SRTM files so they work with DP.

--- DroidPlanner/src/com/droidplanner/helpers/srtm/SrtmCache.java
+++ DroidPlanner/src/com/droidplanner/helpers/srtm/SrtmCache.java
@@ -4,24 +4,32 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.droidplanner.file.DirectoryPath;
+import com.droidplanner.helpers.srtm.Srtm.OnProgressListner;
 
-public class SrtmCache extends AsyncTask<Integer, Integer, Integer> {
+public class SrtmCache extends AsyncTask<Integer, Integer, Integer> implements OnProgressListner {
 
 	@Override
 	protected Integer doInBackground(Integer... params) {
 		Srtm Srtm = new Srtm(DirectoryPath.getSrtmPath());
-
+		Srtm.setListner(this);
 		int alt = 0, sea = 0, high = 0;
 
 		Log.d("SRTM", "fetching data");
-		for (int i = 0; i < 1000; i++) {
-			alt = Srtm.getData(-51.1439127, -29.7026708); // Near my house
-			sea = Srtm.getData(-50.0360209, -29.8055343); // Sea level
-			high = Srtm.getData(-50.0360209, -20.8055343); // High place
-		}
+		alt = Srtm.getData(-51.1439127, -29.7026708); // Near my house
+		sea = Srtm.getData(-50.0360209, -29.8055343); // Sea level
+		high = Srtm.getData(-50.0360209, -20.8055343); // High place
 		Log.d("SRTM", "Altitude:" + alt + " Sea:" + sea + " High:" + high);
-
 		return null;
 	}
+	
+	
+	@Override
+	public void onProgress(String filename, int percentage) {
+		if (percentage>=0) {
+			Log.d("SRTM", "Downloading "+filename+" - "+percentage+"%");			
+		}else{
+			Log.d("SRTM", "Downloading "+filename);
+		}
+	}	
 
 }
