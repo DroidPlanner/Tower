bb9e0d7bff97c531f32071b30a79ddfe2a6e252c
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-27 00:30:11 -0300

    Creating a new drone interpolator class.

--- DroidPlanner/src/com/droidplanner/fragments/markers/DroneMarker.java
+++ DroidPlanner/src/com/droidplanner/fragments/markers/DroneMarker.java
@@ -1,5 +1,9 @@
 package com.droidplanner.fragments.markers;
 
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+
 import com.droidplanner.drone.DroneInterfaces.MapUpdatedListner;
 import com.droidplanner.fragments.FlightMapFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,10 +15,13 @@ public class DroneMarker implements MapUpdatedListner {
 	private Marker droneMarker;
 	private FlightMapFragment flightMapFragment;
 	private DroneBitmaps bitmaps;
+	private DroneInterpolator interpolator;
 
 	public DroneMarker(FlightMapFragment flightMapFragment) {
 		this.flightMapFragment = flightMapFragment;
 		updateDroneMarkers();
+		interpolator = new DroneInterpolator();
+		interpolator.run();
 	}
 
 	private void updatePosition(double yaw, LatLng coord) {
@@ -43,8 +50,8 @@ public class DroneMarker implements MapUpdatedListner {
 	}
 
 	public void updateDroneMarkers() {
-		if (droneMarker!=null) {
-			droneMarker.remove();			
+		if (droneMarker != null) {
+			droneMarker.remove();
 		}
 		buildBitmaps();
 		addMarkerToMap();
@@ -62,9 +69,51 @@ public class DroneMarker implements MapUpdatedListner {
 	}
 
 	public void onDroneUpdate() {
-		updatePosition(flightMapFragment.drone.orientation.getYaw(),
-				flightMapFragment.drone.GPS.getPosition());
-		flightMapFragment.addFlithPathPoint(flightMapFragment.drone.GPS
-				.getPosition());
+		/*
+		 * updatePosition(flightMapFragment.drone.orientation.getYaw(),
+		 * flightMapFragment.drone.GPS.getPosition());
+		 * flightMapFragment.addFlithPathPoint(flightMapFragment.drone.GPS
+		 * .getPosition());
+		 */
+		interpolator.newPosition(flightMapFragment.drone.GPS.getPosition());
+	}
+
+	class DroneInterpolator {
+		private static final int PERIOD_MS = 16;
+		double velX = 0, velY = 0;
+		LatLng lastPosition = new LatLng(0, 0);
+		int iteration = 0;
+		private long lastTime;
+
+		public void newPosition(LatLng newPos) {
+			long currentTime = SystemClock.elapsedRealtime();
+			long dt = (currentTime-lastTime);
+			lastTime = currentTime;
+			
+			velX = (newPos.longitude - lastPosition.longitude)/dt;
+			velY = (newPos.latitude - lastPosition.latitude)/dt;
+			lastPosition = newPos;
+			
+			iteration = 0;
+		}
+		
+		public void run() {
+			final Handler handler = new Handler();
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					double yaw = flightMapFragment.drone.orientation.getYaw();
+					double t = iteration * PERIOD_MS;
+					LatLng coord = new LatLng(lastPosition.latitude + velY*t, lastPosition.longitude + velX*t);
+					updatePosition(yaw, coord);
+					Log.d("I", "interpolator " + t);
+
+					iteration++;
+					// Post again 16ms later.
+					handler.postDelayed(this, PERIOD_MS);
+				}
+			});
+		}
+
 	}
 }
