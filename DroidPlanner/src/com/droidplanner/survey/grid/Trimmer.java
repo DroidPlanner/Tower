96653c5f1914c1427274b1229ca526d5a3e71f69
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-28 16:29:49 -0300

    Extracting method

--- DroidPlanner/src/com/droidplanner/survey/grid/Trimmer.java
+++ DroidPlanner/src/com/droidplanner/survey/grid/Trimmer.java
@@ -8,6 +8,8 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class Trimmer {
 	List<LineLatLng> trimedGrid = new ArrayList<LineLatLng>();
+	LatLng closestPoint = null;
+	LatLng farestPoint = null;
 
 	public Trimmer(List<LineLatLng> grid, List<LineLatLng> polygon) {
 
@@ -16,42 +18,51 @@ public class Trimmer {
 			double closestDistance = Double.MAX_VALUE;
 			double farestDistance = Double.MIN_VALUE;
 
-			LatLng closestPoint = null;
-			LatLng farestPoint = null;
 
 			int crosses = 0;
 
-			for (LineLatLng polyLine : polygon) {
-				LatLng newlatlong = GeoTools.FindLineIntersection(polyLine,gridLine);
+			crosses = findCrossings(polygon, gridLine, closestDistance,
+					farestDistance, crosses);
 
-				if (newlatlong != null) {
-					crosses++;
-					if (closestDistance > GeoTools.getAproximatedDistance(
-							gridLine.p1, newlatlong)) {
-						closestPoint = new LatLng(newlatlong.latitude,
-								newlatlong.longitude);
-						closestDistance = GeoTools.getAproximatedDistance(
-								gridLine.p1, newlatlong);
-					}
-					if (farestDistance < GeoTools.getAproximatedDistance(
-							gridLine.p1, newlatlong)) {
-						farestPoint = new LatLng(newlatlong.latitude,
-								newlatlong.longitude);
-						farestDistance = GeoTools.getAproximatedDistance(
-								gridLine.p1, newlatlong);
-					}
+			processCrossings(crosses);
+		}
+	}
+
+	private int findCrossings(List<LineLatLng> polygon, LineLatLng gridLine,
+			double closestDistance, double farestDistance, int crosses) {
+		for (LineLatLng polyLine : polygon) {
+			LatLng newlatlong = GeoTools.FindLineIntersection(polyLine,gridLine);
+
+			if (newlatlong != null) {
+				crosses++;
+				if (closestDistance > GeoTools.getAproximatedDistance(
+						gridLine.p1, newlatlong)) {
+					closestPoint = new LatLng(newlatlong.latitude,
+							newlatlong.longitude);
+					closestDistance = GeoTools.getAproximatedDistance(
+							gridLine.p1, newlatlong);
+				}
+				if (farestDistance < GeoTools.getAproximatedDistance(
+						gridLine.p1, newlatlong)) {
+					farestPoint = new LatLng(newlatlong.latitude,
+							newlatlong.longitude);
+					farestDistance = GeoTools.getAproximatedDistance(
+							gridLine.p1, newlatlong);
 				}
 			}
+		}
+		return crosses;
+	}
 
-			switch (crosses) {
-			case 0:
-			case 1:
-				break;
-			default: // TODO handle multiple crossings in a better way
-			case 2:
-				trimedGrid.add(new LineLatLng(closestPoint, farestPoint));
-				break;
-			}
+	private void processCrossings(int crosses) {
+		switch (crosses) {
+		case 0:
+		case 1:
+			break;
+		default: // TODO handle multiple crossings in a better way
+		case 2:
+			trimedGrid.add(new LineLatLng(closestPoint, farestPoint));
+			break;
 		}
 	}
 
