39ef620be88b6deebd440862b752fe631ac4301f
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-29 02:18:53 -0300

    Starting to use an AdaptorList for the mission Fragment

--- DroidPlanner/src/com/droidplanner/fragments/MissionFragment.java
+++ DroidPlanner/src/com/droidplanner/fragments/MissionFragment.java
@@ -1,45 +1,41 @@
 package com.droidplanner.fragments;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
+import android.widget.ListView;
 
 import com.droidplanner.R;
 import com.droidplanner.drone.variables.Mission;
 import com.droidplanner.drone.variables.waypoint;
 import com.droidplanner.widgets.tableRow.MissionRow;
 
-public class MissionFragment extends Fragment {
-	public TableLayout table;
+public class MissionFragment extends ListFragment {
+	public ListView list;
 	private Mission mission;
+	private MissionRow adapter;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.mission_fragment, container,
 				false);
-		table = (TableLayout) view.findViewById(R.id.missionTable);
+		list = (ListView) view.findViewById(R.id.listView1);	
+		adapter = new MissionRow(this.getActivity(), android.R.layout.simple_list_item_1);		
+		list.setAdapter(adapter);		
 		return view;
 	}
 
 	public void setMission(Mission mission) {
 		this.mission = mission;
+		adapter = new MissionRow(this.getActivity(), android.R.layout.simple_list_item_1,mission.getWaypoints());
+		setListAdapter(adapter);
 	}
 
 	public void update() {
-		table.removeAllViews();
-		int i = 0;
-		for (waypoint wp : mission.getWaypoints()) {
-			addRow(wp, i++);
-		}
-	}
-
-	private void addRow(waypoint wp, int i) {
-		MissionRow row = new MissionRow(this, wp);
-		table.addView(row);
+		adapter.notifyDataSetChanged();
 	}
 
 	public void onDeleteWaypoint(waypoint wp) {
--- DroidPlanner/src/com/droidplanner/widgets/tableRow/MissionRow.java
+++ DroidPlanner/src/com/droidplanner/widgets/tableRow/MissionRow.java
@@ -1,54 +1,48 @@
 package com.droidplanner.widgets.tableRow;
 
-import java.util.Locale;
+import java.util.List;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TableRow;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.MAVLink.Messages.ApmCommands;
-import com.droidplanner.dialogs.AltitudeDialog;
-import com.droidplanner.dialogs.AltitudeDialog.OnAltitudeChangedListner;
 import com.droidplanner.drone.variables.waypoint;
-import com.droidplanner.fragments.MissionFragment;
 
-public class MissionRow extends TableRow implements OnClickListener,
-		OnAltitudeChangedListner {
+public class MissionRow extends ArrayAdapter<waypoint> {
 
-	private Context context;
-	private MissionFragment fragment;
-	private waypoint waypoint;
+	public MissionRow(Context context, int resource, List<waypoint> objects) {
+		super(context, resource, objects);
+	}
+
+	public MissionRow(Context context, int resource) {
+		super(context, resource);
+	}
+
+	/*
+	 * private List<waypoint> waypoints; private Context context; private
+	 * MissionFragment fragment;
+	 */
+
 
 	private TextView nameView;
 	private TextView altitudeView;
 	private TextView cmdView;
 	private Button removeButton;
+	private LinearLayout layout;
 
-	public MissionRow(Context context) {
-		super(context);
-	}
-
-	public MissionRow(MissionFragment missionFragment, waypoint waypoint) {
-		super(missionFragment.getActivity());
-		this.fragment = missionFragment;
-		this.context = missionFragment.getActivity();
-		this.waypoint = waypoint;
-		createRowViews(context);
-		update();
-	}
-
-	public void update() {
-		altitudeView.setText(String.format(Locale.ENGLISH,"%3fm",waypoint.getHeight()));
-		nameView.setText("WP " + waypoint.getNumber());
-		cmdView.setText(waypoint.getCmd().getName());
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		//createRowViews(parent.getContext());
+		// update();
+		return super.getView(position, layout, parent);
 	}
 
 	private void createRowViews(Context context) {
+		layout = new LinearLayout(context);
 		nameView = new TextView(context);
 		altitudeView = new TextView(context);
 		cmdView = new TextView(context);
@@ -61,55 +55,46 @@ public class MissionRow extends TableRow implements OnClickListener,
 		altitudeView.setWidth(80);
 
 		// Listeners
-		cmdView.setOnClickListener(this);
-		altitudeView.setOnClickListener(this);
-		removeButton.setOnClickListener(this);
+		/*
+		 * cmdView.setOnClickListener(this);
+		 * altitudeView.setOnClickListener(this);
+		 * removeButton.setOnClickListener(this);
+		 */
 
-		addView(nameView);
-		addView(cmdView);
-		addView(altitudeView);
-		addView(removeButton);
+		layout.addView(nameView);
+		layout.addView(cmdView);
+		layout.addView(altitudeView);
+		layout.addView(removeButton);
 	}
 
-	@Override
-	public void onClick(View v) {
-		if (v.equals(removeButton)) {
-			onDeleteButtonClick();
-		} else if (v.equals(altitudeView)) {
-			onAltitudeClick();
-		} else if (v.equals(cmdView)) {
-			onCmdClick();
-		}
-	}
-
-	private void onDeleteButtonClick() {
-		fragment.onDeleteWaypoint(waypoint);
-	}
-
-	private void onAltitudeClick() {
-		AltitudeDialog dialog = new AltitudeDialog(this);
-		dialog.build(waypoint.getHeight(), context);
-	}
-
-	private void onCmdClick() {
-		final String[] list = new String[ApmCommands.getNameList().size()];
-		ApmCommands.getNameList().toArray(list);
-		AlertDialog.Builder dialog = new AlertDialog.Builder(context);
-		dialog.setTitle("Select type");
-		dialog.setItems(list, new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface d, int i) {
-				waypoint.setCmd(ApmCommands.getCmd(list[i]));
-				update();
-				fragment.onWaypointUpdate(waypoint);
-			}
-		});
-		dialog.create().show();
-	}
-
-	@Override
-	public void onAltitudeChanged(double newAltitude) {
-		waypoint.setHeight(newAltitude);
-		update();
-	}
+	/*
+	 * 
+	 * public void update() {
+	 * altitudeView.setText(String.format(Locale.ENGLISH,"%3fm"
+	 * ,waypoint.getHeight())); nameView.setText("WP " + waypoint.getNumber());
+	 * cmdView.setText(waypoint.getCmd().getName()); }
+	 * 
+	 * @Override public void onClick(View v) { if (v.equals(removeButton)) {
+	 * onDeleteButtonClick(); } else if (v.equals(altitudeView)) {
+	 * onAltitudeClick(); } else if (v.equals(cmdView)) { onCmdClick(); } }
+	 * 
+	 * private void onDeleteButtonClick() { fragment.onDeleteWaypoint(waypoint);
+	 * }
+	 * 
+	 * private void onAltitudeClick() { AltitudeDialog dialog = new
+	 * AltitudeDialog(this); dialog.build(waypoint.getHeight(), context); }
+	 * 
+	 * private void onCmdClick() { final String[] list = new
+	 * String[ApmCommands.getNameList().size()];
+	 * ApmCommands.getNameList().toArray(list); AlertDialog.Builder dialog = new
+	 * AlertDialog.Builder(context); dialog.setTitle("Select type");
+	 * dialog.setItems(list, new DialogInterface.OnClickListener() {
+	 * 
+	 * @Override public void onClick(DialogInterface d, int i) {
+	 * waypoint.setCmd(ApmCommands.getCmd(list[i])); update();
+	 * fragment.onWaypointUpdate(waypoint); } }); dialog.create().show(); }
+	 * 
+	 * @Override public void onAltitudeChanged(double newAltitude) {
+	 * waypoint.setHeight(newAltitude); update(); }
+	 */
 }
