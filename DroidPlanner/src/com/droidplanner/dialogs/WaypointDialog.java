d522f80ae519016ac0293e850717a5ccea99900e
Arthur Benemann <arthur.benemann@gmail.com>
2013-08-29 17:20:59 -0300

    Make modifications on the WaypointDialog Stick

--- DroidPlanner/src/com/droidplanner/dialogs/WaypointDialog.java
+++ DroidPlanner/src/com/droidplanner/dialogs/WaypointDialog.java
@@ -4,9 +4,12 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -15,13 +18,15 @@ import com.droidplanner.DroidPlannerApp.OnWaypointUpdateListner;
 import com.droidplanner.R;
 import com.droidplanner.drone.variables.waypoint;
 import com.droidplanner.widgets.SeekBarWithText.SeekBarWithText;
+import com.droidplanner.widgets.SeekBarWithText.SeekBarWithText.OnTextSeekBarChangedListner;
 import com.droidplanner.widgets.spinners.SpinnerSelfSelect;
 
-public class WaypointDialog implements DialogInterface.OnClickListener {
+public class WaypointDialog implements DialogInterface.OnClickListener, OnItemSelectedListener, OnTextSeekBarChangedListner {
 	private OnWaypointUpdateListner listner;
 	private waypoint wp;
 	private SeekBarWithText altitudeSeekBar;
 	private SpinnerSelfSelect typeSpinner;
+	private ApmCommandsAdapter commandAdapter;
 
 	public WaypointDialog(waypoint wp) {
 		this.wp = wp;
@@ -37,7 +42,7 @@ public class WaypointDialog implements DialogInterface.OnClickListener {
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setTitle("Waypoint " + wp.getNumber());
 		builder.setView(buildView(context));
-		builder.setNegativeButton("Cancel", this).setPositiveButton("Ok", this);
+		builder.setPositiveButton("Ok", this);
 		AlertDialog dialog = builder.create();
 		return dialog;
 	}
@@ -49,13 +54,18 @@ public class WaypointDialog implements DialogInterface.OnClickListener {
 
 		typeSpinner = (SpinnerSelfSelect) view
 				.findViewById(R.id.spinnerWaypointType);
-		ApmCommandsAdapter adapter = new ApmCommandsAdapter(context,
+		commandAdapter = new ApmCommandsAdapter(context,
 				android.R.layout.simple_list_item_1, ApmCommands.values());
-		typeSpinner.setAdapter(adapter);
+		typeSpinner.setAdapter(commandAdapter);
+		typeSpinner.setOnItemSelectedListener(this);
+		typeSpinner.setSelection(commandAdapter.getPosition(wp.getCmd()));
+		
 
 		altitudeSeekBar = (SeekBarWithText) view
 				.findViewById(R.id.waypointAltitude);
 		altitudeSeekBar.setValue(wp.getHeight());
+		altitudeSeekBar.setOnChangedListner(this);
+		
 
 		return view;
 
@@ -68,6 +78,22 @@ public class WaypointDialog implements DialogInterface.OnClickListener {
 		}
 	}
 
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View v, int position,
+			long id) {
+		Log.d("Q", "selected"+commandAdapter.getItem(position).getName());
+		wp.setCmd(commandAdapter.getItem(position));
+	}
+	
+	@Override
+	public void onSeekBarChanged() {
+		wp.setHeight(altitudeSeekBar.getValue());
+	}
+	
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {	
+	}
+	
 	class ApmCommandsAdapter extends ArrayAdapter<ApmCommands> {
 
 		public ApmCommandsAdapter(Context context, int resource,
@@ -89,5 +115,7 @@ public class WaypointDialog implements DialogInterface.OnClickListener {
 		}
 
 	}
+
+
 
 }
