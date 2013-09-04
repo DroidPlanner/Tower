17290608409f50f1fb400fa0754c018f69f25995
Arthur Benemann <arthur.benemann@gmail.com>
2013-09-04 18:01:04 -0300

    Error handling for small paths.

--- DroidPlanner/src/com/droidplanner/helpers/geoTools/Dubins/Dubins.java
+++ DroidPlanner/src/com/droidplanner/helpers/geoTools/Dubins/Dubins.java
@@ -9,7 +9,7 @@ import com.google.android.gms.maps.model.LatLng;
 public class Dubins {
 
 	private double radius;
-	private List<LatLng> result;	
+	private List<LatLng> result;
 
 	public Dubins(double radius) {
 		this.radius = radius;
@@ -17,23 +17,35 @@ public class Dubins {
 
 	public List<LatLng> generate(List<LatLng> points) {
 		result = new ArrayList<LatLng>();
-		result.add(points.get(0));		
-		processPath(points);		
-		result.add(points.get(points.size()-1));		
+		result.add(points.get(0));
+		processPath(points);
+		result.add(points.get(points.size() - 1));
 		return result;
 	}
 
 	private void processPath(List<LatLng> points) {
+		try {
+			tryToProcessPath(points);
+		} catch (Exception e) {
+			result.add(points.get(1));
+		}
+	}
+
+	private void tryToProcessPath(List<LatLng> points) throws Exception {
+		if (points.size() < 4) {
+			throw new Exception("Not enough points");
+		}
 		for (int i = 3; i < points.size(); i++) {
-			LineLatLng start = new LineLatLng(points.get(i-3), points.get(i-2));
-			LineLatLng end = new LineLatLng(points.get(i-1), points.get(i));
+			LineLatLng start = new LineLatLng(points.get(i - 3),
+					points.get(i - 2));
+			LineLatLng end = new LineLatLng(points.get(i - 1), points.get(i));
 			processPathSegment(start, end);
 		}
 	}
 
 	private void processPathSegment(LineLatLng start, LineLatLng end) {
 		Path shortestPath = Path.findShortestPath(start, end, radius);
-		
+
 		result.addAll(shortestPath.generatePoints());
 	}
 }
