df97495d9b0103850fbaecdec56c0985f14f9a1c
Arthur Benemann <arthur.benemann@gmail.com>
2013-09-25 15:35:09 -0300

    Adding some of the already designed layouts

--- DroidPlanner/src/com/droidplanner/activitys/NewUIActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/NewUIActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 
 import com.droidplanner.R;
 import com.droidplanner.activitys.helpers.SuperActivity;
+import com.droidplanner.drone.variables.waypoint;
+import com.droidplanner.fragments.helpers.OnMapInteractionListener;
+import com.droidplanner.polygon.PolygonPoint;
+import com.google.android.gms.maps.model.LatLng;
 
-public class NewUIActivity extends SuperActivity {
+public class NewUIActivity extends SuperActivity implements OnMapInteractionListener {
 
 	@Override
 	public int getNavigationItem() {
@@ -16,6 +20,36 @@ public class NewUIActivity extends SuperActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_newui);
+	}
+
+	@Override
+	public void onAddPoint(LatLng point) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMoveHome(LatLng coord) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMoveWaypoint(waypoint waypoint, LatLng latLng) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMovePolygonPoint(PolygonPoint source, LatLng newCoord) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMapClick(LatLng point) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
