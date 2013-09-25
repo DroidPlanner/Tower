ab4a60387f7749d02b2fbcbf06f3a702e8cef656
Arthur Benemann <arthur.benemann@gmail.com>
2013-09-25 17:48:14 -0300

    Extracting the planning menus to a new fragment

--- DroidPlanner/src/com/droidplanner/activitys/NewUIActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/NewUIActivity.java
@@ -1,8 +1,6 @@
 package com.droidplanner.activitys;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +8,7 @@ import android.view.MenuItem;
 import com.droidplanner.R;
 import com.droidplanner.activitys.helpers.SuperActivity;
 import com.droidplanner.drone.variables.waypoint;
-import com.droidplanner.fragments.FlightMapFragment;
+import com.droidplanner.fragments.PlanningFragment;
 import com.droidplanner.fragments.RCFragment;
 import com.droidplanner.fragments.helpers.OnMapInteractionListener;
 import com.droidplanner.polygon.PolygonPoint;
@@ -20,6 +18,7 @@ public class NewUIActivity extends SuperActivity implements
 		OnMapInteractionListener {
 	private FragmentManager fragmentManager;
 	private RCFragment rcFragment;
+	private PlanningFragment planningFragment;
 
 	@Override
 	public int getNavigationItem() {
@@ -47,6 +46,7 @@ public class NewUIActivity extends SuperActivity implements
 			toggleRCFragment();
 			return true;
 		case R.id.menu_ui_b2:
+			togglePlanningFragment();
 			return true;
 		}
 		return super.onMenuItemSelected(featureId, item);
@@ -56,10 +56,21 @@ public class NewUIActivity extends SuperActivity implements
 		if (rcFragment == null) {
 			rcFragment = new RCFragment();
 			fragmentManager.beginTransaction()
-					.add(R.id.containerRCFragment, rcFragment).commit();
+					.add(R.id.containerRC, rcFragment).commit();
 		} else {
 			fragmentManager.beginTransaction().remove(rcFragment).commit();
 			rcFragment = null;
+		}
+	}
+	
+	private void togglePlanningFragment() {
+		if (planningFragment == null) {
+			planningFragment = new PlanningFragment();
+			fragmentManager.beginTransaction()
+					.add(R.id.containerPlanning, planningFragment).commit();
+		} else {
+			fragmentManager.beginTransaction().remove(planningFragment).commit();
+			planningFragment = null;
 		}
 	}
 
