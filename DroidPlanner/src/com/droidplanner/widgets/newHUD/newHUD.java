0a44ca55aeb8324464e5fdfb7d7b2279932036f9
Arthur Benemann <arthur.benemann@gmail.com>
2013-10-06 17:54:36 -0300

    Adding the ground/sky to the HUD

--- DroidPlanner/src/com/droidplanner/widgets/newHUD/newHUD.java
+++ DroidPlanner/src/com/droidplanner/widgets/newHUD/newHUD.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Paint.Style;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -14,29 +15,38 @@ public class newHUD extends View {
 	private static final float YAW_ARROW_SIZE = 1.2f;
 	private static final float  YAW_ARROW_ANGLE = 4.5f;
 	private static final float INTERNAL_RADIUS = 0.95f;
-	private Paint yawPaint;
 	private float halfWidth;
 	private float halfHeight;
-	private int yaw = 0;
 	private float radiusExternal;
 	private float radiusInternal;
+	
+	private Paint yawPaint;
 	private Paint skyPaint;
+	private Paint groundPaint;
 
+	private float yaw = 0;
+	private float roll = 30;
+	private RectF groundPoints;
+	
 	public newHUD(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		initialize();
 	}
 
 	private void initialize() {
-		yawPaint = new Paint();
-		yawPaint.setAntiAlias(true);
-		yawPaint.setColor(Color.WHITE);
-		yawPaint.setStyle(Style.FILL);
 		
-		skyPaint = new Paint();
-		skyPaint.setAntiAlias(true);
-		skyPaint.setColor(Color.BLUE);
-		skyPaint.setStyle(Style.FILL);
+		Paint fillPaint = new Paint();
+		fillPaint.setAntiAlias(true);
+		fillPaint.setStyle(Style.FILL);
+		
+		yawPaint = new Paint(fillPaint);
+		yawPaint.setColor(Color.WHITE);
+		
+		skyPaint = new Paint(fillPaint);
+		skyPaint.setColor(Color.parseColor("#008888"));
+		
+		groundPaint = new Paint(fillPaint);
+		groundPaint.setColor(Color.parseColor("#723700"));
 	}
 
 	@Override
@@ -46,6 +56,7 @@ public class newHUD extends View {
 		halfWidth = w / 2f;
 		radiusExternal = Math.min(halfHeight,halfWidth)/YAW_ARROW_SIZE;
 		radiusInternal = radiusExternal*INTERNAL_RADIUS;
+		groundPoints = new RectF(-radiusInternal, -radiusInternal, radiusInternal, radiusInternal);
 	}
 
 	@Override
@@ -55,6 +66,7 @@ public class newHUD extends View {
 		drawYaw(canvas);
 
 		canvas.drawCircle(0, 0, radiusInternal, skyPaint);
+		canvas.drawArc(groundPoints, roll, 180, true, groundPaint);
 	}
 
 	private void drawYaw(Canvas canvas) {
