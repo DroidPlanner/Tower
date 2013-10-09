7a466a5b9d767e0b87aa428539d51267707a3490
Arthur Benemann <arthur.benemann@gmail.com>
2013-10-09 15:01:03 -0300

    Adding up navigation to the app

--- DroidPlanner/src/com/droidplanner/activitys/NewEditorActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/NewEditorActivity.java
@@ -1,15 +1,23 @@
 package com.droidplanner.activitys;
 
+import android.R.anim;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 import com.droidplanner.R;
 import com.droidplanner.drone.variables.waypoint;
 import com.droidplanner.fragments.EditorControlFragment.OnEditorControlInteraction;
 import com.droidplanner.fragments.helpers.OnMapInteractionListener;
 import com.droidplanner.polygon.PolygonPoint;
+import com.google.android.gms.internal.dy.i;
 import com.google.android.gms.maps.model.LatLng;
 
-public class NewEditorActivity extends NewSuperUI implements OnMapInteractionListener,OnEditorControlInteraction {
+public class NewEditorActivity extends NewSuperUI implements
+		OnMapInteractionListener, OnEditorControlInteraction {
+
 	@Override
 	public int getNavigationItem() {
 		return 7;
@@ -19,49 +27,61 @@ public class NewEditorActivity extends NewSuperUI implements OnMapInteractionLis
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_editor);
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
 	public void onAddPoint(LatLng point) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMoveHome(LatLng coord) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMoveWaypoint(waypoint waypoint, LatLng latLng) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMovingWaypoint(waypoint source, LatLng latLng) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMovePolygonPoint(PolygonPoint source, LatLng newCoord) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMapClick(LatLng point) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void editorModeChanged() {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
