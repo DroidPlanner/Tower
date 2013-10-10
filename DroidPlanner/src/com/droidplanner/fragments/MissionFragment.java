d7dc98683baf962d2fe609bac3d3a8c84e6f9e85
Arthur Benemann <arthur.benemann@gmail.com>
2013-10-09 22:51:00 -0300

    Adding a new HorizontalListView widget

--- DroidPlanner/src/com/droidplanner/fragments/MissionFragment.java
+++ DroidPlanner/src/com/droidplanner/fragments/MissionFragment.java
@@ -1,26 +1,20 @@
 package com.droidplanner.fragments;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.droidplanner.DroidPlannerApp.OnWaypointChangedListner;
 import com.droidplanner.R;
-import com.droidplanner.dialogs.mission.DialogMissionFactory;
 import com.droidplanner.drone.variables.Mission;
 import com.droidplanner.drone.variables.waypoint;
 import com.droidplanner.widgets.tableRow.MissionRow;
-import com.mobeta.android.dslv.DragSortListView;
-import com.mobeta.android.dslv.DragSortListView.DragScrollProfile;
-import com.mobeta.android.dslv.DragSortListView.DropListener;
-import com.mobeta.android.dslv.DragSortListView.RemoveListener;
+import com.mobeta.android.dslv.HorizontalListView;
 
-public class MissionFragment extends ListFragment implements DragScrollProfile, RemoveListener, DropListener, OnWaypointChangedListner{
-	public DragSortListView list;
+public class MissionFragment extends Fragment implements  OnWaypointChangedListner{
+	public HorizontalListView list;
 	private Mission mission;
 	private MissionRow adapter;
 
@@ -29,10 +23,10 @@ public class MissionFragment extends ListFragment implements DragScrollProfile, 
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_mission, container,
 				false);
-		list = (DragSortListView) view.findViewById(R.id.listView1);
-		list.setDropListener(this);
-		list.setRemoveListener(this);
-		list.setDragScrollProfile(this);
+		list = (HorizontalListView) view.findViewById(R.id.listView1);
+		//list.setDropListener(this);
+		//list.setRemoveListener(this);
+		//list.setDragScrollProfile(this);
 		
 		adapter = new MissionRow(this.getActivity(), android.R.layout.simple_list_item_1);		
 		list.setAdapter(adapter);		
@@ -42,7 +36,7 @@ public class MissionFragment extends ListFragment implements DragScrollProfile, 
 	public void setMission(Mission mission) {
 		this.mission = mission;
 		adapter = new MissionRow(this.getActivity(), android.R.layout.simple_list_item_1,mission.getWaypoints());
-		setListAdapter(adapter);
+		list.setAdapter(adapter);
 	}
 
 	public void update() {
@@ -50,37 +44,13 @@ public class MissionFragment extends ListFragment implements DragScrollProfile, 
 		adapter.notifyDataSetChanged();
 	}
 	
-	@Override
-	public void drop(int from, int to) {
-		waypoint item=adapter.getItem(from);
-        adapter.remove(item);
-        adapter.insert(item, to);
-        mission.reNumberWaypoints();
-        adapter.notifyDataSetChanged();
-        mission.onWaypointsUpdate();		
-	}
-
-	@Override
-	public void remove(int which) {
-		mission.removeWaypoint(adapter.getItem(which));
-	}
-
-	@Override
-	public float getSpeed(float w, long t) {
-        if (w > 0.8f) {
-            // Traverse all views in a millisecond
-            return ((float) adapter.getCount()) / 0.001f;
-        } else {
-            return 5.0f * w;
-        }
-	}
-
+	/*
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		Log.d("T", "touched "+position);
 		DialogMissionFactory.getDialog(adapter.getItem(position), this.getActivity(), mission);		
 		super.onListItemClick(l, v, position, id);
-	}
+	}*/
 
 	@Override
 	public void onWaypointsUpdate() {
