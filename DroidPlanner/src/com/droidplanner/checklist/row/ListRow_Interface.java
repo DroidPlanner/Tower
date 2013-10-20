b03387a11bde6f2eecc3c5cad2fbdd78dfb6bcb9
Azrin Aris <azrin.aris@gmail.com>
2013-10-20 22:24:00 +0800

    ListXMLAdapter
    ListXMLAdapter decends from ExpandableListAdapter
    handle most of the common task of supplying data to
    ExpandableListAdapter

---
+++ DroidPlanner/src/com/droidplanner/checklist/row/ListRow_Interface.java
@@ -0,0 +1,8 @@
+package com.droidplanner.checklist.row;
+
+import android.view.View;
+
+public interface ListRow_Interface {
+    public View getView(View convertView);
+    public int getViewType();
+}
