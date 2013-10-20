bd12dc96885453209ca094f4f768f03c5756beed
Azrin Aris <azrin.aris@gmail.com>
2013-10-20 22:32:47 +0800

    Item Type - CheckBox
    Add:
    1. Layout for Checkbox item
    2. Implementation calss for CheckBox item
    3. Enum for item types

---
+++ DroidPlanner/src/com/droidplanner/checklist/row/ListRow_Type.java
@@ -0,0 +1,11 @@
+package com.droidplanner.checklist.row;
+
+public enum ListRow_Type {
+    CHECKBOX_ROW,
+    VALUE_ROW,
+    TOGGLE_ROW,
+    SWITCH_ROW,
+    RADIO_ROW,
+    SELECT_ROW,
+    LEVEL_ROW
+}
