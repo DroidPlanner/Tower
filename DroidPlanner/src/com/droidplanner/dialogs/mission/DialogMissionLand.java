6d42960296b532b12f45fc26f61e17e80f497155
Arthur Benemann <arthur.benemann@gmail.com>
2013-10-31 14:39:01 -0200

    Creating the Land dialog and item.

--- DroidPlanner/src/com/droidplanner/dialogs/mission/DialogMissionLand.java
+++ DroidPlanner/src/com/droidplanner/dialogs/mission/DialogMissionLand.java
@@ -3,30 +3,32 @@ package com.droidplanner.dialogs.mission;
 import android.view.View;
 
 import com.droidplanner.R;
+import com.droidplanner.drone.variables.mission.waypoints.Land;
 import com.droidplanner.widgets.SeekBarWithText.SeekBarWithText;
 import com.droidplanner.widgets.SeekBarWithText.SeekBarWithText.OnTextSeekBarChangedListner;
 
 public class DialogMissionLand extends DialogMission implements
-	OnTextSeekBarChangedListner {
+		OnTextSeekBarChangedListner {
 	private SeekBarWithText yawSeekBar;
-	
+	private Land item;
+
 	@Override
 	protected int getResource() {
 		return R.layout.dialog_mission_land;
 	}
-	
+
 	protected View buildView() {
-		super.buildView();		
-		yawSeekBar = (SeekBarWithText) view
-			.findViewById(R.id.waypointAngle);
-		yawSeekBar.setValue(wp.missionItem.param4);
+		super.buildView();
+		item = (Land) wp;
+		yawSeekBar = (SeekBarWithText) view.findViewById(R.id.waypointAngle);
+		yawSeekBar.setValue(item.yawAngle);
 		yawSeekBar.setOnChangedListner(this);
-	return view;
-	
+		return view;
+
 	}
 
 	@Override
 	public void onSeekBarChanged() {
-		wp.missionItem.param4 = (float) yawSeekBar.getValue();
+		item.yawAngle = (float) yawSeekBar.getValue();
 	}
 }
---
+++ DroidPlanner/src/com/droidplanner/drone/variables/mission/waypoints/Land.java
@@ -0,0 +1,34 @@
+package com.droidplanner.drone.variables.mission.waypoints;
+
+import android.content.Context;
+
+import com.droidplanner.R;
+import com.droidplanner.dialogs.mission.DialogMission;
+import com.droidplanner.dialogs.mission.DialogMissionLand;
+import com.droidplanner.fragments.markers.MarkerManager.MarkerSource;
+import com.droidplanner.fragments.markers.helpers.MarkerWithText;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+
+public class Land extends GenericWaypoint implements MarkerSource {
+
+	public double yawAngle;
+
+	public Land(LatLng coord, double altitude) {
+		super(coord, altitude);
+	}
+
+	@Override
+	protected BitmapDescriptor getIcon(Context context) {
+		return BitmapDescriptorFactory.fromBitmap(MarkerWithText
+				.getMarkerWithTextAndDetail(R.drawable.ic_wp_map, "text",
+						"detail", context));
+	}
+
+	@Override
+	public DialogMission getDialog() {
+		return new DialogMissionLand();
+	}
+
+}
