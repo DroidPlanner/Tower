ba05d62029686190b40084305f9f19119a5e8976
Azrin Aris <azrin.aris@gmail.com>
2013-11-01 16:29:49 +0800

    LoiterTurns : Added specific MAVLinkMessage codes

--- DroidPlanner/src/com/droidplanner/drone/variables/mission/waypoints/LoiterTurns.java
+++ DroidPlanner/src/com/droidplanner/drone/variables/mission/waypoints/LoiterTurns.java
@@ -1,6 +1,7 @@
 package com.droidplanner.drone.variables.mission.waypoints;
 
 import com.MAVLink.Messages.ardupilotmega.msg_mission_item;
+import com.MAVLink.Messages.enums.MAV_CMD;
 import com.droidplanner.fragments.markers.MarkerManager.MarkerSource;
 import com.droidplanner.fragments.mission.MissionDetailFragment;
 import com.droidplanner.fragments.mission.MissionLoiterNFragment;
@@ -30,13 +31,15 @@ public class LoiterTurns extends Loiter implements MarkerSource {
 
 	@Override
 	public msg_mission_item packMissionItem() {
-		// TODO Auto-generated method stub
-		return null;
+		msg_mission_item mavMsg = super.packMissionItem();
+		mavMsg.command = MAV_CMD.MAV_CMD_NAV_LOITER_TURNS;
+		mavMsg.param1 = (float) getTurns();
+		return mavMsg;
 	}
 
 	@Override
-	public void unpackMAVMessage(msg_mission_item mavMessageItem) {
-		// TODO Auto-generated method stub
-		
+	public void unpackMAVMessage(msg_mission_item mavMsg) {
+		super.unpackMAVMessage(mavMsg);
+		setTurns((int) mavMsg.param1);
 	}
 }
