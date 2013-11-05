3bb33e77637cefccfc92d26a31165a48dd9f5c1b
Arthur Benemann <arthur.benemann@gmail.com>
2013-11-05 13:42:49 -0200

    TUNING: Adding fragments to handle the tuning of the copter

---
+++ DroidPlanner/src/com/droidplanner/fragments/TunningFragment.java
@@ -0,0 +1,22 @@
+package com.droidplanner.fragments;
+
+import com.droidplanner.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TunningFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_tunning, container,
+				false);
+		return view;
+				
+	}
+
+}
