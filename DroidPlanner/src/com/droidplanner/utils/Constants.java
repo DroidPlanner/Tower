af1ba1b24aa195c10ea7cf7b02c42b14416348d5
fhuya <fhuyakou@gmail.com>
2013-12-05 18:09:16 -0500

    fixed ui issue with flight activity on landscape orientation.

--- DroidPlanner/src/com/droidplanner/utils/Constants.java
+++ DroidPlanner/src/com/droidplanner/utils/Constants.java
@@ -22,24 +22,24 @@ public class Constants {
     /*
     Intent actions
      */
-    private static final String ACTION_PREFIX = PACKAGE_NAME + ".action.";
+    private static final String PREFIX_ACTION = PACKAGE_NAME + ".action.";
 
-    public static final String ACTION_MENU_DRAWER_LOCK_UPDATE = ACTION_PREFIX +
+    public static final String ACTION_MENU_DRAWER_LOCK_UPDATE = PREFIX_ACTION +
             "MENU_DRAWER_LOCK_UPDATE";
-    public static final String ACTION_CONFIGURATION_TUNING = ACTION_PREFIX +
+    public static final String ACTION_CONFIGURATION_TUNING = PREFIX_ACTION +
             "CONFIGURATION_TUNING";
-    public static final String ACTION_CONFIGURATION_RC = ACTION_PREFIX + "CONFIGURATION_RC";
-    public static final String ACTION_CONFIGURATION_PARAMETERS = ACTION_PREFIX +
+    public static final String ACTION_CONFIGURATION_RC = PREFIX_ACTION + "CONFIGURATION_RC";
+    public static final String ACTION_CONFIGURATION_PARAMETERS = PREFIX_ACTION +
             "CONFIGURATION_PARAMETERS";
-    public static final String ACTION_CONFIGURATION_SETTINGS = ACTION_PREFIX +
+    public static final String ACTION_CONFIGURATION_SETTINGS = PREFIX_ACTION +
             "CONFIGURATION_SETTINGS";
 
 
     /*
     Bundle extras
      */
-    private static final String EXTRA_PREFIX = PACKAGE_NAME + ".extra.";
-    public static final String EXTRA_MENU_DRAWER_LOCK = EXTRA_PREFIX + "MENU_DRAWER_LOCK";
+    private static final String PREFIX_EXTRA = PACKAGE_NAME + ".extra.";
+    public static final String EXTRA_MENU_DRAWER_LOCK = PREFIX_EXTRA + "MENU_DRAWER_LOCK";
 
     /**
      * Private constructor to prevent instantiation of this class.
