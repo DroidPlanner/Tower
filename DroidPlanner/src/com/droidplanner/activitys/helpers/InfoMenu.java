060a1658ed3de2cdcdec4d2d23089e6b03bb8fe8
Azrin Aris <azrin.aris@gmail.com>
2013-12-07 15:14:29 +0800

    InfoMenu: Read/Write implementation
    Add:
    1. New argument to InfoMenu constructor (drone, context)
    2. Listener callback methods
    
    SuperUI:
    Supplied new argument to the InfoMenu new constructor

--- DroidPlanner/src/com/droidplanner/activitys/helpers/InfoMenu.java
+++ DroidPlanner/src/com/droidplanner/activitys/helpers/InfoMenu.java
@@ -1,5 +1,6 @@
 package com.droidplanner.activitys.helpers;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,23 +11,30 @@ import com.droidplanner.drone.Drone;
 import com.droidplanner.drone.DroneInterfaces.HomeDistanceChangedListner;
 import com.droidplanner.drone.DroneInterfaces.InfoListner;
 import com.droidplanner.drone.DroneInterfaces.OnStateListner;
+import com.droidplanner.drone.DroneInterfaces.OnWaypointManagerListener;
+import com.droidplanner.drone.variables.mission.WaypointEvent_Type;
 import com.droidplanner.widgets.TimerView;
 import com.droidplanner.widgets.spinners.SelectModeSpinner;
 
 public class InfoMenu implements InfoListner, HomeDistanceChangedListner,
-		OnStateListner {
+		OnStateListner, OnWaypointManagerListener {
 	private Drone drone;
 	private MenuItem battery;
 	private MenuItem gps;
 	private MenuItem propeler;
 	private MenuItem home;
 	private MenuItem signal;
+	private Context context;
+	private ProgressDialog pd;
+
 	public SelectModeSpinner mode;
 	
 	private TimerView timer;
 
-	public InfoMenu(Drone drone) {
+	public InfoMenu(Drone drone, Context context) {
 		this.drone = drone;
+		this.context = context;
+		this.drone.waypointMananger.setWaypointManagerListener(this);
 	}
 
 	public void inflateMenu(Menu menu, MenuInflater menuInflater) {
@@ -102,6 +110,49 @@ public class InfoMenu implements InfoListner, HomeDistanceChangedListner,
 	public void onFailsafeChanged() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void onBeginWaypointEvent(WaypointEvent_Type wpEvent) {
+		if(pd!=null){
+			pd.dismiss();
+			pd = null;
+		}
+		
+		pd = new ProgressDialog(context);
+		switch(wpEvent){
+		case WP_UPLOAD:
+			pd.setTitle(R.string.wpevent_upload);
+			break;
+		case WP_DOWNLOAD:
+			pd.setTitle(R.string.wpevent_download);
+			break;
+		}
+		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		pd.setIndeterminate(true);
+		pd.setCancelable(false);
+		pd.setCanceledOnTouchOutside(true);
+
+		pd.show();
+	}
+
+	@Override
+	public void onWaypointEvent(WaypointEvent_Type wpEvent, int index, int count) {
+		if (pd != null) {
+			if (pd.isIndeterminate()) {
+				pd.setIndeterminate(false);
+				pd.setMax(count);
+			}
+			pd.setProgress(index);
+		}
+	}
+
+	@Override
+	public void onEndWaypointEvent(WaypointEvent_Type wpEvent) {
+		if (pd != null) {
+			pd.dismiss();
+			pd = null;
+		}		
 	}	
 
 
