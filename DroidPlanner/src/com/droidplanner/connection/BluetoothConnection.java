d4db615d6c448ba8460fc0350378091c971257dd
fhuya <fhuyakou@gmail.com>
2013-12-11 13:39:37 -0500

    refactored bluetooth fixes from master. needs testing.  code cleanup.

--- DroidPlanner/src/com/droidplanner/connection/BluetoothConnection.java
+++ DroidPlanner/src/com/droidplanner/connection/BluetoothConnection.java
@@ -15,47 +15,67 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 public class BluetoothConnection extends MAVLinkConnection {
-	private static final String BLUE = "BLUETOOTH";
-	private static final String UUID_SPP_DEVICE = "00001101-0000-1000-8000-00805F9B34FB";
-	private BluetoothAdapter mBluetoothAdapter;
-	private OutputStream out;
-	private InputStream in;
-	private BluetoothSocket bluetoothSocket;
+    private static final String BLUE = "BLUETOOTH";
+    private static final String UUID_SPP_DEVICE = "00001101-0000-1000-8000-00805F9B34FB";
 
-	public BluetoothConnection(Context parentContext) {
-		super(parentContext);
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
-			Log.d(BLUE, "Null adapters");
-		}
-	}
+    /**
+     * Keeps the list of valid uuids the client should be looking for.
+     *
+     * @since 1.2.0
+     */
+    private static final Set<UUID> sValidUuids = new HashSet<UUID>(BluetoothServer.UUIDS.length +
+            1);
 
-	@Override
-	protected void openConnection() throws IOException {
-		Log.d(BLUE, "Connect");
+    static {
+        for (UUID uuid : BluetoothServer.UUIDS) {
+            sValidUuids.add(uuid);
+        }
+        sValidUuids.add(UUID.fromString(UUID_SPP_DEVICE));
+    }
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private OutputStream out;
+    private InputStream in;
+    private BluetoothSocket bluetoothSocket;
+
+    public BluetoothConnection(Context parentContext) {
+        super(parentContext);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Log.d(BLUE, "Null adapters");
+        }
+    }
+
+    @Override
+    protected void openConnection() throws IOException {
+        Log.d(BLUE, "Connect");
 
         //Retrieve the stored address
-        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(parentContext);
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences
+                (parentContext);
         String address = settings.getString(Constants.PREF_BLUETOOTH_DEVICE_ADDRESS, null);
 
-		BluetoothDevice device = address == null ? findBluetoothDevice(): mBluetoothAdapter
-                .getRemoteDevice(address);
+        BluetoothDevice device = address == null
+                ? findSerialBluetoothBoard()
+                : mBluetoothAdapter.getRemoteDevice(address);
 
-        List<UUID> uuidList = new ArrayList<UUID>();
-        uuidList.add(UUID.fromString(UUID_SPP_DEVICE));
-        for(UUID uuid : BluetoothServer.UUIDS){
-            uuidList.add(uuid);
-        }
-
-        for (UUID uuid : uuidList) {
+        for (UUID uuid : sValidUuids) {
             try {
-                bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid);
+                /*
+                If the current uuid is equals to the serial board uuid,
+                then connect using an insecure connection.
+                Otherwise we're most likely talking to another android device acting as a relay
+                server, so connect using a secure connection.
+                 */
+                bluetoothSocket = uuid.toString().equals(UUID_SPP_DEVICE)
+                        ? device.createInsecureRfcommSocketToServiceRecord(uuid)
+                        : device.createRfcommSocketToServiceRecord(uuid);
+
                 mBluetoothAdapter.cancelDiscovery();
                 bluetoothSocket.connect();
 
@@ -69,60 +89,56 @@ public class BluetoothConnection extends MAVLinkConnection {
             }
         }
 
-        if (out == null || in == null){
+        if (out == null || in == null) {
             throw new IOException("Bluetooth socket connect failed.");
         }
-	}
+    }
 
-	@SuppressLint("NewApi")
-	private BluetoothDevice findBluetoothDevice() throws UnknownHostException {
-		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-		// If there are paired devices
-		if (pairedDevices.size() > 0) {
-			// Loop through paired devices
-			for (BluetoothDevice device : pairedDevices) {
-				// Add the name and address to an array adapter to show in a
-				// ListView
-				Log.d(BLUE, device.getName() + " #" + device.getAddress() + "#");
-				for (ParcelUuid id : device.getUuids()) {
-					// TODO maybe this will not work on newer devices
-					Log.d(BLUE, "id:" + id.toString());
-					if (id.toString().equalsIgnoreCase(UUID_SPP_DEVICE)) {
-						return device;
-					}
-				}
-			}
-		}
-		throw new UnknownHostException("No Bluetooth Device found");
-	}
+    @SuppressLint("NewApi")
+    private BluetoothDevice findSerialBluetoothBoard() throws UnknownHostException {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a
+                // ListView
+                Log.d(BLUE, device.getName() + " #" + device.getAddress() + "#");
+                for (ParcelUuid id : device.getUuids()) {
+                    // TODO maybe this will not work on newer devices
+                    Log.d(BLUE, "id:" + id.toString());
+                    if (id.toString().equalsIgnoreCase(UUID_SPP_DEVICE)) {
+                        Log.d(BLUE, ">> Selected: " + device.getName()
+                                + " Using: " + id.toString());
+                        return device;
+                    }
+                }
+            }
+        }
+        throw new UnknownHostException("No Bluetooth Device found");
+    }
 
-	@Override
-	protected void readDataBlock() throws IOException {
-		iavailable = in.read(readData);
+    @Override
+    protected void readDataBlock() throws IOException {
+        iavailable = in.read(readData);
 
-	}
+    }
 
-	@Override
-	protected void sendBuffer(byte[] buffer) throws IOException {
-		if (out != null) {
-			out.write(buffer);
-		}
-	}
+    @Override
+    protected void sendBuffer(byte[] buffer) throws IOException {
+        if (out != null) {
+            out.write(buffer);
+        }
+    }
 
-	@Override
-	protected void closeConnection() throws IOException {
-		bluetoothSocket.close();
-	}
+    @Override
+    protected void closeConnection() throws IOException {
+        bluetoothSocket.close();
+        Log.d(BLUE, "## BT Closed ##");
+    }
 
-	@Override
-	protected void getPreferences(SharedPreferences prefs) {
-		// TODO Auto-generated method stub
-	}
-
-	/*
-	 * private getUUID(device: BluetoothDevice) = { val uuids =
-	 * Option(device.getUuids).getOrElse(Array()).map(_.getUuid) uuids.find {
-	 * uuid => uuid.toString.startsWith(serialUUIDprefix) } }
-	 */
-
+    @Override
+    protected void getPreferences(SharedPreferences prefs) {
+        // TODO Auto-generated method stub
+    }
 }
