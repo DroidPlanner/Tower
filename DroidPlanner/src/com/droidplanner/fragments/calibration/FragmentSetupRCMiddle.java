a6e7b04cd5f2a3e571376a2bfa4fdf9ed38d9acf
Azrin Aris <azrin.aris@gmail.com>
2013-12-24 07:41:12 +0800

    FRagmentSetupRCMiddle: Cancle button code implementation

--- DroidPlanner/src/com/droidplanner/fragments/calibration/FragmentSetupRCMiddle.java
+++ DroidPlanner/src/com/droidplanner/fragments/calibration/FragmentSetupRCMiddle.java
@@ -14,21 +14,31 @@ import android.widget.Button;
 public class FragmentSetupRCMiddle extends Fragment implements OnClickListener{
 	public RcSetupFragment rcSetupFragment;
 	private Button btnNext;
+	private Button btnCancel;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_setup_rc_middle, container,
 				false);
-		btnNext = (Button)view.findViewById(R.id.buttonRCCancel);
+		btnNext = (Button)view.findViewById(R.id.ButtonNext);
 		btnNext.setOnClickListener(this);
+		
+		btnCancel = (Button)view.findViewById(R.id.ButtonCancel);
+		btnCancel.setOnClickListener(this);
+
 		return view;
 	}
 
 	@Override
 	public void onClick(View arg0) {
 		if(rcSetupFragment!=null){
-			rcSetupFragment.changeSetupPanel(3);
+			if(arg0.equals(btnNext)){
+				rcSetupFragment.changeSetupPanel(3);
+			}
+			else if(arg0.equals(btnCancel)) {
+				rcSetupFragment.cancel();
+			}
 		}
 	}
 
