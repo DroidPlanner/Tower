1b6cabda8a7cf62b9cb68fe3c2fdb938c0a579ed
Arthur Benemann <arthur.benemann@gmail.com>
2014-01-13 19:54:03 -0200

    TDD: changing the assert parameters order to match the jUnit convention

--- DroidPlannerTests/src/org/droidplanner/helpers/units/LenghtTest.java
+++ DroidPlannerTests/src/org/droidplanner/helpers/units/LenghtTest.java
@@ -1,52 +1,52 @@
 package org.droidplanner.helpers.units;
 
-import junit.framework.*;
+import junit.framework.TestCase;
 
 public class LenghtTest extends TestCase {
 	private static final double TOLERANCE = 1e-8;
 
-	private Length lenght;
+	private Length length;
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		lenght = new Length(1.0);
+		length = new Length(1.0);
 	}
 
 	public void testValueInMeters() {
-		assertEquals(lenght.valueInMeters(), 1, TOLERANCE);
+		assertEquals(1, length.valueInMeters(), TOLERANCE);
 	}
 
 	public void testSet() {
-		lenght.set(2);
-		assertEquals(lenght.valueInMeters(), 2, TOLERANCE);
+		length.set(2);
+		assertEquals(2, length.valueInMeters(), TOLERANCE);
 	}
 
 	public void testToString() {
-		lenght.set(1.0);
-		assertEquals(lenght.toString(), "1.0 m");
-		lenght.set(1.234);
-		assertEquals(lenght.toString(), "1.2 m");
-		lenght.set(10);
-		assertEquals(lenght.toString(), "10.0 m");
-		
-		lenght.set(1000);
-		assertEquals(lenght.toString(), "1.0 km");
-		lenght.set(1234);
-		assertEquals(lenght.toString(), "1.2 km");
-		
-		lenght.set(0.001);
-		assertEquals(lenght.toString(), "1.0 mm");
-		lenght.set(0.01234);
-		assertEquals(lenght.toString(), "12.3 mm");
-		
-		lenght.set(1e-6);
-		assertEquals(lenght.toString(), "1.0E-6 m");
+		length.set(1.0);
+		assertEquals("1.0 m", length.toString());
+		length.set(1.234);
+		assertEquals("1.2 m", length.toString());
+		length.set(10);
+		assertEquals("10.0 m", length.toString());
+
+		length.set(1000);
+		assertEquals("1.0 km", length.toString());
+		length.set(1234);
+		assertEquals("1.2 km", length.toString());
+
+		length.set(0.001);
+		assertEquals("1.0 mm", length.toString());
+		length.set(0.01234);
+		assertEquals("12.3 mm", length.toString());
+
+		length.set(1e-6);
+		assertEquals("1.0E-6 m", length.toString());
 	}
-	
-	public void testEquals(){
-		assertTrue(lenght.equals(new Length(1.0)));
-		assertFalse(lenght.equals(new Length(2.0)));
-		assertFalse(lenght.equals(new Object()));
+
+	public void testEquals() {
+		assertTrue(length.equals(new Length(1.0)));
+		assertFalse(length.equals(new Length(2.0)));
+		assertFalse(length.equals(new Object()));
 	}
 
 }
