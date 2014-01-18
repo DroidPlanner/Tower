996a02e793d81dd384cf6cf6740e26aca70feb53
Azrin Aris <azrin.aris@gmail.com>
2014-01-18 12:16:47 +0800

    Up Down button: code implementation
    normal press will increment the value by the value set to ‘inc’
    long press of 1 sec will automatically continuously increment the value

--- DroidPlanner/src/org/droidplanner/widgets/NumberFieldEdit/NumberFieldEdit.java
+++ DroidPlanner/src/org/droidplanner/widgets/NumberFieldEdit/NumberFieldEdit.java
@@ -2,9 +2,13 @@ package org.droidplanner.widgets.NumberFieldEdit;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Handler;
 import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -12,7 +16,7 @@ import android.widget.TextView;
 
 import org.droidplanner.R;
 
-public class NumberFieldEdit extends LinearLayout {
+public class NumberFieldEdit extends LinearLayout implements OnTouchListener {
 
 	private TextView textView;
 	private EditText editText;
@@ -26,6 +30,9 @@ public class NumberFieldEdit extends LinearLayout {
 	private String title = "Title";
 	private String unit = "";
 	private String formatString = "%2.1f";
+	private short delay = 1000;
+	private boolean countUp;
+	private boolean waitingForLongPress = false;
 
 	public NumberFieldEdit(Context context) {
 		super(context);
@@ -52,57 +59,63 @@ public class NumberFieldEdit extends LinearLayout {
 	}
 
 	private void setFormat(String string) {
-		if (string!=null) {
+		if (string != null) {
 			formatString = string;
-		}		
+		}
 	}
 
 	private void createViews(Context context) {
 		setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
 				LayoutParams.WRAP_CONTENT));
 		setOrientation(HORIZONTAL);
-		
+
 		textView = new TextView(context);
 		editText = new EditText(context);
 		buttonPlus = new ImageButton(context);
 		buttonMinus = new ImageButton(context);
 		buttonLayout = new LinearLayout(context);
 
-		textView.setGravity(Gravity.LEFT|Gravity.CENTER_VERTICAL);
-		editText.setGravity(Gravity.RIGHT|Gravity.CENTER_VERTICAL);
-		
+		textView.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+		editText.setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
+
 		textView.setTextSize(16);
 		editText.setTextSize(16);
-		
-		textView.setLayoutParams(new LayoutParams(0,LayoutParams.MATCH_PARENT,5));
-		editText.setLayoutParams(new LayoutParams(0,LayoutParams.MATCH_PARENT,5));
-		buttonLayout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+
+		textView.setLayoutParams(new LayoutParams(0, LayoutParams.MATCH_PARENT,
+				5));
+		editText.setLayoutParams(new LayoutParams(0, LayoutParams.MATCH_PARENT,
+				5));
+		buttonLayout.setLayoutParams(new LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 		buttonLayout.setFocusable(true);
 		buttonLayout.setFocusableInTouchMode(true);
-		
-		editText.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+		editText.setInputType(InputType.TYPE_CLASS_NUMBER
+				| InputType.TYPE_NUMBER_FLAG_DECIMAL);
 		editText.clearFocus();
 
-		
-		LayoutParams p =new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+		LayoutParams p = new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
 		p.setMargins(5, 0, 0, 0);
 
 		buttonPlus.setBackgroundResource(R.drawable.button_mode_options);
 		buttonPlus.setImageResource(R.drawable.ic_plus);
 		buttonPlus.setLayoutParams(p);
 
-		
 		buttonMinus.setBackgroundResource(R.drawable.button_mode_options);
 		buttonMinus.setImageResource(R.drawable.ic_minus);
 		buttonMinus.setLayoutParams(p);
-		
+
+		buttonPlus.setOnTouchListener(this);
+		buttonMinus.setOnTouchListener(this);
+
 		buttonLayout.addView(buttonPlus);
 		buttonLayout.addView(buttonMinus);
 
 		addView(textView);
 		addView(editText);
 		addView(buttonLayout);
-		
+
 		editText.clearFocus();
 		buttonLayout.requestFocus();
 	}
@@ -129,7 +142,7 @@ public class NumberFieldEdit extends LinearLayout {
 
 	private void updateTitle() {
 		String t = title;
-		t+= unit.isEmpty()?"":String.format(" (%s)",unit);
+		t += unit.isEmpty() ? "" : String.format(" (%s)", unit);
 		textView.setText(t);
 	}
 
@@ -138,14 +151,57 @@ public class NumberFieldEdit extends LinearLayout {
 	}
 
 	public void setValue(double value) {
-		this.value = value<min?min:value;
-		this.value = value>max?max:value;
-		editText.setText(String.format(formatString, value));
+		if (value >= max)
+			value = max;
+		else if (value <= min)
+			value = min;
+
+		this.value = value;
+		editText.setText(String.format(formatString, this.value));
 	}
 
 	public void setAbsValue(double value) {
-		if(value<0)
+		if (value < 0)
 			value *= -1.0;
-		editText.setText(String.format(formatString, value));
+
+		this.value = value;
+		editText.setText(String.format(formatString, this.value));
 	}
+
+	private void updateValue() {
+		if (countUp)
+			setValue(this.value + inc);
+		else
+			setValue(this.value - inc);
+	}
+
+	final Handler handler = new Handler();
+	Runnable mLongPressed = new Runnable() {
+		public void run() {
+			delay = 50;
+			updateValue();
+			handler.postDelayed(this, delay);
+		}
+	};
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		if (v.equals(buttonMinus) || v.equals(buttonPlus)) {
+			countUp = v.equals(buttonPlus);
+
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
+				if (!waitingForLongPress ) {
+					waitingForLongPress = true;
+					handler.postDelayed(mLongPressed, delay);
+				}
+			} else if (event.getAction() == MotionEvent.ACTION_UP) {
+				waitingForLongPress = false;
+				handler.removeCallbacks(mLongPressed);
+				delay = 1000;
+				updateValue();
+			}
+		}
+		return super.onTouchEvent(event);
+	}
+
 }
