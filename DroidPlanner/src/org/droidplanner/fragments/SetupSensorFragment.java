8079c5efcb282c6dedcff18024a415502e89821c
Azrin Aris <azrin.aris@gmail.com>
2014-01-18 19:11:58 +0800

    Refactoring: Extend SuperSetupFragment to SetupSensorFragment
    This fragment will handle all sensor calibration activities

---
+++ DroidPlanner/src/org/droidplanner/fragments/SetupSensorFragment.java
@@ -0,0 +1,46 @@
+package org.droidplanner.fragments;
+
+import org.droidplanner.fragments.helpers.SuperSetupFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * This fragment is used to calibrate the drone's compass, and accelerometer.
+ */
+public class SetupSensorFragment extends SuperSetupFragment {
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+	}
+
+    @Override
+    public void onDetach(){
+        super.onDetach();    }
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
+
+}
