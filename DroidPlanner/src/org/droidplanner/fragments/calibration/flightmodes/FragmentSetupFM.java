94550d64631704a72d051a0c8dc04721e954ea99
Azrin Aris <azrin.aris@gmail.com>
2014-01-19 17:17:12 +0800

    Flight mode parameters: create parameter handler for Flight Mode

--- DroidPlanner/src/org/droidplanner/fragments/calibration/flightmodes/FragmentSetupFM.java
+++ DroidPlanner/src/org/droidplanner/fragments/calibration/flightmodes/FragmentSetupFM.java
@@ -1,11 +1,13 @@
 package org.droidplanner.fragments.calibration.flightmodes;
 
+import org.droidplanner.calibration.FM_CalParameters;
 import org.droidplanner.drone.Drone;
 import org.droidplanner.drone.DroneInterfaces.DroneEventsType;
 import org.droidplanner.drone.DroneInterfaces.OnDroneListner;
 import org.droidplanner.fragments.SetupRadioFragment;
 import org.droidplanner.fragments.calibration.SetupMainPanel;
 import org.droidplanner.fragments.calibration.SetupSidePanel;
+import org.droidplanner.widgets.spinners.SpinnerSelfSelect.OnSpinnerItemSelectedListener;
 import org.droidplanner.R;
 
 import android.app.Activity;
@@ -17,22 +19,24 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class FragmentSetupFM extends SetupMainPanel implements OnDroneListner{
+public class FragmentSetupFM extends SetupMainPanel implements OnDroneListner {
 
 	private int[] pwm = { 1230, 1360, 1490, 1620, 1750 };
-	private int[] flightModeValue = {1,2,3,4,5,6,7,8,9,10,11,13,14};
-	
+	private int[] flightModeValue = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13 };
+	private int[] flightModeIndex = { 1, 2, 4, 8, 16, 32 };
+	private double dataFM[] = new double[8];
+
 	private String[] listPWM;
 	private CheckBox[] chkbxSimple = new CheckBox[6];
-	private CheckBox[] chkbxSuperSimple = new CheckBox[6];;	
+	private CheckBox[] chkbxSuperSimple = new CheckBox[6];;
 	private Spinner[] pwmSpinners = new Spinner[6];;
 	private TextView[] textPWM = new TextView[6];
 	private LinearLayout[] layoutPWM = new LinearLayout[6];
-	
+
 	private TextView textPWMRange, textPWMCurrent;
-	
+	private FM_CalParameters fmParameters;
 	private Drone drone;
-	
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
@@ -43,6 +47,7 @@ public class FragmentSetupFM extends SetupMainPanel implements OnDroneListner{
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		this.drone = parentActivity.drone;
+		fmParameters = new FM_CalParameters(drone);
 	}
 
 	@Override
@@ -61,7 +66,7 @@ public class FragmentSetupFM extends SetupMainPanel implements OnDroneListner{
 	public void onDroneEvent(DroneEventsType event, Drone drone) {
 		switch (event) {
 		case RC_IN:
-			updatePWMData();
+			updatePWMPanels();
 			break;
 		case RC_OUT:
 		default:
@@ -81,54 +86,57 @@ public class FragmentSetupFM extends SetupMainPanel implements OnDroneListner{
 
 	@Override
 	public void setupLocalViews(View v) {
-		textPWMRange = (TextView)v.findViewById(R.id.textViewPWMRange);
-		textPWMCurrent = (TextView)v.findViewById(R.id.textViewPWM);
-		
-		textPWM[0] = (TextView)v.findViewById(R.id.textViewFM1);
-		textPWM[1] = (TextView)v.findViewById(R.id.textViewFM2);
-		textPWM[2] = (TextView)v.findViewById(R.id.textViewFM3);
-		textPWM[3] = (TextView)v.findViewById(R.id.textViewFM4);
-		textPWM[4] = (TextView)v.findViewById(R.id.textViewFM5);
-		textPWM[5] = (TextView)v.findViewById(R.id.textViewFM6);
-		
-		pwmSpinners[0] = (Spinner)v.findViewById(R.id.spinnerFM1);
-		pwmSpinners[1] = (Spinner)v.findViewById(R.id.spinnerFM2);
-		pwmSpinners[2] = (Spinner)v.findViewById(R.id.spinnerFM3);
-		pwmSpinners[3] = (Spinner)v.findViewById(R.id.spinnerFM4);
-		pwmSpinners[4] = (Spinner)v.findViewById(R.id.spinnerFM5);
-		pwmSpinners[5] = (Spinner)v.findViewById(R.id.spinnerFM6);
-		
-		chkbxSimple[0] = (CheckBox)v.findViewById(R.id.checkBoxFM1);
-		chkbxSimple[1] = (CheckBox)v.findViewById(R.id.checkBoxFM2);
-		chkbxSimple[2] = (CheckBox)v.findViewById(R.id.checkBoxFM3);
-		chkbxSimple[3] = (CheckBox)v.findViewById(R.id.checkBoxFM4);
-		chkbxSimple[4] = (CheckBox)v.findViewById(R.id.checkBoxFM5);
-		chkbxSimple[5] = (CheckBox)v.findViewById(R.id.checkBoxFM6);
-		
-		chkbxSuperSimple[0] = (CheckBox)v.findViewById(R.id.checkBoxFMS1);
-		chkbxSuperSimple[1] = (CheckBox)v.findViewById(R.id.checkBoxFMS2);
-		chkbxSuperSimple[2] = (CheckBox)v.findViewById(R.id.checkBoxFMS3);
-		chkbxSuperSimple[3] = (CheckBox)v.findViewById(R.id.checkBoxFMS4);
-		chkbxSuperSimple[4] = (CheckBox)v.findViewById(R.id.checkBoxFMS5);
-		chkbxSuperSimple[5] = (CheckBox)v.findViewById(R.id.checkBoxFMS6);
+		textPWMRange = (TextView) v.findViewById(R.id.textViewPWMRange);
+		textPWMCurrent = (TextView) v.findViewById(R.id.textViewPWM);
 
-		layoutPWM[0] = (LinearLayout)v.findViewById(R.id.layoutFM1);
-		layoutPWM[1] = (LinearLayout)v.findViewById(R.id.layoutFM2);
-		layoutPWM[2] = (LinearLayout)v.findViewById(R.id.layoutFM3);
-		layoutPWM[3] = (LinearLayout)v.findViewById(R.id.layoutFM4);
-		layoutPWM[4] = (LinearLayout)v.findViewById(R.id.layoutFM5);
-		layoutPWM[5] = (LinearLayout)v.findViewById(R.id.layoutFM6);
+		textPWM[0] = (TextView) v.findViewById(R.id.textViewFM1);
+		textPWM[1] = (TextView) v.findViewById(R.id.textViewFM2);
+		textPWM[2] = (TextView) v.findViewById(R.id.textViewFM3);
+		textPWM[3] = (TextView) v.findViewById(R.id.textViewFM4);
+		textPWM[4] = (TextView) v.findViewById(R.id.textViewFM5);
+		textPWM[5] = (TextView) v.findViewById(R.id.textViewFM6);
+
+		pwmSpinners[0] = (Spinner) v.findViewById(R.id.spinnerFM1);
+		pwmSpinners[1] = (Spinner) v.findViewById(R.id.spinnerFM2);
+		pwmSpinners[2] = (Spinner) v.findViewById(R.id.spinnerFM3);
+		pwmSpinners[3] = (Spinner) v.findViewById(R.id.spinnerFM4);
+		pwmSpinners[4] = (Spinner) v.findViewById(R.id.spinnerFM5);
+		pwmSpinners[5] = (Spinner) v.findViewById(R.id.spinnerFM6);
+
+		chkbxSimple[0] = (CheckBox) v.findViewById(R.id.checkBoxFM1);
+		chkbxSimple[1] = (CheckBox) v.findViewById(R.id.checkBoxFM2);
+		chkbxSimple[2] = (CheckBox) v.findViewById(R.id.checkBoxFM3);
+		chkbxSimple[3] = (CheckBox) v.findViewById(R.id.checkBoxFM4);
+		chkbxSimple[4] = (CheckBox) v.findViewById(R.id.checkBoxFM5);
+		chkbxSimple[5] = (CheckBox) v.findViewById(R.id.checkBoxFM6);
+
+		chkbxSuperSimple[0] = (CheckBox) v.findViewById(R.id.checkBoxFMS1);
+		chkbxSuperSimple[1] = (CheckBox) v.findViewById(R.id.checkBoxFMS2);
+		chkbxSuperSimple[2] = (CheckBox) v.findViewById(R.id.checkBoxFMS3);
+		chkbxSuperSimple[3] = (CheckBox) v.findViewById(R.id.checkBoxFMS4);
+		chkbxSuperSimple[4] = (CheckBox) v.findViewById(R.id.checkBoxFMS5);
+		chkbxSuperSimple[5] = (CheckBox) v.findViewById(R.id.checkBoxFMS6);
+
+		layoutPWM[0] = (LinearLayout) v.findViewById(R.id.layoutFM1);
+		layoutPWM[1] = (LinearLayout) v.findViewById(R.id.layoutFM2);
+		layoutPWM[2] = (LinearLayout) v.findViewById(R.id.layoutFM3);
+		layoutPWM[3] = (LinearLayout) v.findViewById(R.id.layoutFM4);
+		layoutPWM[4] = (LinearLayout) v.findViewById(R.id.layoutFM5);
+		layoutPWM[5] = (LinearLayout) v.findViewById(R.id.layoutFM6);
 
 		setupSpinners();
+
 	}
 
 	private void setupSpinners() {
-		final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(parentActivity,
-		        R.array.FligthMode_CopterV3_1, R.layout.spinner_setup_item);
-		
+		final ArrayAdapter<CharSequence> adapter = ArrayAdapter
+				.createFromResource(parentActivity,
+						R.array.FligthMode_CopterV3_1,
+						R.layout.spinner_setup_item);
+
 		adapter.setDropDownViewResource(R.layout.spinner_setup_item_dropdown);
-		
-		for(Spinner spinner : pwmSpinners){
+
+		for (Spinner spinner : pwmSpinners) {
 			spinner.setAdapter(adapter);
 		}
 	}
@@ -136,41 +144,67 @@ public class FragmentSetupFM extends SetupMainPanel implements OnDroneListner{
 	@Override
 	public void doCalibrationStep(int step) {
 		switch (step) {
-		case 1: 
+		case 1:
 			uploadCalibrationData();
 			break;
-		case 0: 
-			default:
+		case 0:
+		default:
 			sidePanel = getInitialPanel();
 		}
 	}
 
 	private SetupSidePanel getInitialPanel() {
 		sidePanel = ((SetupRadioFragment) getParentFragment())
-				.changeSidePanel(new FragmentSetupFMConfiguration());		
+				.changeSidePanel(new FragmentSetupFMConfiguration());
 		return sidePanel;
 	}
 
 	private void uploadCalibrationData() {
-		// TODO Auto-generated method stub
-		
+		int cnt = 0;
+
+		// Read all spinners value
+		for (Spinner spinner : pwmSpinners) {
+			dataFM[cnt] = spinner.getSelectedItemPosition();
+			cnt++;
+		}
+
+		// read SIMPLE MODE check boxes and create bit value
+		cnt = 0;
+		for (CheckBox chkbx : chkbxSimple) {
+			if (chkbx.isChecked()) {
+				dataFM[6] += flightModeIndex[cnt];
+			}
+			cnt++;
+		}
+
+		// read SUPER SIMPLE MODE check boxes and create bit value
+		cnt = 0;
+		for (CheckBox chkbx : chkbxSuperSimple) {
+			if (chkbx.isChecked()) {
+				dataFM[7] += flightModeIndex[cnt];
+			}
+			cnt++;
+		}
+
 	}
 
-	private void updatePWMData() {
+	private void updatePWMPanels() {
 		int pwmIn = drone.RC.in[4];
 		int pwmId = getPWMRangeIndex(pwmIn);
-		
+
 		textPWMCurrent.setText(String.format("PWM in : %d", pwmIn));
-		textPWMRange.setText("Flight Mode #"+String.valueOf(pwmId+1)+ " ("+ listPWM[pwmId]+ ")");
+		textPWMRange.setText("Flight Mode #" + String.valueOf(pwmId + 1) + " ("
+				+ listPWM[pwmId] + ")");
 		updateLayout(pwmId);
-		
+
 	}
 
 	private void updateLayout(int pwmId) {
 		int cnt = 0;
-		for(LinearLayout layout: layoutPWM){
-			if(cnt==pwmId)
-				layout.setBackgroundColor(getResources().getColor(R.color.air_speed_label));
+		for (LinearLayout layout : layoutPWM) {
+			if (cnt == pwmId)
+				layout.setBackgroundColor(getResources().getColor(
+						R.color.air_speed_label));
 			else
 				layout.setBackgroundColor(0);
 			cnt++;
@@ -194,4 +228,5 @@ public class FragmentSetupFM extends SetupMainPanel implements OnDroneListner{
 
 		return -1;
 	}
+
 }
