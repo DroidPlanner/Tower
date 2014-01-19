f958682dcea850e1e79f68aca5f0d56611f28bf4
Paku- <pawel@4x4.org.pl>
2014-01-19 19:48:24 +0100

    GuidedModeWarning: Dialog added, just an idea for review. No altitude to enter as requested.

--- DroidPlanner/src/org/droidplanner/fragments/FlightMapFragment.java
+++ DroidPlanner/src/org/droidplanner/fragments/FlightMapFragment.java
@@ -2,6 +2,11 @@ package org.droidplanner.fragments;
 
 import java.util.List;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
 import org.droidplanner.drone.Drone;
 import org.droidplanner.drone.DroneInterfaces.DroneEventsType;
 import org.droidplanner.drone.variables.GuidedPoint;
@@ -37,10 +42,14 @@ public class FlightMapFragment extends DroneMap implements
 	private MapPath droneLeashPath;
 	private int maxFlightPathSize;
 	public boolean isAutoPanEnabled;
+	
+	private boolean warnOnGuidedMode;
 
 	public boolean hasBeenZoomed = false;
 
 	public DroneMarker droneMarker;
+	
+	Activity parentActivity;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup,
@@ -66,6 +75,8 @@ public class FlightMapFragment extends DroneMap implements
 		maxFlightPathSize = Integer.valueOf(prefs.getString(
 				"pref_max_fligth_path_size", "0"));
 		isAutoPanEnabled = prefs.getBoolean("pref_auto_pan_enabled", false);
+		warnOnGuidedMode = prefs.getBoolean("pref_warn_guided_mode", true);
+		
 	}
 
 	@Override
@@ -109,10 +120,49 @@ public class FlightMapFragment extends DroneMap implements
 		flightPathOptions.color(0xfffd693f).width(6).zIndex(1);
 		flightPath = mMap.addPolyline(flightPathOptions);
 	}
+	
+	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		//we need true current and directly linked Activity object for AlertDialog - kind of bug - we can not use context here.
+		parentActivity = activity;
+	}	
 
 	@Override
 	public void onMapLongClick(LatLng coord) {
-		drone.guidedPoint.changeGuidedCoordinate(coord);
+		///
+		
+		//we need new and final variable to use it inside the listener below		
+		final LatLng tmpCoord = coord;
+
+		
+		OnClickListener positiveButtonClickListener = new AlertDialog.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				drone.guidedPoint.changeGuidedCoordinate(tmpCoord);
+			}
+		};
+
+		OnClickListener negativeButtonClickListener = new AlertDialog.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		};		
+		
+		getPreferences();
+		if (warnOnGuidedMode){
+			
+			final AlertDialog.Builder dlg = new AlertDialog.Builder(parentActivity);
+			dlg.setTitle("Guided Mode Warning !!!");
+			dlg.setMessage("You are about to enter the Guided Mode");
+			dlg.setCancelable(false);
+			dlg.setPositiveButton("Ok - go Guided", positiveButtonClickListener);
+			dlg.setNegativeButton("Take me back ...", negativeButtonClickListener);
+			dlg.create();
+			dlg.show();									
+		}else{
+			//here var is not so important but use the same for clarity.
+			drone.guidedPoint.changeGuidedCoordinate(tmpCoord);
+		}
 	}
 
 	@Override
