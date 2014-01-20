0753a09301216de6f68e6fb42be44d4036bcca78
Azrin Aris <azrin.aris@gmail.com>
2014-01-20 12:31:42 +0800

    Channel Options: Class implementation

--- DroidPlanner/src/org/droidplanner/fragments/calibration/ch/FragmentSetupCH.java
+++ DroidPlanner/src/org/droidplanner/fragments/calibration/ch/FragmentSetupCH.java
@@ -1,33 +1,77 @@
 package org.droidplanner.fragments.calibration.ch;
 
 import org.droidplanner.R;
+import org.droidplanner.calibration.CH_CalParameters;
+import org.droidplanner.calibration.CalParameters;
+import org.droidplanner.calibration.CalParameters.OnCalibrationEvent;
 import org.droidplanner.drone.Drone;
+import org.droidplanner.fragments.SetupRadioFragment;
+import org.droidplanner.fragments.calibration.FragmentSetupProgress;
+import org.droidplanner.fragments.calibration.FragmentSetupSend;
 import org.droidplanner.fragments.calibration.SetupMainPanel;
 import org.droidplanner.fragments.calibration.SetupSidePanel;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class FragmentSetupCH extends SetupMainPanel {
+public class FragmentSetupCH extends SetupMainPanel implements
+		OnCalibrationEvent {
 
 	private int[] valueCH6;
 	private int[] valueCH;
 	private String[] stringCH6;
 	private String[] stringCH;
 
+	private Spinner spinnerCH6, spinnerCH7, spinnerCH8;
+	private EditText editTuneH, editTuneL;
+
 	private Drone drone;
+	private CH_CalParameters chParameters;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		this.drone = parentActivity.drone;
-		getCH6Options();
-		getCHOptions();
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
+		downloadCalibrationData();
+	}
+
+	@Override
+	public void onReadCalibration(CalParameters calParameters) {
+		doCalibrationStep(0);
+		updatePanelInfo();
+
+	}
+
+	@Override
+	public void onSentCalibration(CalParameters calParameters) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCalibrationData(CalParameters calParameters, int index,
+			int count, boolean isSending) {
+		if (sidePanel != null && chParameters != null) {
+			String title;
+			if (isSending) {
+				title = getResources().getString(
+						R.string.setup_ch_desc_uploading);
+			} else {
+				title = getResources().getString(
+						R.string.setup_ch_desc_downloading);
+			}
+
+			((FragmentSetupProgress) sidePanel).updateProgress(index + 1,
+					count, title);
+		}
 	}
 
 	@Override
@@ -37,20 +81,104 @@ public class FragmentSetupCH extends SetupMainPanel {
 
 	@Override
 	public SetupSidePanel getSidePanel() {
-		// TODO Auto-generated method stub
-		return null;
+		return getInitialPanel();
 	}
 
 	@Override
 	public void setupLocalViews(View v) {
-		// TODO Auto-generated method stub
+		editTuneL = (EditText) v.findViewById(R.id.editTextTuneL);
+		editTuneH = (EditText) v.findViewById(R.id.editTextTuneH);
 
+		spinnerCH6 = (Spinner) v.findViewById(R.id.spinnerCH6);
+		spinnerCH7 = (Spinner) v.findViewById(R.id.spinnerCH7);
+		spinnerCH8 = (Spinner) v.findViewById(R.id.spinnerCH8);
+
+		setupSpinners();
 	}
 
 	@Override
 	public void doCalibrationStep(int step) {
-		// TODO Auto-generated method stub
+		switch (step) {
+		case 1:
+			uploadCalibrationData();
+			break;
+		case 0:
+		default:
+			sidePanel = getInitialPanel();
+		}
+	}
 
+	private SetupSidePanel getInitialPanel() {
+		sidePanel = new FragmentSetupSend();
+		sidePanel.updateTitle(R.string.setup_ch_side_title);
+		sidePanel.updateDescription(R.string.setup_ch_side_desc);
+
+		return sidePanel;
+	}
+
+	private SetupSidePanel getProgressPanel(boolean isSending) {
+		sidePanel = ((SetupRadioFragment) getParentFragment())
+				.changeSidePanel(new FragmentSetupProgress());
+
+		if (isSending) {
+			sidePanel.updateTitle(R.string.progress_title_uploading);
+			sidePanel.updateDescription(R.string.progress_desc_uploading);
+		} else {
+			sidePanel.updateTitle(R.string.progress_title_downloading);
+			sidePanel.updateDescription(R.string.progress_desc_downloading);
+		}
+
+		return sidePanel;
+	}
+
+	private void uploadCalibrationData() {
+		if (chParameters == null || !drone.MavClient.isConnected())
+			return;
+
+		sidePanel = getProgressPanel(true);
+
+		// TODO setParameterValues here
+		chParameters.sendCalibrationParameters();
+	}
+
+	private void downloadCalibrationData() {
+		if (chParameters == null || !drone.MavClient.isConnected())
+			return;
+		sidePanel = getProgressPanel(false);
+		chParameters.getCalibrationParameters(drone);
+	}
+
+	private void updatePanelInfo() {
+		if (chParameters == null)
+			return;
+
+		editTuneL.setText(String.format("%d", chParameters.getParamValueByName("TUNE_LOW")));
+		editTuneH.setText(String.format("%d", chParameters.getParamValueByName("TUNE_HIGH")));
+
+		spinnerCH6.setSelection(getSpinnerIndexFromValue(
+				(int) chParameters.getParamValueByName("TUNE"), valueCH6));
+		spinnerCH7.setSelection(getSpinnerIndexFromValue(
+				(int) chParameters.getParamValueByName("CH7_OPT"), valueCH));
+		spinnerCH7.setSelection(getSpinnerIndexFromValue(
+				(int) chParameters.getParamValueByName("CH8_OPT"), valueCH));
+	}
+
+	private void setupSpinners() {
+		getCH6Options();
+		getCHOptions();
+
+		final ArrayAdapter<String> adapterCH = new ArrayAdapter<String>(
+				parentActivity, R.layout.spinner_setup_item, stringCH);
+		final ArrayAdapter<String> adapterTune = new ArrayAdapter<String>(
+				parentActivity, R.layout.spinner_setup_item, stringCH6);
+
+		adapterCH.setDropDownViewResource(R.layout.spinner_setup_item_dropdown);
+		adapterTune
+				.setDropDownViewResource(R.layout.spinner_setup_item_dropdown);
+
+		spinnerCH6.setAdapter(adapterTune);
+		spinnerCH7.setAdapter(adapterCH);
+		spinnerCH8.setAdapter(adapterCH);
 	}
 
 	private void getCHOptions() {
@@ -59,8 +187,8 @@ public class FragmentSetupCH extends SetupMainPanel {
 		valueCH = new int[pairs.length];
 		stringCH = null;
 		stringCH = new String[pairs.length];
-		
-		int i=0;
+
+		int i = 0;
 		for (String item : pairs) {
 			String pair[] = item.split(";");
 			valueCH[i] = Integer.parseInt(pair[0]);
@@ -75,8 +203,8 @@ public class FragmentSetupCH extends SetupMainPanel {
 		valueCH6 = new int[pairs.length];
 		stringCH6 = null;
 		stringCH6 = new String[pairs.length];
-		
-		int i=0;
+
+		int i = 0;
 		for (String item : pairs) {
 			String pair[] = item.split(";");
 			valueCH6[i] = Integer.parseInt(pair[0]);
@@ -85,4 +213,11 @@ public class FragmentSetupCH extends SetupMainPanel {
 		}
 	}
 
+	private int getSpinnerIndexFromValue(int value, int[] valueList) {
+		for (int i = 0; i < valueList.length; i++) {
+			if (valueList[i] == value)
+				return i;
+		}
+		return -1;
+	}
 }
