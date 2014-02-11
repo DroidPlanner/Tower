42035621e46d8704f74b04c8328d5f86835b070e
Paul Loizeaux <paulloizeaux@gmail.com>
2014-02-11 13:12:12 -0500

    Removed tablet and quad waypoint sync check

--- DroidPlanner/src/com/droidplanner/activitys/PlanningActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/PlanningActivity.java
@@ -37,8 +37,8 @@ import com.droidplanner.survey.grid.Grid;
 import com.google.android.gms.maps.model.LatLng;
 
 public class PlanningActivity extends SuperActivity implements
-		OnMapInteractionListener, OnWaypointUpdateListner,
-		OnAltitudeChangedListner, OnPathFinishedListner, OnNewGridListner {
+OnMapInteractionListener, OnWaypointUpdateListner,
+OnAltitudeChangedListner, OnPathFinishedListner, OnNewGridListner {
 
 	public Polygon polygon;
 	private PlanningMapFragment planningMapFragment;
@@ -66,7 +66,7 @@ public class PlanningActivity extends SuperActivity implements
 				.findFragmentById(R.id.missionFragment);
 		surveyFragment = (SurveyFragment) getFragmentManager()
 				.findFragmentById(R.id.surveyFragment);
-		
+
 		lengthView = (TextView) findViewById(R.id.textViewTotalLength);
 
 		polygon = new Polygon();
@@ -76,8 +76,8 @@ public class PlanningActivity extends SuperActivity implements
 		planningMapFragment.setMission(drone.mission);
 		surveyFragment.setSurveyData(polygon,drone.mission.getDefaultAlt());
 		surveyFragment.setOnSurveyListner(this);
-		
-		
+
+
 		drone.mission.missionListner = this;
 
 		checkIntent();
@@ -91,7 +91,7 @@ public class PlanningActivity extends SuperActivity implements
 		String type = intent.getType();
 		if (Intent.ACTION_VIEW.equals(action) && type != null) {
 			Toast.makeText(this, intent.getData().getPath(), Toast.LENGTH_LONG)
-					.show();
+			.show();
 			openMission(intent.getData().getPath());
 			update();
 			zoom();
@@ -103,7 +103,7 @@ public class PlanningActivity extends SuperActivity implements
 		getMenuInflater().inflate(R.menu.menu_map_type, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
@@ -116,56 +116,36 @@ public class PlanningActivity extends SuperActivity implements
 		case R.id.menu_open_file:
 			openMissionFile();
 			return true;
-		
-		// Menu option Send to APM is pressed
+
+			// Menu option Send to APM is pressed
 		case R.id.menu_send_to_apm:
 			// Start of modification for waypoint safety checks
-			
+
 			// Use the method to make sure the input method safe
 			// Initialize tts engine before use?, seems like first attempt doesn't make any words
 			if(checkValidMission())
 			{
-				
+
 				// Mission path on tablet is valid
-				
+
 				// Sent mission to tablet
 				drone.mission.sendMissionToAPM();
-				
+
 				// Let user know that mission is valid
 				drone.tts.speak("Mission valid");
-				
-				// Check that the quad and the tablet agree on the mission
-				if(checkMissionSent())
-				{
-					
-					// Misison path on quad and tablet are the same
-					
-					// Let the user know that the mission on the quad and tablet agree					
-					drone.tts.speak("Save to quad successful");
-				}
-				else
-				{
-					
-					// Mission on the tablet and quad do not agree 
-					
-					
-					// Let the user know that the tablet and quad do not agree on the mission
-					drone.tts.speak("Save to quad failure");
-					
-					// Put in a break here as we had a failure?
-				}
+
 			}
 			else
 			{
-				
+
 				// Mission path on tablet is invalid
-				
+
 				// let user know waypoint path is invalid
 				drone.tts.speak("Mission incorrect");
-				
+
 				// Put in a break here as we had a failure?
 			}
-			
+
 			return true;
 		case R.id.menu_clear_wp:
 			clearWaypointsAndUpdate();
@@ -177,7 +157,7 @@ public class PlanningActivity extends SuperActivity implements
 
 	private void zoom() {
 		planningMapFragment
-				.zoomToExtents(drone.mission.getAllVisibleCoordinates());
+		.zoomToExtents(drone.mission.getAllVisibleCoordinates());
 	}
 
 	private void processReceivedPoints(List<LatLng> points) {
@@ -299,10 +279,10 @@ public class PlanningActivity extends SuperActivity implements
 	private void menuSaveFile() {
 		if (writeMission()) {
 			Toast.makeText(this, R.string.file_saved, Toast.LENGTH_SHORT)
-					.show();
+			.show();
 		} else {
 			Toast.makeText(this, R.string.error_when_saving, Toast.LENGTH_SHORT)
-					.show();
+			.show();
 		}
 	}
 
@@ -321,45 +301,20 @@ public class PlanningActivity extends SuperActivity implements
 		polygon.clearPolygon();
 		update();		
 	}
-	
+
 	// Check that the input mission on the app is fine
 	private boolean checkValidMission(){
-		
+
 		// Might be better to just have wayPoints as a List instead of an ArrayList, polymorphism, also, how DroidPlanner calls stores this data hurts my head
 		// Get an ArrayList of Waypoints class, waypoint.java
 		ArrayList<waypoint>  wayPoints = (ArrayList) drone.mission.getWaypoints();
-		
+
 		// Check if the first waypoint (is this waypoint 1 or home??) is takeoff, last one is land, and there is three or more waypoints
 		if(wayPoints.get(0).getCmd() == ApmCommands.CMD_NAV_TAKEOFF && wayPoints.get(wayPoints.size() - 1).getCmd() == ApmCommands.CMD_NAV_LAND && wayPoints.size() > 2)
 		{
 			return true;
 		}
-		
-		return false;
-	}
-	
-	private boolean checkMissionSent(){
-		
-		// I'm guessing drone.mission is the waypoint path stored on the tablet. It would be helpful if DroidPlanner told us this in documentation...
-		String plannedMission = drone.mission.getWaypointData();
-		
-		// Load waypoints from drone over MAVLink?
-		drone.waypointMananger.getWaypoints(); // Null pointer exception, cuz no drone is connected over mavlink. Put in check for this??
-		
-		// Since we loaded the waypoint path from the drone it updated the tablet's copy, load this in a new string
-		String droneMission = drone.mission.getWaypointData(); // Does this erase our current waypoints saved on the drone?
-		
-		// Output the second string to Toast, display it for a long time
-		Toast.makeText(this, droneMission, Toast.LENGTH_LONG).show();
-		
-		// Check if plannedMission and droneMission are same using String equals() method
-		if((plannedMission.equals(droneMission)))
-		{
-			// The two sets of waypoints are the same
-			return true;
-		}
-		
-		// Two sets of waypoints are not the same
+
 		return false;
 	}
 
