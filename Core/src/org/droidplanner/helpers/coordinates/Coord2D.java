ea5ff189f201842dbdb50b2b6e50f1201418a0b8
Arthur Benemann <arthur.benemann@gmail.com>
2014-02-22 20:17:51 -0300

    Core: Fixing small error in the comment

--- Core/src/org/droidplanner/helpers/coordinates/Coord2D.java
+++ Core/src/org/droidplanner/helpers/coordinates/Coord2D.java
@@ -1,16 +1,20 @@
 package org.droidplanner.helpers.coordinates;
 
 public class Coord2D {
-	private double x; // aka Latitude
-	private double y; // aka Longitude
+	private double x; // aka Longitude
+	private double y; // aka Latitude
 
-	public Coord2D(double x2, double y) {
-		set(x2, y);
+	public Coord2D(double x, double y) {
+		set(x, y);
 	}
 
 	public void set(double x, double y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public void set(Coord2D coord) {
+		set(coord.x, coord.y);
 	}
 
 	public double getX() {
@@ -20,4 +24,5 @@ public class Coord2D {
 	public double getY() {
 		return y;
 	}
+
 }
