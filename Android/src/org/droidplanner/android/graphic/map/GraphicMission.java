9513cc5a9222851efe717a6c7e343cf224f90414
Arthur Benemann <arthur.benemann@gmail.com>
2014-03-28 02:09:30 -0300

    MapManager: Make mission Markers at least show up on the map.

--- Android/src/org/droidplanner/android/graphic/map/GraphicMission.java
+++ Android/src/org/droidplanner/android/graphic/map/GraphicMission.java
@@ -3,13 +3,19 @@ package org.droidplanner.android.graphic.map;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.droidplanner.core.drone.Drone;
 import org.droidplanner.android.fragments.helpers.MapPath.PathSource;
+import org.droidplanner.android.graphic.DroneHelper;
 import org.droidplanner.android.graphic.map.MarkerManager.MarkerSource;
+import org.droidplanner.core.drone.Drone;
 import org.droidplanner.core.mission.Mission;
 import org.droidplanner.core.mission.MissionItem;
+import org.droidplanner.core.mission.waypoints.Waypoint;
+
+import android.content.Context;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GraphicMission implements PathSource {
 
@@ -22,9 +28,7 @@ public class GraphicMission implements PathSource {
 	public List<MarkerSource> getMarkers() {
 		List<MarkerSource> markers = new ArrayList<MarkerSource>();
 		for (MissionItem item : mission.getItems()) {
-			if (item instanceof MarkerSource) {
-				markers.add((MarkerSource) item);
-			}
+				markers.add(new itemMarker((Waypoint) item));
 		}
 		return markers;
 	}
@@ -33,6 +37,26 @@ public class GraphicMission implements PathSource {
 	public List<LatLng> getPathPoints() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private class itemMarker implements MarkerSource{
+		Waypoint item;
+
+		public itemMarker(Waypoint item) {
+			this.item = item;
+		}
+
+		@Override
+		public MarkerOptions build(Context context) {
+			return new MarkerOptions()
+					.position(DroneHelper.CoordToLatLang(item.getCoordinate()))
+					.draggable(true).anchor(0.5f, 0.5f);
+		}
+
+		@Override
+		public void update(Marker marker, Context context) {
+			marker.setPosition(DroneHelper.CoordToLatLang(item.getCoordinate()));			
+		}
 	}
 
 }
