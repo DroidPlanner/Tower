24a7b10252a86710e63d8c65ec36e4f792435860
Arthur Benemann <arthur.benemann@gmail.com>
2014-05-10 12:18:15 -0700

    Adding event for Misssio Progress Updates and correspoonding case on the TTS class

--- Core/src/org/droidplanner/core/drone/variables/MissionStats.java
+++ Core/src/org/droidplanner/core/drone/variables/MissionStats.java
@@ -2,10 +2,12 @@ package org.droidplanner.core.drone.variables;
 
 import org.droidplanner.core.drone.Drone;
 import org.droidplanner.core.drone.DroneVariable;
+import org.droidplanner.core.drone.DroneInterfaces.DroneEventsType;
+import org.droidplanner.core.helpers.units.Length;
 
 public class MissionStats extends DroneVariable {
 	private double distanceToWp = 0;
-	private short goingForWaypoint = -1;
+	private short currentWP = -1;
 
 	public MissionStats(Drone myDrone) {
 		super(myDrone);
@@ -16,8 +18,17 @@ public class MissionStats extends DroneVariable {
 	}
 
 	public void setWpno(short seq) {
-		goingForWaypoint = seq;
+		this.currentWP = seq;
+		myDrone.events.notifyDroneEvent(DroneEventsType.MISSION_WP_UPDATE);
 
+	}
+
+	public int getCurrentWP() {
+		return currentWP;
+	}
+	
+	public Length getDistanceToWP(){
+		return new Length(distanceToWp);
 	}
 
 }
