a97502f39b5be4f022060e59081318637b299b15
Arthur Benemann <arthur.benemann@gmail.com>
2014-05-15 18:02:43 -0700

    Android: Implement leash follow-me type.

--- Android/src/org/droidplanner/android/gcs/FollowMe.java
+++ Android/src/org/droidplanner/android/gcs/FollowMe.java
@@ -4,6 +4,7 @@ import org.droidplanner.core.drone.Drone;
 import org.droidplanner.core.drone.DroneInterfaces.DroneEventsType;
 import org.droidplanner.core.drone.DroneInterfaces.OnDroneListener;
 import org.droidplanner.core.helpers.coordinates.Coord2D;
+import org.droidplanner.core.helpers.geoTools.GeoTools;
 
 import android.content.Context;
 import android.location.Location;
@@ -17,9 +18,12 @@ import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationRequest;
 
-public class FollowMe implements GooglePlayServicesClient.ConnectionCallbacks,GooglePlayServicesClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener, OnDroneListener {
-	private static final long MIN_TIME_MS = 200;
-	private static final float MIN_DISTANCE_M = 0;
+public class FollowMe implements GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener,
+		com.google.android.gms.location.LocationListener, OnDroneListener {
+	private static final long MIN_TIME_MS = 500;
+	private static final float MIN_DISTANCE_M = 1;
+	private static final double LEASH_LENGTH = 10.0;
 	private Context context;
 	private boolean followMeEnabled = false;
 	private Drone drone;
@@ -51,7 +55,7 @@ public class FollowMe implements GooglePlayServicesClient.ConnectionCallbacks,Go
 
 		// Register the listener with the Location Manager to receive location
 		// updates
-		
+
 		LocationRequest mLocationRequest = LocationRequest.create();
 		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 		mLocationRequest.setInterval(MIN_TIME_MS);
@@ -60,7 +64,7 @@ public class FollowMe implements GooglePlayServicesClient.ConnectionCallbacks,Go
 		mLocationClient.requestLocationUpdates(mLocationRequest, this);
 
 		followMeEnabled = true;
-		drone.state.setMode(ApmModes.ROTOR_GUIDED);
+		// drone.state.setMode(ApmModes.ROTOR_GUIDED);
 	}
 
 	private void disableFollowMe() {
@@ -74,54 +78,73 @@ public class FollowMe implements GooglePlayServicesClient.ConnectionCallbacks,Go
 		return followMeEnabled;
 	}
 
-	@Override
-	public void onLocationChanged(Location location) {
-		Coord2D coord = new Coord2D(location.getLatitude(),
-				location.getLongitude());
-		Log.d("follow", coord.toString());
-		drone.guidedPoint.newGuidedCoord(coord);
-	}
-
 	private boolean isEnabledInPreferences() {
 		return true; // TODO remove this method if not needed
 		/*
-		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(context);
-
-		return prefs.getBoolean("pref_follow_me_mode_enabled", false);
-		*/
+		 * SharedPreferences prefs = PreferenceManager
+		 * .getDefaultSharedPreferences(context);
+		 * 
+		 * return prefs.getBoolean("pref_follow_me_mode_enabled", false);
+		 */
 	}
 
 	@Override
 	public void onConnectionFailed(ConnectionResult arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onConnected(Bundle arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onDisconnected() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onDroneEvent(DroneEventsType event, Drone drone) {
-		switch(event){
+		switch (event) {
 		case MODE:
 			if ((drone.state.getMode() != ApmModes.ROTOR_GUIDED)) {
 				disableFollowMe();
-			}			
+			}
 			break;
 		default:
 			return;
-		
+
 		}
-		
+
 	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		Coord2D gcsCoord = new Coord2D(location.getLatitude(),
+				location.getLongitude());
+		Log.d("follow", gcsCoord.toString());
+
+		// TODO implement some sort of Follow-me type selection
+		// processNewLocationAsOverYourHead(gcsCoord);
+		processNewLocationAsLeash(gcsCoord);
+	}
+
+	private void processNewLocationAsOverYourHead(Coord2D gcsCoord) {
+		drone.guidedPoint.newGuidedCoord(gcsCoord);
+	}
+
+	private void processNewLocationAsLeash(Coord2D gcsCoord) {
+		if (GeoTools.getDistance(gcsCoord, drone.GPS.getPosition())
+				.valueInMeters() > LEASH_LENGTH) {
+			double headingGCStoDrone = GeoTools.getHeadingFromCoordinates(
+					gcsCoord, drone.GPS.getPosition());
+			Coord2D goCoord = GeoTools.newCoordFromBearingAndDistance(gcsCoord,
+					headingGCStoDrone, LEASH_LENGTH);
+			drone.guidedPoint.newGuidedCoord(goCoord);
+		}
+	}
+
 }
