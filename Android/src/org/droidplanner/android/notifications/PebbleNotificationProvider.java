394bd0c08cef4ecc75beb66c7cf24585015d9e79
squilter <s.quilter@aol.com>
2014-07-04 17:08:49 -0700

    sending data mostly works

--- Android/src/org/droidplanner/android/notifications/PebbleNotificationProvider.java
+++ Android/src/org/droidplanner/android/notifications/PebbleNotificationProvider.java
@@ -6,6 +6,7 @@ import org.droidplanner.core.drone.Drone;
 import org.droidplanner.core.drone.DroneInterfaces;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
@@ -17,10 +18,7 @@ NotificationHandler.NotificationProvider {
 	
 	private static final int KEY_MODE = 0;
 	private static final int KEW_FOLLOW_TYPE=1;
-	private static final int KEY_TELEM_1 = 2;
-	private static final int KEY_TELEM_2 = 3;
-	private static final int KEY_TELEM_3 = 4;
-	private static final int KEY_TELEM_4 = 5;
+	private static final int KEY_TELEM = 2;
 	
 	private static final UUID DP_UUID = UUID.fromString("79a2893d-fc7d-48c4-bc9a-34854d94ef6e");
 	/**
@@ -36,18 +34,28 @@ NotificationHandler.NotificationProvider {
 	public void onDroneEvent(DroneInterfaces.DroneEventsType event, Drone drone) {
 		switch (event) {
 		case MODE:
-			sendDataToWatch(KEY_MODE, "seb");
+			sendDataToWatch(KEY_MODE, drone.state.getMode().getName());
 			break;
 		case BATTERY:
-			//send stuff
+			sendDataToWatch(KEY_TELEM, getTelemetryToSend(drone) );
 			break;
 		case SPEED:
-			//send stuff
+			sendDataToWatch(KEY_TELEM, getTelemetryToSend(drone));
 			break;
 		default:
 			break;
 
 		}
+	}
+	
+	private String getTelemetryToSend(Drone drone){
+		String bat = "Bat:" + Double.toString(roundToOneDecimal(drone.battery.getBattVolt())) + "V";
+		String speed = "Speed: " + Double.toString(roundToOneDecimal(drone.speed.getAirSpeed()));
+		return bat + "\n" + speed;
+		
+	}
+	private double roundToOneDecimal(double value){
+		return (double)Math.round(value * 10) / 10;
 	}
 	
     public void sendDataToWatch(int id, String str) {
@@ -59,5 +67,6 @@ NotificationHandler.NotificationProvider {
         // Send the assembled dictionary to the weather watch-app; this is a no-op if the app isn't running or is not
         // installed
         PebbleKit.sendDataToPebble(mContext.getApplicationContext(), DP_UUID, data);
+        Log.d("seB","sending data: "+str);
     }
 }
