d047fc50062e604475e2a736a4be4905640c6939
Arthur Benemann <arthur.benemann@gmail.com>
2014-07-07 17:57:32 -0700

    Added a class to send ROI commands

---
+++ Core/src/org/droidplanner/core/MAVLink/MavLinkROI.java
@@ -0,0 +1,22 @@
+package org.droidplanner.core.MAVLink;
+
+import org.droidplanner.core.drone.Drone;
+import org.droidplanner.core.helpers.coordinates.Coord3D;
+
+import com.MAVLink.Messages.ardupilotmega.msg_command_long;
+import com.MAVLink.Messages.enums.MAV_CMD;
+
+public class MavLinkROI {
+	public static void setROI(Drone drone, Coord3D coord) {
+		msg_command_long msg = new msg_command_long();
+		msg.target_system = 1;
+		msg.target_component = 1;
+		msg.command = MAV_CMD.MAV_CMD_DO_SET_ROI;
+		
+		msg.param5 = (float) coord.getX();
+		msg.param6 = (float) coord.getY();
+		msg.param7 = (float) 0.0;
+		
+		drone.MavClient.sendMavPacket(msg.pack());
+	}
+}
