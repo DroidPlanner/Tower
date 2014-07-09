d5a8185aeb4f8d35c44eef65ffa871c8bac2c5f9
fhuya <fhuyakou@gmail.com>
2014-07-08 21:26:18 -0400

    google map api experimentation

--- AndroidWear/src/main/java/org/droidplanner/android/wear/WearUI.java
+++ AndroidWear/src/main/java/org/droidplanner/android/wear/WearUI.java
@@ -2,23 +2,50 @@ package org.droidplanner.android.wear;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.FragmentGridPagerAdapter;
+import android.support.wearable.view.GridViewPager;
 import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
 
-public class WearUI extends Activity {
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
 
-    private TextView mTextView;
+import org.droidplanner.android.wear.views.WearUIPagerAdapter;
+
+public class WearUI extends Activity implements MessageApi.MessageListener, NodeApi.NodeListener {
+
+    private GridViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear_ui);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mViewPager = (GridViewPager) stub.findViewById(R.id.grid_view_pager);
+
+                final FragmentGridPagerAdapter pagerAdapter = new WearUIPagerAdapter(getFragmentManager());
+                mViewPager.setAdapter(pagerAdapter);
             }
         });
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+    }
+
+    @Override
+    public void onPeerConnected(Node node) {
+
+    }
+
+    @Override
+    public void onPeerDisconnected(Node node) {
+
     }
 }
---
+++ AndroidWear/src/main/java/org/droidplanner/android/wear/views/WearUIPagerAdapter.java
@@ -0,0 +1,37 @@
+package org.droidplanner.android.wear.views;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.wearable.view.FragmentGridPagerAdapter;
+
+import org.droidplanner.android.wear.fragments.WearMapFragment;
+
+/**
+ * Fragment pager adapter for the droidplanner wear UI.
+ */
+public class WearUIPagerAdapter extends FragmentGridPagerAdapter {
+
+    public WearUIPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getFragment(int i, int i2) {
+        return new WearMapFragment();
+    }
+
+    @Override
+    public int getRowCount() {
+        return 1;
+    }
+
+    @Override
+    public int getColumnCount(int i) {
+        return 1;
+    }
+
+    @Override
+    public long getFragmentId(int row, int column){
+        return 0l;
+    }
+}
