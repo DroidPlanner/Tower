2319aedb5455f88a06876f64cd99b5aaf3e3d6b9
Arthur Benemann <arthur.benemann@gmail.com>
2014-07-14 09:47:28 -0700

    Add comments to methods in DirectoryPath.java

--- Android/src/org/droidplanner/android/utils/file/DirectoryPath.java
+++ Android/src/org/droidplanner/android/utils/file/DirectoryPath.java
@@ -6,23 +6,33 @@ import android.os.Environment;
 
 public class DirectoryPath {
 
+	/**
+	 * Main path used to store files related to the program
+	 * 
+	 * @return Path to DroidPlanner/ folder in external storage
+	 */
 	static public String getDroidPlannerPath() {
 		String root = Environment.getExternalStorageDirectory().getPath();
 		return (root + "/DroidPlanner/");
 	}
 
+	/**
+	 * Storage folder for Parameters
+	 */
 	static public String getParametersPath() {
 		return getDroidPlannerPath() + "/Parameters/";
 	}
 
+	/**
+	 * Storage folder for mission files
+	 */
 	static public String getWaypointsPath() {
 		return getDroidPlannerPath() + "/Waypoints/";
 	}
 
-	static public String getGCPPath() {
-		return getDroidPlannerPath() + "/GCP/";
-	}
-
+	/**
+	 * Folder where telemetry log files are stored
+	 */
 	static public File getTLogPath() {
 		File f = new File(getDroidPlannerPath() + "/Logs/");
 		f.mkdirs();
@@ -38,18 +48,30 @@ public class DirectoryPath {
 		return f;
 	}
 
+	/**
+	 * Storage folder for user map tiles
+	 */
 	static public String getMapsPath() {
 		return getDroidPlannerPath() + "/Maps/";
 	}
 
+	/**
+	 * Storage folder for user camera description files
+	 */
 	public static String getCameraInfoPath() {
 		return getDroidPlannerPath() + "/CameraInfo/";
 	}
 
+	/**
+	 * Storage folder for stacktraces
+	 */
 	public static String getLogCatPath() {
 		return getDroidPlannerPath() + "/LogCat/";
 	}
 
+	/**
+	 * Storage folder for SRTM data
+	 */
 	static public String getSrtmPath() {
 		return getDroidPlannerPath() + "/Srtm/";
 	}
