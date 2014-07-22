3eea7909401e43d49f1d077a7ceabbb437bf3511
squilter <s.quilter@aol.com>
2014-07-22 15:30:02 -0700

    Refactor: Formatting

--- Core/src/org/droidplanner/core/drone/variables/ROIPoint.java
+++ Core/src/org/droidplanner/core/drone/variables/ROIPoint.java
@@ -1,23 +1,17 @@
 package org.droidplanner.core.drone.variables;
 
-import org.droidplanner.core.MAVLink.MavLinkModes;
 import org.droidplanner.core.MAVLink.MavLinkROI;
 import org.droidplanner.core.drone.Drone;
 import org.droidplanner.core.drone.DroneVariable;
-import org.droidplanner.core.drone.DroneInterfaces.DroneEventsType;
-import org.droidplanner.core.drone.DroneInterfaces.OnDroneListener;
 import org.droidplanner.core.helpers.coordinates.Coord2D;
 import org.droidplanner.core.helpers.coordinates.Coord3D;
 import org.droidplanner.core.helpers.units.Altitude;
-
-import com.MAVLink.Messages.ApmModes;
 
 public class ROIPoint extends DroneVariable {
 	private Coord2D coord = new Coord2D(0, 0);
 	private Altitude altitude = new Altitude(0.0);
 
-
-    public ROIPoint(Drone myDrone) {
+	public ROIPoint(Drone myDrone) {
 		super(myDrone);
 	}
 
@@ -26,17 +20,17 @@ public class ROIPoint extends DroneVariable {
 	}
 
 	public void setROIAlt(Altitude alt) {
-		this.altitude=alt;
+		this.altitude = alt;
 	}
 
 	private void changeCoord(Coord2D coord) {
-			this.coord = coord;
-			sendROIPoint();
+		this.coord = coord;
+		sendROIPoint();
 	}
 
 	private void sendROIPoint() {
-		MavLinkROI.setROI(myDrone, new Coord3D(coord.getLat(),
-				coord.getLng(), altitude));
+		MavLinkROI.setROI(myDrone, new Coord3D(coord.getLat(), coord.getLng(),
+				altitude));
 	}
 
 	public Coord2D getCoord() {
