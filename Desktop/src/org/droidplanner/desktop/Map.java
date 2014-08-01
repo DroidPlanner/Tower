b5154873e8325025546ef739a8d46726e184d9db
Arthur Benemann <arthur.benemann@gmail.com>
2014-08-01 20:26:59 -0300

    Use the Cached TileLoader

--- Desktop/src/org/droidplanner/desktop/Map.java
+++ Desktop/src/org/droidplanner/desktop/Map.java
@@ -1,9 +1,12 @@
 package org.droidplanner.desktop;
 
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.OsmFileCacheTileLoader;
 
 public class Map extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -13,6 +16,11 @@ public class Map extends JFrame {
 		super("Map");
 		setSize(800, 600);
 		JMapViewer map = new JMapViewer();
+		try {
+			map.setTileLoader(new OsmFileCacheTileLoader(map));
+		} catch (SecurityException | IOException e) {
+			e.printStackTrace();
+		}
 		add(map);
 		
 		map.addMapMarker(new MapMarkerDot(-29, -51));
