c6729175ac220aa88040e8f151d407dc4fb7375b
Arthur Benemann <arthur.benemann@gmail.com>
2014-08-19 13:39:56 -0700

    Extracted the fake location generator to a new class

--- Desktop/src/org/droidplanner/desktop/logic/Logic.java
+++ Desktop/src/org/droidplanner/desktop/logic/Logic.java
@@ -2,8 +2,6 @@ package org.droidplanner.desktop.logic;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import org.droidplanner.core.MAVLink.MAVLinkStreams.MAVLinkOutputStream;
 import org.droidplanner.core.MAVLink.MavLinkMsgHandler;
@@ -15,11 +13,6 @@ import org.droidplanner.core.drone.profiles.VehicleProfile;
 import org.droidplanner.core.drone.variables.Type.FirmwareType;
 import org.droidplanner.core.gcs.follow.Follow;
 import org.droidplanner.core.gcs.follow.Follow.TextNotificationReceiver;
-import org.droidplanner.core.gcs.location.Location;
-import org.droidplanner.core.gcs.location.Location.LocationFinder;
-import org.droidplanner.core.gcs.location.Location.LocationReceiver;
-import org.droidplanner.core.helpers.coordinates.Coord2D;
-import org.droidplanner.core.helpers.geoTools.GeoTools;
 import org.droidplanner.desktop.Communication.Connection;
 
 import com.MAVLink.Messages.MAVLinkMessage;
@@ -52,60 +45,7 @@ public class Logic implements Runnable {
 				// TODO Auto-generated method stub
 
 			}
-		}, new LocationFinder() {
-
-			private LocationReceiver receiver;
-
-			@Override
-			public void setLocationListner(LocationReceiver receiver) {
-				this.receiver = receiver;				
-			}
-
-			@Override
-			public void enableLocationUpdates() {
-				Timer timer = new Timer();
-				timer.scheduleAtFixedRate(new TimerTask() {
-					Coord2D start = new Coord2D(-35.363154,149.165067);
-					Location[] locations = {
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 00)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 10)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 20)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 30)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 40)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 50)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 60)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 70)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 80)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 90)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 80)), 
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 70)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 60)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 50)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 40)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 30)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 20)),
-							new Location(GeoTools.newCoordFromBearingAndDistance(start, 90, 10)),
-							};
-					int index = 0;
-
-					@Override
-					public void run() {
-						receiver.onLocationChanged(locations[index++]);
-						if (index >= locations.length) {
-							index = 0;
-						}
-
-					}
-				}, 0, 5*1000);
-
-			}
-
-			@Override
-			public void disableLocationUpdates() {
-				// TODO Auto-generated method stub
-
-			}
-		}, new TextNotificationReceiver() {
+		}, new FakeLocation(), new TextNotificationReceiver() {
 
 			@Override
 			public void shortText(String notification) {
