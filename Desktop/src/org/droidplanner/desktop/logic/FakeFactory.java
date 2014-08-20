564ee6e2eaf3b46fb864ab151b5585e2106da42c
Arthur Benemann <arthur.benemann@gmail.com>
2014-08-20 11:59:34 -0700

    Implement the clock method for the drone object

--- Desktop/src/org/droidplanner/desktop/logic/FakeFactory.java
+++ Desktop/src/org/droidplanner/desktop/logic/FakeFactory.java
@@ -11,19 +11,19 @@ public class FakeFactory {
 
 	static Preferences fakePreferences() {
 		return new Preferences() {
-	
+
 			@Override
 			public VehicleProfile loadVehicleProfile(FirmwareType firmwareType) {
 				// TODO Auto-generated method stub
 				return null;
 			}
-	
+
 			@Override
 			public FirmwareType getVehicleType() {
 				// TODO Auto-generated method stub
 				return FirmwareType.ARDU_COPTER;
 			}
-	
+
 			@Override
 			public Rates getRates() {
 				return new Rates();
@@ -33,39 +33,37 @@ public class FakeFactory {
 
 	static Handler fakeHandler() {
 		return new Handler() {
-	
+
 			@Override
 			public void removeCallbacks(Runnable thread) {
 				// TODO Auto-generated method stub
-	
+
 			}
-	
+
 			@Override
 			public void postDelayed(Runnable thread, long timeout) {
 				// TODO Auto-generated method stub
-	
+
 			}
 		};
 	}
 
 	static Clock fakeClock() {
 		return new Clock() {
-	
 			@Override
 			public long elapsedRealtime() {
-				// TODO Auto-generated method stub
-				return 0;
+				return System.currentTimeMillis();
 			}
 		};
 	}
 
 	static TextNotificationReceiver notificationReceiver() {
 		return new TextNotificationReceiver() {
-	
+
 			@Override
 			public void shortText(String notification) {
 				System.out.println(notification);
-	
+
 			}
 		};
 	}
