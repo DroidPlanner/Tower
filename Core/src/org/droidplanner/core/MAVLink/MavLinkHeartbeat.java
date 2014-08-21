528c9e7a67fc1b244b8476bf9286a9241193c5c0
alextel69 <alextel69@gmail.com>
2014-08-21 21:50:12 +0300

    Revert "Set sequence number in msg_heartbeat, which is sent to drone"
    This reverts commit c01bc339e16f41c382e4c9b64ddbdf58eee4c6e8.

--- Core/src/org/droidplanner/core/MAVLink/MavLinkHeartbeat.java
+++ Core/src/org/droidplanner/core/MAVLink/MavLinkHeartbeat.java
@@ -22,7 +22,6 @@ public class MavLinkHeartbeat {
 	 * @since 1.2.0
 	 */
 	private static final msg_heartbeat sMsg = new msg_heartbeat();
-	private static int sequence_number = 0;
 	static {
 		sMsg.type = MAV_TYPE.MAV_TYPE_GCS;
 		sMsg.autopilot = MAV_AUTOPILOT.MAV_AUTOPILOT_GENERIC;
@@ -45,14 +44,8 @@ public class MavLinkHeartbeat {
 	 * @since 1.2.0
 	 */
 	public static void sendMavHeartbeat(Drone drone) {
-		if (drone != null){
-			sequence_number++;
-			if (sequence_number > 255){
-				sequence_number = 0;
-			}
-			sMsgPacket.seq = sequence_number;
+		if (drone != null)
 			drone.MavClient.sendMavPacket(sMsgPacket);
-		}
 	}
 
 }
