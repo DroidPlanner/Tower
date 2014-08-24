0abe2fbc7f34a5f3e282d14da5b7bd73469625b3
TShapinsky <tobiasshapinsky@gmail.com>
2014-08-24 17:37:31 -0400

    extracted constants

--- Core/src/org/droidplanner/core/drone/variables/Speed.java
+++ Core/src/org/droidplanner/core/drone/variables/Speed.java
@@ -4,6 +4,9 @@ import org.droidplanner.core.drone.DroneVariable;
 import org.droidplanner.core.model.Drone;
 
 public class Speed extends DroneVariable {
+	public static final int COLLISION_SECONDS_BEFORE_COLLISION = 2;
+	public static final double COLLISION_DANGEROUS_SPEED = -3.0;
+	public static final double COLLISION_SAFE_ALTITUDE = 1.0;
 	private org.droidplanner.core.helpers.units.Speed verticalSpeed = new org.droidplanner.core.helpers.units.Speed(
 			0);
 	private org.droidplanner.core.helpers.units.Speed groundSpeed = new org.droidplanner.core.helpers.units.Speed(
@@ -44,7 +47,7 @@ public class Speed extends DroneVariable {
 		this.verticalSpeed = new org.droidplanner.core.helpers.units.Speed(climb);
 		//if drone will crash in 2 seconds at constant climb rate and climb rate < -3 m/s and altitude > 1 meter
 		double altitude = myDrone.getAltitude().getAltitude();
-		if(altitude + climb*2 < 0 && climb < -3.0 && altitude > 1.0){
+		if(altitude + climb* COLLISION_SECONDS_BEFORE_COLLISION < 0 && climb < COLLISION_DANGEROUS_SPEED && altitude > COLLISION_SAFE_ALTITUDE){
 			myDrone.getState().setCollisionImminent(true);
 		}else{
 			myDrone.getState().setCollisionImminent(false);
