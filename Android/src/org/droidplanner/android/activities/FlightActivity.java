658767cdbf772c89858ce06bac26a976d925aea4
Shaune <shaune.jan@gmail.com>
2014-08-28 23:56:54 +0800

    move string to xml for localization
    1. move some hardcode string to xml for localization
    2. add arrys.xml for traditional chinese localization

--- Android/src/org/droidplanner/android/activities/FlightActivity.java
+++ Android/src/org/droidplanner/android/activities/FlightActivity.java
@@ -350,7 +350,7 @@ public class FlightActivity extends DrawerNavigationUI implements
 
 	@Override
 	public CharSequence[][] getHelpItems() {
-		return new CharSequence[][] { { "How to plan and fly a mission" },
+		return new CharSequence[][] { { getResources().getString(R.string.help_item_description) },
 				{ "https://www.youtube.com/watch?v=btsk7bzn-9Q" } };
 	}
 }
