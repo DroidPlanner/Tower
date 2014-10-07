4d0097a0ac3218b0e30aa0781e585675f9649f57
Arthur Benemann <arthur.benemann@gmail.com>
2014-10-07 13:46:43 -0700

    Android: Plot magnetometer data in calibration screen

--- Android/src/org/droidplanner/android/fragments/calibration/mag/FragmentSetupMAG.java
+++ Android/src/org/droidplanner/android/fragments/calibration/mag/FragmentSetupMAG.java
@@ -1,12 +1,26 @@
 package org.droidplanner.android.fragments.calibration.mag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.droidplanner.R;
+import org.droidplanner.android.DroidPlannerApp;
 import org.droidplanner.android.fragments.calibration.SetupMainPanel;
 import org.droidplanner.android.fragments.calibration.SetupSidePanel;
+import org.droidplanner.android.widgets.scatterplot.ScatterPlot;
+import org.droidplanner.core.MAVLink.MavLinkStreamRates;
+import org.droidplanner.core.drone.DroneInterfaces.DroneEventsType;
+import org.droidplanner.core.drone.DroneInterfaces.OnDroneListener;
+import org.droidplanner.core.model.Drone;
 
 import android.view.View;
 
-public class FragmentSetupMAG extends SetupMainPanel {
+public class FragmentSetupMAG extends SetupMainPanel implements OnDroneListener {
+
+	private ScatterPlot plot1,plot2;
+	
+	private List<Float> data1 = new ArrayList<Float>();
+	private List<Float> data2 = new ArrayList<Float>();
 
 	@Override
 	public int getPanelLayout() {
@@ -21,13 +35,32 @@ public class FragmentSetupMAG extends SetupMainPanel {
 
 	@Override
 	public void setupLocalViews(View v) {
-		// TODO Auto-generated method stub
-
+		plot1 = (ScatterPlot) v.findViewById(R.id.scatterPlot1);
+		plot2 = (ScatterPlot) v.findViewById(R.id.scatterPlot2);
 	}
 
 	@Override
 	public void doCalibrationStep(int step) {
-		// TODO Auto-generated method stub
+		Drone drone = ((DroidPlannerApp) getActivity().getApplication()).getDrone();
+		MavLinkStreamRates.setupStreamRates(drone.getMavClient(), 0, 0, 0, 0, 0, 0, 50, 0);
+		drone.addDroneListener(this);
+	}
 
+	@Override
+	public void onDroneEvent(DroneEventsType event, Drone drone) {
+		switch (event) {
+		case MAGNETOMETER:
+			int[] mag = drone.getMagnetometer().getVector();
+			data1.add(mag[0]/1000f);
+			data1.add(mag[1]/1000f);
+			plot1.newDataSet((Float[]) data1.toArray(new Float[data1.size()]));
+			data2.add(mag[1]/1000f);
+			data2.add(mag[2]/1000f);
+			plot2.newDataSet((Float[]) data2.toArray(new Float[data2.size()]));
+			break;
+		default:
+			break;
+
+		}
 	}
 }
