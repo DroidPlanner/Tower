464c4428906ce55b94ccb838107b17aee3e3bf2a
Arthur Benemann <arthur.benemann@gmail.com>
2014-10-08 11:11:45 -0700

    Library: Add EllipsoidFit library from https://github.com/KEOpenSource/EllipsoidFit

---
+++ Libraries/EllipsoidFit/EllipsoidFit/src/ellipsoidFit/Main.java
@@ -0,0 +1,17 @@
+package ellipsoidFit;
+
+/**
+ * Run the EllipsoidFit sample.
+ * 
+ * Apache License Version 2.0, January 2004.
+ * 
+ * @author Kaleb
+ * @version 1.0
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		EllipsoidFitSample sample = new EllipsoidFitSample();
+	}
+}
