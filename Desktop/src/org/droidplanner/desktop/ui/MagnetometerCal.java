1b30f1907617e3f93ad08f5d07eb177c768ff89f
Arthur Benemann <arthur.benemann@gmail.com>
2014-10-10 09:08:33 -0700

    Desktop: Show the estimated ellipse in the mag. cal.

--- Desktop/src/org/droidplanner/desktop/ui/MagnetometerCal.java
+++ Desktop/src/org/droidplanner/desktop/ui/MagnetometerCal.java
@@ -1,5 +1,6 @@
 package org.droidplanner.desktop.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -18,78 +19,99 @@ import ellipsoidFit.FitPoints;
 public class MagnetometerCal implements OnMagCalibrationListner {
 	private static final int Y_SIZE = 600;
 	private static final int X_SIZE = 600;
-	private List<Float> data1 = new ArrayList<Float>();
+	private List<Float> data1 = new ArrayList<Float>(), data2 = new ArrayList<Float>();
 	public GraphPanel graph;
-	private ScatterPlot plot1;
+	private ScatterPlot plot1, plot2;
 
 	public MagnetometerCal() {
 
 		JFrame frame = new JFrame("Parameters");
+		frame.setLayout(new BorderLayout());
 		plot1 = new ScatterPlot();
+		plot1.setPreferredSize(new Dimension(X_SIZE / 2, Y_SIZE / 2));
+		plot2 = new ScatterPlot();
+		plot2.setPreferredSize(new Dimension(X_SIZE / 2, Y_SIZE / 2));
 		frame.setPreferredSize(new Dimension(X_SIZE, Y_SIZE));
-		frame.add(plot1);
+		frame.add(plot1, BorderLayout.EAST);
+		frame.add(plot2);
 		frame.pack();
 		frame.setVisible(true);
 	}
 
 	static void create(org.droidplanner.core.model.Drone drone) {
 		MagnetometerCal window = new MagnetometerCal();
-		new MagnetometerCalibration(drone,window);
+		new MagnetometerCalibration(drone, window);
 	}
 
 	@Override
-	public void newEstimation(FitPoints ellipsoidFit,int sampleSize, int[] magVector) {
-		System.out.println(String.format("Sample %d\traw %s\tFit %2.1f \tCenter %s\tRadius %s",sampleSize, Arrays.toString(magVector),ellipsoidFit.getFitness()*100,ellipsoidFit.center.toString(), ellipsoidFit.radii.toString()));
+	public void newEstimation(FitPoints ellipsoidFit, int sampleSize, int[] magVector) {
+		System.out.println(String.format("Sample %d\traw %s\tFit %2.1f \tCenter %s\tRadius %s",
+				sampleSize, Arrays.toString(magVector), ellipsoidFit.getFitness() * 100,
+				ellipsoidFit.center.toString(), ellipsoidFit.radii.toString()));
 		data1.add((float) magVector[0]);
 		data1.add((float) magVector[1]);
 		plot1.newDataSet((Float[]) data1.toArray(new Float[data1.size()]));
 		if (ellipsoidFit.center.isNaN() || ellipsoidFit.radii.isNaN()) {
 			plot1.updateSphere(null);
-		}else{
-			plot1.updateSphere(new int[] {(int) ellipsoidFit.center.getEntry(0),(int) ellipsoidFit.center.getEntry(1),(int) ellipsoidFit.radii.getEntry(0),(int) ellipsoidFit.radii.getEntry(1)});
+		} else {
+			plot1.updateSphere(new int[] { (int) ellipsoidFit.center.getEntry(0),
+					(int) ellipsoidFit.center.getEntry(1), (int) ellipsoidFit.radii.getEntry(0),
+					(int) ellipsoidFit.radii.getEntry(1) });
 		}
 		plot1.repaint(100);
+
+		data2.add((float) magVector[1]);
+		data2.add((float) magVector[2]);
+		plot2.newDataSet((Float[]) data2.toArray(new Float[data2.size()]));
+		if (ellipsoidFit.center.isNaN() || ellipsoidFit.radii.isNaN()) {
+			plot2.updateSphere(null);
+		} else {
+			plot2.updateSphere(new int[] { (int) ellipsoidFit.center.getEntry(1),
+					(int) ellipsoidFit.center.getEntry(2), (int) ellipsoidFit.radii.getEntry(1),
+					(int) ellipsoidFit.radii.getEntry(2) });
+		}
+		plot2.repaint(100);
+
 	}
 
 	@Override
 	public void finished(FitPoints fit) {
-		System.err.println("####################################################################################################################################\n");
+		System.err
+				.println("################################################################################################################\n");
 	}
 
 	public class ScatterPlot extends Canvas {
-
 		private static final long serialVersionUID = 1L;
 
-		private static final float SCALE_FACTOR = 1/1000f;
-	
+		private static final float SCALE_FACTOR = 1 / 1000f;
+
 		private Float[] points = new Float[] {};
 
 		private int[] sphere = null;
 
-		private int halfWidth,halfHeight,halfScale;
-	
-		public ScatterPlot() {
-			halfWidth = X_SIZE / 2;
-			halfHeight = Y_SIZE / 2;
-			halfScale = (halfHeight > halfWidth) ? halfWidth : halfHeight;
-		}
-		
+		private int halfWidth, halfHeight, halfScale;
+
 		public void newDataSet(Float[] array) {
 			points = array;
 		}
-		
+
 		public void updateSphere(int[] sphere) {
 			this.sphere = sphere;
 		}
 
 		@Override
 		public void paint(Graphics canvas) {
+
+			halfWidth = this.getWidth() / 2;
+			halfHeight = this.getHeight() / 2;
+			halfScale = (halfHeight > halfWidth) ? halfWidth : halfHeight;
+
 			canvas.drawString("XX", 0, 0);
-	
+
 			// Draw the graph lines
 			canvas.drawLine(halfWidth, 0, halfWidth, halfHeight * 2);
 			canvas.drawLine(0, halfHeight, halfWidth * 2, halfHeight);
-	
+
 			// Draw the points
 			int x = 0, y = 0;
 			for (int i = 0; i < points.length; i += 2) {
@@ -100,26 +122,27 @@ public class MagnetometerCal implements OnMagCalibrationListner {
 			canvas.drawOval(x - 5 / 2, y - 5 / 2, 5, 5);
 
 			// Draw the estimated Sphere
-			if (sphere!=null) {
+			if (sphere != null) {
 				x = mapToImgX(sphere[0]);
 				y = mapToImgY(sphere[1]);
 				int width = (int) scale(sphere[2]);
-				int height = (int)scale(sphere[3]);
-				canvas.drawOval(x-width,y-height,width*2,height*2);
+				int height = (int) scale(sphere[3]);
+				canvas.drawOval(x - width, y - height, width * 2, height * 2);
 			}
-			
 		}
 
 		private int mapToImgX(float coord) {
 			return (int) (scale(coord) + halfWidth);
 		}
+
 		private int mapToImgY(float coord) {
 			return (int) (-scale(coord) + halfHeight);
 		}
+
 		private float scale(float value) {
-			return SCALE_FACTOR*halfScale * value;
-		}		
-	
+			return SCALE_FACTOR * halfScale * value;
+		}
+
 	}
 
 }
