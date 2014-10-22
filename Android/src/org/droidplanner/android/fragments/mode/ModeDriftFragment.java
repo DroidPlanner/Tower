0775040e313c806c732668e0a016d6e9ffe7f5bc
ne0fhyk <fhuyakou@gmail.com>
2014-10-21 23:35:11 -0700

    code cleanup.

--- Android/src/org/droidplanner/android/fragments/mode/ModeDriftFragment.java
+++ Android/src/org/droidplanner/android/fragments/mode/ModeDriftFragment.java
@@ -12,7 +12,6 @@ public class ModeDriftFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_mode_drift, container, false);
-		return view;
+		return inflater.inflate(R.layout.fragment_mode_drift, container, false);
 	}
 }
