7447dcd1bfede3def9e099a4e09eb5f1f5148d70
Arthur Benemann <arthur.benemann@gmail.com>
2014-10-29 14:05:18 -0700

    Editor: Creating the survey fragment should change the survey properties
    Fixed this by updating the views before adding the change listeners

--- Android/src/org/droidplanner/android/proxy/mission/item/fragments/MissionSurveyFragment.java
+++ Android/src/org/droidplanner/android/proxy/mission/item/fragments/MissionSurveyFragment.java
@@ -116,12 +116,13 @@ public class MissionSurveyFragment extends MissionDetailFragment implements OnCl
 		innerWPsCheckbox.setOnClickListener(this);
 		cameraSpinner.setOnSpinnerItemSelectedListener(this);
 
+		updateViews();
+		
         mAnglePicker.addChangingListener(this);
         mOverlapPicker.addChangingListener(this);
         mSidelapPicker.addChangingListener(this);
         mAltitudePicker.addChangingListener(this);
 
-		updateViews();
 
         typeSpinner.setSelection(commandAdapter.getPosition(MissionItemType.SURVEY));
 	}
