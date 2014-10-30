2af35bbd14a213fdfdc4503c920f37ab3f509e89
Arthur Benemann <arthur.benemann@gmail.com>
2014-10-30 16:23:06 -0700

    Layout: Clean-up of the EPM and camera fragments

--- Android/src/org/droidplanner/android/proxy/mission/item/fragments/MissionEpmGrabberFragment.java
+++ Android/src/org/droidplanner/android/proxy/mission/item/fragments/MissionEpmGrabberFragment.java
@@ -1,14 +1,12 @@
 package org.droidplanner.android.proxy.mission.item.fragments;
 
 import org.droidplanner.R;
-import org.droidplanner.android.widgets.spinnerWheel.CardWheelHorizontalView;
 import org.droidplanner.core.mission.MissionItemType;
 
 import android.os.Bundle;
 import android.view.View;
 
-public class MissionEpmGrabberFragment extends MissionDetailFragment implements
-		CardWheelHorizontalView.OnCardWheelChangedListener {
+public class MissionEpmGrabberFragment extends MissionDetailFragment {
 
 	@Override
 	protected int getResource() {
@@ -19,9 +17,5 @@ public class MissionEpmGrabberFragment extends MissionDetailFragment implements
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		typeSpinner.setSelection(commandAdapter.getPosition(MissionItemType.EPM_GRIPPER));
-	}
-
-	@Override
-	public void onChanged(CardWheelHorizontalView wheel, int oldValue, int newValue) {
 	}
 }
