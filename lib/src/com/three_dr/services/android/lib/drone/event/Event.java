e190dbbdfdd84e480dd8906e7ca2716629c55718
ne0fhyk <fhuyakou@gmail.com>
2014-10-29 21:09:59 -0700

    in the process of updating the ParamsFragment screen to make use of the new api.

---
+++ lib/src/com/three_dr/services/android/lib/drone/event/Event.java
@@ -0,0 +1,25 @@
+package com.three_dr.services.android.lib.drone.event;
+
+/**
+ * Stores all possible drone events.
+ */
+public class Event {
+
+    private static final String CLAZZ_NAME = Event.class.getName();
+
+    public static final String EVENT_CONNECTED = CLAZZ_NAME + ".CONNECTED";
+
+    public static final String EVENT_DISCONNECTED = CLAZZ_NAME + ".DISCONNECTED";
+
+    public static final String EVENT_PARAMETERS_REFRESH_STARTED = CLAZZ_NAME +
+            ".PARAMETERS_REFRESH_STARTED";
+
+    public static final String EVENT_PARAMETERS_REFRESH_ENDED = CLAZZ_NAME +
+            ".PARAMETERS_REFRESH_ENDED";
+
+    public static final String EVENT_PARAMETERS_RECEIVED = CLAZZ_NAME + ".PARAMETERS_RECEIVED";
+
+    public static final String EVENT_TYPE_UPDATED = CLAZZ_NAME + ".TYPE_UPDATED";
+
+
+}
