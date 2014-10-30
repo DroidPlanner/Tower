98d4ad41b0b71a8e1ae57db0aab0337da20f1e57
ne0fhyk <fhuyakou@gmail.com>
2014-10-29 21:12:26 -0700

    Squashed 'Libraries/3DR-Services-Library/' changes from d73a96a..41165d9
    41165d9 updated the parameter class to include metadata information. added an utility class to load parameter metadata from the assets resources. stub implementation for the drone manager service, whose task will be to provide access to the dp_kernel api.
    
    git-subtree-dir: Libraries/3DR-Services-Library
    git-subtree-split: 41165d9ffcb80ab941293d23ac64d6162fc6863b

--- lib/src/com/three_dr/services/android/lib/drone/property/Parameter.java
+++ lib/src/com/three_dr/services/android/lib/drone/property/Parameter.java
@@ -8,9 +8,16 @@ import android.os.Parcelable;
  */
 public class Parameter implements Parcelable {
 
-    private final String name;
-    private final double value;
-    private final int type;
+    private String name;
+    private double value;
+    private int type;
+
+    private String displayName;
+    private String description;
+
+    private String units;
+    private String range;
+    private String values;
 
     public Parameter(String name, double value, int type) {
         this.name = name;
@@ -22,46 +29,64 @@ public class Parameter implements Parcelable {
         return name;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
     public double getValue() {
         return value;
+    }
+
+    public void setValue(double value){
+        this.value = value;
     }
 
     public int getType() {
         return type;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Parameter)) return false;
-
-        Parameter parameter = (Parameter) o;
-
-        if (type != parameter.type) return false;
-        if (Double.compare(parameter.value, value) != 0) return false;
-        if (name != null ? !name.equals(parameter.name) : parameter.name != null) return false;
-
-        return true;
+    public void setType(int type){
+        this.type = type;
     }
 
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = name != null ? name.hashCode() : 0;
-        temp = Double.doubleToLongBits(value);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + type;
-        return result;
+    public String getDisplayName() {
+        return displayName;
     }
 
-    @Override
-    public String toString() {
-        return "Parameter{" +
-                "name='" + name + '\'' +
-                ", value=" + value +
-                ", type=" + type +
-                '}';
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getValues() {
+        return values;
+    }
+
+    public void setValues(String values) {
+        this.values = values;
     }
 
     @Override
@@ -74,15 +99,25 @@ public class Parameter implements Parcelable {
         dest.writeString(this.name);
         dest.writeDouble(this.value);
         dest.writeInt(this.type);
+        dest.writeString(this.displayName);
+        dest.writeString(this.description);
+        dest.writeString(this.units);
+        dest.writeString(this.range);
+        dest.writeString(this.values);
     }
 
     private Parameter(Parcel in) {
         this.name = in.readString();
         this.value = in.readDouble();
         this.type = in.readInt();
+        this.displayName = in.readString();
+        this.description = in.readString();
+        this.units = in.readString();
+        this.range = in.readString();
+        this.values = in.readString();
     }
 
-    public static final Parcelable.Creator<Parameter> CREATOR = new Parcelable.Creator<Parameter>() {
+    public static final Creator<Parameter> CREATOR = new Creator<Parameter>() {
         public Parameter createFromParcel(Parcel source) {
             return new Parameter(source);
         }
