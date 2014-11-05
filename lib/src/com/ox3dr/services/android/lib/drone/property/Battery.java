f945b194f4b9d4a3bbc5d78538ab225ff91b928f
ne0fhyk <fhuyakou@gmail.com>
2014-11-04 23:19:50 -0800

    added battery discharge property to the Battery class.

--- lib/src/com/ox3dr/services/android/lib/drone/property/Battery.java
+++ lib/src/com/ox3dr/services/android/lib/drone/property/Battery.java
@@ -11,11 +11,14 @@ public class Battery implements Parcelable {
     private final double batteryVoltage;
     private final double batteryRemain;
     private final double batteryCurrent;
+    private final Double batteryDischarge;
 
-    public Battery(double batteryVoltage, double batteryRemain, double batteryCurrent) {
+    public Battery(double batteryVoltage, double batteryRemain, double batteryCurrent,
+                   Double batteryDischarge) {
         this.batteryVoltage = batteryVoltage;
         this.batteryRemain = batteryRemain;
         this.batteryCurrent = batteryCurrent;
+        this.batteryDischarge = batteryDischarge;
     }
 
     public double getBatteryVoltage() {
@@ -30,40 +33,8 @@ public class Battery implements Parcelable {
         return batteryCurrent;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Battery)) return false;
-
-        Battery battery = (Battery) o;
-
-        if (Double.compare(battery.batteryCurrent, batteryCurrent) != 0) return false;
-        if (Double.compare(battery.batteryRemain, batteryRemain) != 0) return false;
-        if (Double.compare(battery.batteryVoltage, batteryVoltage) != 0) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        temp = Double.doubleToLongBits(batteryVoltage);
-        result = (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(batteryRemain);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(batteryCurrent);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Battery{" +
-                "batteryVoltage=" + batteryVoltage +
-                ", batteryRemain=" + batteryRemain +
-                ", batteryCurrent=" + batteryCurrent +
-                '}';
+    public Double getBatteryDischarge() {
+        return batteryDischarge;
     }
 
     @Override
@@ -76,15 +47,17 @@ public class Battery implements Parcelable {
         dest.writeDouble(this.batteryVoltage);
         dest.writeDouble(this.batteryRemain);
         dest.writeDouble(this.batteryCurrent);
+        dest.writeValue(this.batteryDischarge);
     }
 
     private Battery(Parcel in) {
         this.batteryVoltage = in.readDouble();
         this.batteryRemain = in.readDouble();
         this.batteryCurrent = in.readDouble();
+        this.batteryDischarge = (Double) in.readValue(Double.class.getClassLoader());
     }
 
-    public static final Parcelable.Creator<Battery> CREATOR = new Parcelable.Creator<Battery>() {
+    public static final Creator<Battery> CREATOR = new Creator<Battery>() {
         public Battery createFromParcel(Parcel source) {
             return new Battery(source);
         }
