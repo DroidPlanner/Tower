bf33495cdaa9f5eed7d24e6c96a3e6fd0d8a5eb6
ne0fhyk <fhuyakou@gmail.com>
2014-11-05 16:56:50 -0800

    updated structure of the VehicleMode property.

--- lib/src/com/ox3dr/services/android/lib/drone/property/VehicleMode.java
+++ lib/src/com/ox3dr/services/android/lib/drone/property/VehicleMode.java
@@ -6,13 +6,52 @@ import android.os.Parcelable;
 /**
  * Created by fhuya on 10/28/14.
  */
-public class VehicleMode implements Parcelable {
+public enum VehicleMode implements Parcelable {
+
+    PLANE_MANUAL(0, Type.TYPE_PLANE, "Manual"),
+    PLANE_CIRCLE(1, Type.TYPE_PLANE, "Circle"),
+    PLANE_STABILIZE(2, Type.TYPE_PLANE, "Stabilize"),
+    PLANE_TRAINING(3, Type.TYPE_PLANE, "Training"),
+    PLANE_ACRO(4, Type.TYPE_PLANE, "Acro"),
+    PLANE_FLY_BY_WIRE_A(5, Type.TYPE_PLANE, "FBW A"),
+    PLANE_FLY_BY_WIRE_B(6, Type.TYPE_PLANE, "FBW B"),
+    PLANE_CRUISE(7, Type.TYPE_PLANE, "Cruise"),
+    PLANE_AUTOTUNE(8, Type.TYPE_PLANE, "Autotune"),
+    PLANE_AUTO(10, Type.TYPE_PLANE, "Auto"),
+    PLANE_RTL(11, Type.TYPE_PLANE, "RTL"),
+    PLANE_LOITER(12, Type.TYPE_PLANE, "Loiter"),
+    PLANE_GUIDED(15, Type.TYPE_PLANE, "Guided"),
+
+    COPTER_STABILIZE(0, Type.TYPE_COPTER, "Stabilize"),
+    COPTER_ACRO(1, Type.TYPE_COPTER, "Acro"),
+    COPTER_ALT_HOLD(2, Type.TYPE_COPTER, "Alt Hold"),
+    COPTER_AUTO(3, Type.TYPE_COPTER, "Auto"),
+    COPTER_GUIDED(4, Type.TYPE_COPTER, "Guided"),
+    COPTER_LOITER(5, Type.TYPE_COPTER, "Loiter"),
+    COPTER_RTL(6, Type.TYPE_COPTER, "RTL"),
+    COPTER_CIRCLE(7, Type.TYPE_COPTER, "Circle"),
+    COPTER_LAND(9, Type.TYPE_COPTER, "Land"),
+    COPTER_DRIFT(11, Type.TYPE_COPTER, "Drift"),
+    COPTER_SPORT(13, Type.TYPE_COPTER, "Sport"),
+    COPTER_FLIP(14, Type.TYPE_COPTER, "Flip"),
+    COPTER_AUTOTUNE(15, Type.TYPE_COPTER, "Autotune"),
+    COPTER_POSHOLD(16, Type.TYPE_COPTER, "PosHold"),
+
+    ROVER_MANUAL(0, Type.TYPE_ROVER, "Manual"),
+    ROVER_LEARNING(2, Type.TYPE_ROVER, "Learning"),
+    ROVER_STEERING(3, Type.TYPE_ROVER, "Steering"),
+    ROVER_HOLD(4, Type.TYPE_ROVER, "Hold"),
+    ROVER_AUTO(10, Type.TYPE_ROVER, "Auto"),
+    ROVER_RTL(11, Type.TYPE_ROVER, "RTL"),
+    ROVER_GUIDED(15, Type.TYPE_ROVER, "Guided"),
+    ROVER_INITIALIZING(16, Type.TYPE_ROVER, "Initializing");
+
 
     private final int mode;
     private final int droneType;
     private final String label;
 
-    public VehicleMode(int mode, int droneType, String label){
+    VehicleMode(int mode, int droneType, String label){
         this.mode = mode;
         this.droneType = droneType;
         this.label = label;
@@ -31,30 +70,25 @@ public class VehicleMode implements Parcelable {
     }
 
     @Override
-    public int describeContents() {
+    public int describeContents(){
         return 0;
     }
 
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(this.mode);
-        dest.writeInt(this.droneType);
-        dest.writeString(this.label);
+    public void writeToParcel(final Parcel dest, final int flags){
+        dest.writeString(name());
     }
 
-    private VehicleMode(Parcel in) {
-        this.mode = in.readInt();
-        this.droneType = in.readInt();
-        this.label = in.readString();
-    }
-
-    public static final Parcelable.Creator<VehicleMode> CREATOR = new Parcelable.Creator<VehicleMode>() {
+    public static final Creator<VehicleMode> CREATOR = new Creator<VehicleMode>() {
+        @Override
         public VehicleMode createFromParcel(Parcel source) {
-            return new VehicleMode(source);
+            return VehicleMode.valueOf(source.readString());
         }
 
+        @Override
         public VehicleMode[] newArray(int size) {
             return new VehicleMode[size];
         }
     };
 }
+
