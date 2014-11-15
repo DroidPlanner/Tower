4199c46d5b48aac86fd03833709b0278b2831877
ne0fhyk <fhuyakou@gmail.com>
2014-11-15 11:26:12 -0800

    dialog layout update.

--- lib/src/main/java/com/o3dr/android/client/utils/InstallServiceDialog.java
+++ lib/src/main/java/com/o3dr/android/client/utils/InstallServiceDialog.java
@@ -3,7 +3,7 @@ package com.o3dr.android.client.utils;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,7 +12,7 @@ import com.o3dr.android.client.R;
 /**
  * Created by fhuya on 11/14/14.
  */
-public class InstallServiceDialog extends ActionBarActivity {
+public class InstallServiceDialog extends FragmentActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -33,6 +33,7 @@ public class InstallServiceDialog extends ActionBarActivity {
             public void onClick(View v) {
                 startActivity(new Intent(Intent.ACTION_VIEW,
                         Uri.parse("market://details?id=org.droidplanner.services.android")));
+                finish();
             }
         });
     }
