cb4a3e0c817a98511fccc61bf2ce7394d0d5e742
ne0fhyk <fhuyakou@gmail.com>
2014-11-20 20:00:39 -0800

    removed mapbox map implementation.

--- Android/src/org/droidplanner/android/utils/DroneHelper.java
+++ Android/src/org/droidplanner/android/utils/DroneHelper.java
@@ -6,23 +6,14 @@ import android.content.res.Resources;
 import android.location.Location;
 
 import com.google.android.gms.maps.model.LatLng;
-import com.mapbox.mapboxsdk.api.ILatLng;
 
 public class DroneHelper {
 	static public LatLng CoordToLatLang(Coord2D coord) {
 		return new LatLng(coord.getLat(), coord.getLng());
 	}
 
-	public static com.mapbox.mapboxsdk.geometry.LatLng CoordToLatLng(Coord2D coord) {
-		return new com.mapbox.mapboxsdk.geometry.LatLng(coord.getLat(), coord.getLng());
-	}
-
 	public static Coord2D LatLngToCoord(LatLng point) {
 		return new Coord2D(point.latitude, point.longitude);
-	}
-
-	public static Coord2D ILatLngToCoord(ILatLng point) {
-		return new Coord2D(point.getLatitude(), point.getLongitude());
 	}
 
 	public static Coord2D LocationToCoord(Location location) {
