e74ea44ef4073008d882eec7394283d53ed5a133
ne0fhyk <fhuyakou@gmail.com>
2014-11-30 10:03:52 -0800

    npe bug fix.

--- Android/src/org/droidplanner/android/DroidPlannerApp.java
+++ Android/src/org/droidplanner/android/DroidPlannerApp.java
@@ -325,6 +325,7 @@ public class DroidPlannerApp extends Application implements DroneListener, Servi
 
     @Override
     public void onDroneServiceInterrupted(String errorMsg){
-        Log.e(TAG, errorMsg);
+        if(errorMsg != null)
+            Log.e(TAG, errorMsg);
     }
 }
