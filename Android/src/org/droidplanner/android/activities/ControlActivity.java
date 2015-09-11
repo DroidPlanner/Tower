63c6ea41c9c4cd79133bcd3fc9ed3164d24e8316
ne0fhyk <fhuyakou@gmail.com>
2015-09-11 14:12:32 -0700

    Cleanup of the control activity implementation.

--- Android/src/org/droidplanner/android/activities/ControlActivity.java
+++ Android/src/org/droidplanner/android/activities/ControlActivity.java
@@ -10,7 +10,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 
 import com.MAVLink.common.msg_attitude;
@@ -32,10 +31,8 @@ import com.o3dr.services.android.lib.util.MathUtils;
 
 import org.droidplanner.android.R;
 import org.droidplanner.android.fragments.actionbar.ActionBarTelemFragment;
-import org.droidplanner.android.fragments.widget.telem.WidgetSoloLinkVideo;
-import org.droidplanner.android.widgets.AttitudeIndicator;
+import org.droidplanner.android.fragments.widget.MiniWidgetSoloLinkVideo;
 import org.droidplanner.android.widgets.JoystickView;
-import org.w3c.dom.Attr;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -47,26 +44,27 @@ import timber.log.Timber;
  * Created by Toby on 8/5/2015.
  */
 public class ControlActivity extends DrawerNavigationUI {
-    private JoystickView leftJoystick, rightJoystick;
-    private static final int ignoreVel = ((1<<3) | (1<<4) | (1 << 5));
-    private static final int ignoreAcc = ((1<<6) | (1<<7) | (1 << 8));
-    private static final int ignorePos = ((1<<0) | (1<<1) | (1<<2));
-    private long lastRecieved;
-    private float lastYaw, lastYawSpeed;
+
+    private static final int ignoreVel = ((1 << 3) | (1 << 4) | (1 << 5));
+    private static final int ignoreAcc = ((1 << 6) | (1 << 7) | (1 << 8));
+    private static final int ignorePos = ((1 << 0) | (1 << 1) | (1 << 2));
+
     private static final float MAX_VEL = 5f, MAX_VEL_Z = 5f, MAX_YAW_RATE = 5f;
+
     private final static IntentFilter eventFilter = new IntentFilter();
-    private VehicleMode mode;
-    static{
+    static {
         eventFilter.addAction(AttributeEvent.ATTITUDE_UPDATED);
         eventFilter.addAction(AttributeEvent.STATE_VEHICLE_MODE);
     }
+
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            switch(intent.getAction()){
+            switch (intent.getAction()) {
                 case AttributeEvent.ATTITUDE_UPDATED:
                     attitudeUpdated();
                     break;
+
                 case AttributeEvent.STATE_VEHICLE_MODE:
                     State state = dpApp.getDrone().getAttribute(AttributeType.STATE);
                     mode = state.getVehicleMode();
@@ -74,22 +72,32 @@ public class ControlActivity extends DrawerNavigationUI {
             }
         }
     };
+
+    private JoystickView leftJoystick, rightJoystick;
+
+    private long lastReceived;
+    private float lastYaw, lastYawSpeed;
+    private VehicleMode mode;
+
     private Fragment actionBarFragment;
     private View flightModeIcon;
 
     private void attitudeUpdated() {
         Drone drone = dpApp.getDrone();
-        if(drone == null)
+        if (drone == null)
             return;
+
         Attitude att = drone.getAttribute(AttributeType.ATTITUDE);
         Home home = drone.getAttribute(AttributeType.HOME);
         Gps gps = drone.getAttribute(AttributeType.GPS);
-        if(flightModeIcon == null){
+
+        if (flightModeIcon == null) {
             flightModeIcon = findViewById(R.id.bar_flight_mode_icon);
         }
-        if(att != null && gps != null && gps.isValid() && home != null && home.isValid() && flightModeIcon != null){
+
+        if (att != null && gps != null && gps.isValid() && home != null && home.isValid() && flightModeIcon != null) {
             double heading = MathUtils.getHeadingFromCoordinates(home.getCoordinate(), gps.getPosition());
-            flightModeIcon.animate().rotation((float)((360 + heading - att.getYaw()) % (360))).start();
+            flightModeIcon.animate().rotation((float) ((360 + heading - att.getYaw()) % (360))).start();
         }
     }
 
@@ -97,63 +105,68 @@ public class ControlActivity extends DrawerNavigationUI {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
-        leftJoystick = (JoystickView)findViewById(R.id.left_joystick);
-        rightJoystick = (JoystickView)findViewById(R.id.right_joystick);
-        Bitmap reticle = BitmapFactory.decodeResource(getResources(), R.drawable.ic_control_grey_600_24dp);
+
+        final Bitmap reticle = BitmapFactory.decodeResource(getResources(), R.drawable.ic_control_grey_600_24dp);
+
+        leftJoystick = (JoystickView) findViewById(R.id.left_joystick);
         leftJoystick.setReticle(reticle);
-        rightJoystick.setReticle(reticle);
         leftJoystick.setSpring(JoystickView.Axis.X, true);
         leftJoystick.setSpring(JoystickView.Axis.Y, true);
-        rightJoystick.setSpring(JoystickView.Axis.Y, true);
-        rightJoystick.setSpring(JoystickView.Axis.X, true);
         leftJoystick.setHaptic(JoystickView.Axis.Y, true);
         leftJoystick.setJoystickListener(new JoystickView.JoystickListener() {
             @Override
             public void joystickMoved(float x, float y) {
-                float heading = lastYaw + lastYawSpeed * ((System.currentTimeMillis() - lastRecieved) / 1000f);
+                float heading = computeHeading();
                 sendMove(heading);
                 sendYaw(heading);
             }
         });
+
+        rightJoystick = (JoystickView) findViewById(R.id.right_joystick);
+        rightJoystick.setReticle(reticle);
+        rightJoystick.setSpring(JoystickView.Axis.Y, true);
+        rightJoystick.setSpring(JoystickView.Axis.X, true);
         rightJoystick.setJoystickListener(new JoystickView.JoystickListener() {
             @Override
             public void joystickMoved(float x, float y) {
-                float heading = lastYaw + lastYawSpeed * ((System.currentTimeMillis() - lastRecieved) / 1000f);
+                float heading = computeHeading();
                 sendMove(heading);
             }
         });
-        ScheduledExecutorService scheduledExecutorService =  Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.scheduleAtFixedRate(new Runnable(){
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
-                    float heading = lastYaw + lastYawSpeed * ((System.currentTimeMillis() - lastRecieved)/1000f);
-//                    Timber.d("sending yaw");
-                    sendYaw(heading);
+                float heading = computeHeading();
+                sendYaw(heading);
             }
         }, 0, 33, TimeUnit.MILLISECONDS);
 
 
         FragmentManager fm = getSupportFragmentManager();
         actionBarFragment = fm.findFragmentById(R.id.widget_view);
-        if(!(actionBarFragment instanceof WidgetSoloLinkVideo)){
-            actionBarFragment = new WidgetSoloLinkVideo();
+        if (!(actionBarFragment instanceof MiniWidgetSoloLinkVideo)) {
+            actionBarFragment = new MiniWidgetSoloLinkVideo();
             fm.beginTransaction().replace(R.id.widget_view, actionBarFragment).commit();
         }
+    }
 
+    private float computeHeading(){
+        return lastYaw + lastYawSpeed * ((System.currentTimeMillis() - lastReceived) / 1000f);
     }
 
     private void sendYaw(float heading) {
-        if(mode == null || !mode.equals(VehicleMode.COPTER_GUIDED))
+        if (mode == null || !mode.equals(VehicleMode.COPTER_GUIDED))
             return;
+
         float yaw = leftJoystick.getAxis(JoystickView.Axis.X);
-        heading/= Math.PI;
+        heading /= Math.PI;
         heading *= 180f;
-        if(Math.abs(yaw) > 0.05) {
+        if (Math.abs(yaw) > 0.05) {
             msg_command_long msgYaw = new msg_command_long();
             msgYaw.command = MAV_CMD.MAV_CMD_CONDITION_YAW;
-            msgYaw.param1 = (360 + (heading + yaw*30f)) % 360;
-//            Timber.d("yaw: %f", msgYaw.param1);
+            msgYaw.param1 = (360 + (heading + yaw * 30f)) % 360;
             msgYaw.param2 = Math.abs(yaw) * 30f;
             msgYaw.param3 = Math.signum(yaw);
             msgYaw.param4 = 0;
@@ -161,12 +174,12 @@ public class ControlActivity extends DrawerNavigationUI {
         }
     }
 
-    private void sendMove(float heading){
+    private void sendMove(float heading) {
         float throttle = leftJoystick.getAxis(JoystickView.Axis.Y);
         float x = rightJoystick.getAxis(JoystickView.Axis.X);
         float y = rightJoystick.getAxis(JoystickView.Axis.Y);
         float yaw = leftJoystick.getAxis(JoystickView.Axis.X);
-        if(mode != null && mode.equals(VehicleMode.COPTER_GUIDED)) {
+        if (mode != null && mode.equals(VehicleMode.COPTER_GUIDED)) {
             if (x != 0 && y != 0) {
                 double theta = Math.atan(y / x);
                 if (theta < 0) {
@@ -181,14 +194,14 @@ public class ControlActivity extends DrawerNavigationUI {
                 y = (float) (Math.sin(heading + theta) * magnitude);
 
             }
-        }else{
+        } else {
             y = -y;
             throttle = -throttle;
         }
         Timber.d("x: %f, y: %f, z: %f, yaw: %f", x, y, throttle, yaw);
         msg_set_position_target_local_ned msg = new msg_set_position_target_local_ned();
         msg.vz = throttle * MAX_VEL_Z;
-        msg.vy = y *  MAX_VEL;
+        msg.vy = y * MAX_VEL;
         msg.vx = x * MAX_VEL;
         msg.yaw_rate = yaw * MAX_YAW_RATE;
         msg.type_mask = ignoreAcc | ignorePos;
@@ -196,7 +209,7 @@ public class ControlActivity extends DrawerNavigationUI {
     }
 
     @Override
-    protected void addToolbarFragment(){
+    protected void addToolbarFragment() {
         final int toolbarId = getToolbarId();
         final FragmentManager fm = getSupportFragmentManager();
         Fragment actionBarTelem = fm.findFragmentById(toolbarId);
@@ -234,14 +247,21 @@ public class ControlActivity extends DrawerNavigationUI {
         dpApp.getDrone().addMavlinkObserver(new MavlinkObserver() {
             @Override
             public void onMavlinkMessageReceived(MavlinkMessageWrapper mavlinkMessageWrapper) {
-                if(mavlinkMessageWrapper.getMavLinkMessage().msgid == msg_attitude.MAVLINK_MSG_ID_ATTITUDE){
-                    msg_attitude msg = (msg_attitude)mavlinkMessageWrapper.getMavLinkMessage();
+                if (mavlinkMessageWrapper.getMavLinkMessage().msgid == msg_attitude.MAVLINK_MSG_ID_ATTITUDE) {
+                    msg_attitude msg = (msg_attitude) mavlinkMessageWrapper.getMavLinkMessage();
                     lastYaw = msg.yaw;
 //                    Timber.d("yaw: %f",lastYaw);
                     lastYawSpeed = msg.yawspeed;
-                    lastRecieved = System.currentTimeMillis();
+                    lastReceived = System.currentTimeMillis();
                 }
             }
         });
+    }
+
+    private void updateYawParams(){
+        final Drone drone = dpApp.getDrone();
+        final Attitude attitude = drone.getAttribute(AttributeType.ATTITUDE);
+        lastYaw = (float) attitude.getYaw();
+//        lastYawSpeed =
     }
 }
