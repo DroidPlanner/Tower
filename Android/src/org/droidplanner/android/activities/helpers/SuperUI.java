920f6e98eb64cffd7a3f49c40998165d2c1201d7
busyStone <jlu.xzp@gmail.com>
2015-11-12 17:05:12 +0800

    [M] kill switch should be available only when preference enabled and the drone support the property

--- Android/src/org/droidplanner/android/activities/helpers/SuperUI.java
+++ Android/src/org/droidplanner/android/activities/helpers/SuperUI.java
@@ -253,49 +253,55 @@ public abstract class SuperUI extends AppCompatActivity implements DroidPlannerA
         final MenuItem toggleConnectionItem = menu.findItem(R.id.menu_connect);
 
         Drone drone = dpApp.getDrone();
-        if (drone != null && drone.isConnected()) {
-            menu.setGroupEnabled(R.id.menu_group_connected, true);
-            menu.setGroupVisible(R.id.menu_group_connected, true);
-
-            final MenuItem killSwitchItem = menu.findItem(R.id.menu_kill_switch);
-            final boolean isKillEnabled = mAppPrefs.isKillSwitchEnabled();
-            if(killSwitchItem != null && isKillEnabled) {
-                CapabilityApi.getApi(drone).checkFeatureSupport(CapabilityApi.FeatureIds.KILL_SWITCH, new CapabilityApi.FeatureSupportListener() {
-                    @Override
-                    public void onFeatureSupportResult(String s, int i, Bundle bundle) {
-                        switch (i) {
-                            case CapabilityApi.FEATURE_SUPPORTED:
-                                killSwitchItem.setEnabled(true);
-                                killSwitchItem.setVisible(true);
-                                break;
-
-                            default:
-                                killSwitchItem.setEnabled(false);
-                                killSwitchItem.setVisible(false);
-                                break;
-                        }
-                    }
-                });
-
-            }
-
-            final boolean areMissionMenusEnabled = enableMissionMenus();
-
-            final MenuItem sendMission = menu.findItem(R.id.menu_upload_mission);
-            sendMission.setEnabled(areMissionMenusEnabled);
-            sendMission.setVisible(areMissionMenusEnabled);
-
-            final MenuItem loadMission = menu.findItem(R.id.menu_download_mission);
-            loadMission.setEnabled(areMissionMenusEnabled);
-            loadMission.setVisible(areMissionMenusEnabled);
-
-            toggleConnectionItem.setTitle(R.string.menu_disconnect);
-        } else {
+        if (drone == null || !drone.isConnected()){
             menu.setGroupEnabled(R.id.menu_group_connected, false);
             menu.setGroupVisible(R.id.menu_group_connected, false);
 
             toggleConnectionItem.setTitle(R.string.menu_connect);
+
+            return super.onCreateOptionsMenu(menu);
         }
+
+        menu.setGroupEnabled(R.id.menu_group_connected, true);
+        menu.setGroupVisible(R.id.menu_group_connected, true);
+
+        final MenuItem killSwitchItem = menu.findItem(R.id.menu_kill_switch);
+        killSwitchItem.setEnabled(false);
+        killSwitchItem.setVisible(false);
+
+        final boolean isKillEnabled = mAppPrefs.isKillSwitchEnabled();
+        if(killSwitchItem != null && isKillEnabled) {
+            CapabilityApi.getApi(drone).checkFeatureSupport(CapabilityApi.FeatureIds.KILL_SWITCH, new CapabilityApi.FeatureSupportListener() {
+                @Override
+                public void onFeatureSupportResult(String s, int i, Bundle bundle) {
+                    switch (i) {
+                        case CapabilityApi.FEATURE_SUPPORTED:
+                            killSwitchItem.setEnabled(true);
+                            killSwitchItem.setVisible(true);
+                            break;
+
+                        default:
+                            killSwitchItem.setEnabled(false);
+                            killSwitchItem.setVisible(false);
+                            break;
+                    }
+                }
+            });
+
+        }
+
+        final boolean areMissionMenusEnabled = enableMissionMenus();
+
+        final MenuItem sendMission = menu.findItem(R.id.menu_upload_mission);
+        sendMission.setEnabled(areMissionMenusEnabled);
+        sendMission.setVisible(areMissionMenusEnabled);
+
+        final MenuItem loadMission = menu.findItem(R.id.menu_download_mission);
+        loadMission.setEnabled(areMissionMenusEnabled);
+        loadMission.setVisible(areMissionMenusEnabled);
+
+        toggleConnectionItem.setTitle(R.string.menu_disconnect);
+
         return super.onCreateOptionsMenu(menu);
     }
 
