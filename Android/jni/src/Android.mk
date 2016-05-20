4ec755dd2973af9be0d25d76b335d40e6c3595b4
ne0fhyk <fhuyakou@gmail.com>
2016-05-20 13:30:29 -0700

    Edit build files to fix compilation issues

--- Android/jni/src/Android.mk
+++ Android/jni/src/Android.mk
@@ -6,7 +6,7 @@ include /Users/fredia/Playground/libs/OpenCV-android-sdk/sdk/native/jni/OpenCV.m
 
 LOCAL_MODULE    := tracker-jni
 
-#LOCAL_C_INCLUDES := 3rdparty/cvblobs
+LOCAL_C_INCLUDES := 3rdparty/cvblobs
 
 LOCAL_SRC_FILES := native.cpp libopentld/imacq/ImAcq.cpp libopentld/mftracker/BB.cpp libopentld/mftracker/BBPredict.cpp libopentld/mftracker/FBTrack.cpp libopentld/mftracker/Lk.cpp libopentld/mftracker/Median.cpp libopentld/tld/TrackerUtils.cpp libopentld/tld/Clustering.cpp libopentld/tld/DetectionResult.cpp libopentld/tld/DetectorCascade.cpp libopentld/tld/EnsembleClassifier.cpp libopentld/tld/ForegroundDetector.cpp libopentld/tld/MedianFlowTracker.cpp libopentld/tld/NNClassifier.cpp libopentld/tld/TLD.cpp libopentld/tld/TLDUtil.cpp libopentld/tld/VarianceFilter.cpp 3rdparty/cvblobs/blob.cpp 3rdparty/cvblobs/BlobContour.cpp 3rdparty/cvblobs/BlobOperators.cpp 3rdparty/cvblobs/BlobProperties.cpp 3rdparty/cvblobs/BlobResult.cpp 3rdparty/cvblobs/ComponentLabeling.cpp
     
