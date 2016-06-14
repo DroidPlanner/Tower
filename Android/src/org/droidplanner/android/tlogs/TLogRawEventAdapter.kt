4ac8791605555cef98018e0cbafda4e0299800ee
ne0fhyk <fhuyakou@gmail.com>
2016-06-14 00:26:26 -0700

    Add recycler view adapter for the TLogRawViewer

---
+++ Android/src/org/droidplanner/android/tlogs/TLogRawEventAdapter.kt
@@ -0,0 +1,46 @@
+package org.droidplanner.android.tlogs
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.o3dr.android.client.utils.data.tlog.TLogParser
+import org.droidplanner.android.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * @author ne0fhyk (Fredia Huya-Kouadio)
+ */
+class TLogRawEventAdapter : RecyclerView.Adapter<TLogRawEventAdapter.ViewHolder>() {
+
+    class ViewHolder(eventView: View, val eventInfo: TextView, val eventTimestamp: TextView) :
+            RecyclerView.ViewHolder(eventView)
+
+    companion object {
+        private val dateFormatter = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.US)
+    }
+    private val loadedEvents = ArrayList<TLogParser.Event>()
+
+    fun loadTLogEvents(events: List<TLogParser.Event>){
+        loadedEvents.clear()
+        loadedEvents.addAll(events)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = loadedEvents.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val event = loadedEvents[position]
+        holder.eventInfo.text = event.mavLinkMessage.toString()
+        holder.eventTimestamp.text = dateFormatter.format(Date(event.timestamp))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder? {
+        val eventView = LayoutInflater.from(parent?.context).inflate(R.layout.list_item_tlog_raw_event, parent, false)
+        val eventTimestamp = eventView.findViewById(R.id.event_timestamp) as TextView
+        val eventInfo = eventView.findViewById(R.id.event_info) as TextView
+        return ViewHolder(eventView, eventInfo, eventTimestamp)
+    }
+}
--- Android/src/org/droidplanner/android/tlogs/TLogRawViewer.kt
+++ Android/src/org/droidplanner/android/tlogs/TLogRawViewer.kt
@@ -1,9 +1,76 @@
 package org.droidplanner.android.tlogs
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.o3dr.android.client.utils.data.tlog.TLogParser
+import org.droidplanner.android.R
+import org.droidplanner.android.view.NiceProgressView
 
 /**
  * @author ne0fhyk (Fredia Huya-Kouadio)
  */
-class TLogRawViewer : Fragment() {
+class TLogRawViewer : Fragment(), TLogDataSubscriber {
+
+    private val tlogEventsAdapter = TLogRawEventAdapter()
+
+    private val noTLogView by lazy {
+        getView()?.findViewById(R.id.no_tlog_selected) as TextView?
+    }
+
+    private val dataLoadProgress by lazy {
+        getView()?.findViewById(R.id.tlog_data_load_progress) as NiceProgressView?
+    }
+
+    private val rawData by lazy {
+        getView()?.findViewById(R.id.tlog_raw_data) as RecyclerView?
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tlog_raw_viewer, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rawData?.apply{
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(getContext())
+            adapter = tlogEventsAdapter
+        }
+    }
+
+    override fun onTLogDataLoaded(events: List<TLogParser.Event>){
+        // Refresh the recycler view
+        tlogEventsAdapter.loadTLogEvents(events)
+        if(events.isEmpty()){
+            stateNoData()
+        }
+        else{
+            stateDataLoaded()
+        }
+    }
+
+    private fun stateNoData(){
+        noTLogView?.visibility = View.VISIBLE
+        dataLoadProgress?.visibility = View.GONE
+        rawData?.visibility = View.GONE
+    }
+
+    private fun stateLoadingData(){
+        noTLogView?.visibility = View.GONE
+        dataLoadProgress?.visibility = View.VISIBLE
+        rawData?.visibility = View.GONE
+    }
+
+    private fun stateDataLoaded(){
+        noTLogView?.visibility = View.GONE
+        dataLoadProgress?.visibility = View.GONE
+        rawData?.visibility = View.VISIBLE
+    }
 }
