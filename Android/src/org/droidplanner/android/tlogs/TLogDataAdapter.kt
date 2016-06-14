95bb297aee4a50691a59c7bdf52b53ecbeb5b25e
ne0fhyk <fhuyakou@gmail.com>
2016-06-14 01:44:45 -0700

    Implement TLogDataAdapter

--- Android/src/org/droidplanner/android/tlogs/TLogDataAdapter.kt
+++ Android/src/org/droidplanner/android/tlogs/TLogDataAdapter.kt
@@ -1,11 +1,15 @@
 package org.droidplanner.android.tlogs
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.o3dr.android.client.utils.data.tlog.TLogUtils
 import org.droidplanner.android.DroidPlannerApp
+import org.droidplanner.android.R
 import java.io.File
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -15,10 +19,16 @@ class TLogDataAdapter(app: DroidPlannerApp) : RecyclerView.Adapter<TLogDataAdapt
 
     companion object {
         private const val TLOG_FILENAME_EXT = ".tlog"
+        private val dateFormatter = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.US)
+    }
+
+    interface TLogSelectionListener {
+        fun onTLogSelected(tlogFile: File)
     }
 
     class ViewHolder(val container: View, val dataTimestamp: TextView) : RecyclerView.ViewHolder(container)
 
+    private var tlogSelectionListener : TLogSelectionListener? = null
     private val tlogsFiles = ArrayList<Pair<Date, File>>()
 
     init {
@@ -32,18 +42,36 @@ class TLogDataAdapter(app: DroidPlannerApp) : RecyclerView.Adapter<TLogDataAdapt
 
             for(file in files){
                 // Parse the filename to retrieve the creation date
-                val filename = file.nameWithoutExtension
+                val filename = file.name
+                val creationDate = TLogUtils.parseTLogConnectionTimestamp(filename) ?: continue
 
+                tlogsFiles.add(Pair(creationDate, file))
+            }
+
+            //Sort the tlog files per date
+            if(tlogsFiles.isNotEmpty()){
+                //TODO: use Collections.sort for the sorting
             }
         }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-
+    fun setTLogSelectionListener(listener: TLogSelectionListener?){
+        this.tlogSelectionListener = listener
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder? {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val (date, tlogFile) = tlogsFiles[position]
+        holder.dataTimestamp.text = dateFormatter.format(date)
+        holder.dataTimestamp.setOnClickListener {
+            // Notify the listener of the selected tlog file
+            tlogSelectionListener?.onTLogSelected(tlogFile)
+        }
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder? {
+        val containerView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_tlog_data, parent, false)
+        val dataTimestamp = containerView.findViewById(R.id.tlog_data_timestamp) as TextView
+        return ViewHolder(containerView, dataTimestamp)
     }
 
     override fun getItemCount() = tlogsFiles.size
--- Android/src/org/droidplanner/android/tlogs/TLogDataPicker.kt
+++ Android/src/org/droidplanner/android/tlogs/TLogDataPicker.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.droidplanner.android.DroidPlannerApp
 import org.droidplanner.android.R
 
 /**
@@ -28,6 +29,6 @@ class TLogDataPicker : DialogFragment(){
         val layoutMgr = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         tlogsView?.setLayoutManager(layoutMgr)
 
-        tlogsView?.adapter = TLogsAdapter()
+        tlogsView?.adapter = TLogDataAdapter(activity.getApplication() as DroidPlannerApp)
     }
 }
