864614f2a0a6b87f72cea2c3f5398c878358e8b8
ne0fhyk <fhuyakou@gmail.com>
2016-07-12 14:12:04 -0700

    Add todo for the remaining tlog history task

--- Android/src/org/droidplanner/android/tlog/event/TLogEventDetail.kt
+++ Android/src/org/droidplanner/android/tlog/event/TLogEventDetail.kt
@@ -2,6 +2,9 @@ package org.droidplanner.android.tlog.event
 
 import android.support.v4.app.Fragment
 
+/**
+ * TODO: Complete implementation for the TLog event detail class
+ */
 class TLogEventDetail : Fragment(){
 
 }
