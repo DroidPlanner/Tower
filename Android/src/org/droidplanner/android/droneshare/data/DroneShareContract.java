5a816c2da2f6a95590a469ac8dad43fe892fbc42
ne0fhyk <fhuyakou@gmail.com>
2016-08-02 02:22:37 -0700

    Clean unused tables

--- Android/src/org/droidplanner/android/droneshare/data/DroneShareContract.java
+++ Android/src/org/droidplanner/android/droneshare/data/DroneShareContract.java
@@ -1,6 +1,5 @@
 package org.droidplanner.android.droneshare.data;
 
-import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -10,21 +9,19 @@ import android.provider.BaseColumns;
 public final class DroneShareContract {
 
     static final String DB_NAME = "droneshare";
-    static final int DB_VERSION = 1;
+    static final int DB_VERSION = 2;
 
     private DroneShareContract(){}
 
     static String[] getSQLCreateEntries(){
         return new String[]{
             UploadData.SQL_CREATE_ENTRIES,
-            TLogMetadata.SQL_CREATE_ENTRIES
         };
     }
 
     static String[] getSQLDeleteEntries(){
         return new String[]{
             UploadData.SQL_DELETE_ENTRIES,
-            TLogMetadata.SQL_DELETE_ENTRIES
         };
     }
 
@@ -44,43 +41,5 @@ public final class DroneShareContract {
                 " )";
 
         static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
-    }
-
-    public static final class TLogMetadata implements BaseColumns {
-        static final String TABLE_NAME = "tlog_metadata";
-
-        static final String COL_SESSION_ID = "session_id";
-        static final String COL_TLOG_URI = "tlog_uri";
-        static final String COL_TLOG_ALL_EVENTS_COUNT = "all_events_count";
-        static final String COL_TLOG_POSITION_EVENTS_COUNT = "position_events_count";
-
-        static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
-                _ID + " INTEGER PRIMARY KEY, " +
-                COL_TLOG_URI + " TEXT NOT NULL," +
-                COL_SESSION_ID + " INTEGER, " +
-                COL_TLOG_ALL_EVENTS_COUNT + " INTEGER NOT NULL, " +
-                COL_TLOG_POSITION_EVENTS_COUNT + " INTEGER NOT NULL " +
-                ")";
-
-        static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
-
-        public final long id;
-        public final Uri tlogUri;
-        public final long sessionId;
-        public final int allEventsCount;
-        public final int positionEventsCount;
-
-        public TLogMetadata(long id, Uri tlogUri, int allEventsCount, int positionEventsCount) {
-            this(id, tlogUri, -1, allEventsCount, positionEventsCount);
-        }
-
-        public TLogMetadata(long id, Uri tlogUri, long sessionId, int allEventsCount, int positionEventsCount) {
-            this.id = id;
-            this.tlogUri = tlogUri;
-            this.sessionId = sessionId;
-            this.allEventsCount = allEventsCount;
-            this.positionEventsCount = positionEventsCount;
-        }
     }
 }
--- Android/src/org/droidplanner/android/droneshare/data/DroneShareDB.kt
+++ Android/src/org/droidplanner/android/droneshare/data/DroneShareDB.kt
@@ -6,7 +6,6 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.net.Uri
 import android.support.v4.util.Pair
-import org.droidplanner.android.droneshare.data.DroneShareContract.TLogMetadata
 import org.droidplanner.android.droneshare.data.DroneShareContract.UploadData
 import org.droidplanner.android.droneshare.data.SessionContract.SessionData
 import timber.log.Timber
@@ -88,61 +87,4 @@ class DroneShareDB(context: Context) :
         db.update(UploadData.TABLE_NAME, values, selection, selectionArgs)
     }
 
-    fun insertTLogMetadata(tlogUri: Uri, sessionId: Long?, allEventsCount: Int, positionEventsCount: Int) {
-        val db = getWritableDatabase()
-
-        val values = ContentValues().apply{
-            put(TLogMetadata.COL_TLOG_URI, tlogUri.toString())
-            put(TLogMetadata.COL_SESSION_ID, sessionId)
-            put(TLogMetadata.COL_TLOG_ALL_EVENTS_COUNT, allEventsCount)
-            put(TLogMetadata.COL_TLOG_POSITION_EVENTS_COUNT, positionEventsCount)
-        }
-
-        db.insert(TLogMetadata.TABLE_NAME, null, values)
-    }
-
-    fun getAllMetadataPerSession(): List<Pair<SessionData, TLogMetadata>> {
-        val db = getReadableDatabase()
-
-        val query = "SELECT " +
-                // session_data table columns
-                "${SessionData.COLUMN_NAME_START_TIME}, " +
-                "${SessionData.COLUMN_NAME_END_TIME}, ${SessionData.COLUMN_NAME_CONNECTION_TYPE}, " +
-                "${SessionData.COLUMN_NAME_TLOG_LOGGING_URI}, " +
-                // tlog_metadata table columns
-                "${TLogMetadata._ID}, " +
-                "${TLogMetadata.COL_TLOG_URI}, " +
-                "${TLogMetadata.COL_SESSION_ID}, " +
-                "${TLogMetadata.COL_TLOG_ALL_EVENTS_COUNT}, " +
-                "${TLogMetadata.COL_TLOG_POSITION_EVENTS_COUNT} " +
-                "FROM ${TLogMetadata.TABLE_NAME} JOIN ${SessionData.TABLE_NAME} " +
-                "ON ${TLogMetadata.COL_SESSION_ID} = ${SessionData._ID} " +
-                "ORDER BY ${SessionData.COLUMN_NAME_START_TIME} ASC";
-
-        val cursor = db.rawQuery(query, null)
-        val result = ArrayList<Pair<SessionData, TLogMetadata>>(cursor.count)
-        if(cursor.moveToFirst()){
-            do {
-                val sessionId = cursor.getLong(cursor.getColumnIndex(TLogMetadata.COL_SESSION_ID))
-                val startTime = cursor.getLong(cursor.getColumnIndex(SessionData.COLUMN_NAME_START_TIME))
-                val endTime = cursor.getLong(cursor.getColumnIndex(SessionData.COLUMN_NAME_END_TIME))
-                val connectionTypeLabel = cursor.getString(cursor.getColumnIndex(SessionData.COLUMN_NAME_CONNECTION_TYPE))
-                val tlogEncodedUri = cursor.getString(cursor.getColumnIndex(SessionData.COLUMN_NAME_TLOG_LOGGING_URI))
-                val tlogLoggingUri = Uri.parse(tlogEncodedUri)
-                val sessionData = SessionData(sessionId, startTime, endTime, connectionTypeLabel, tlogLoggingUri)
-
-                val metadataId = cursor.getLong(cursor.getColumnIndex(TLogMetadata._ID))
-                val encodedUri = cursor.getString(cursor.getColumnIndex(TLogMetadata.COL_TLOG_URI))
-                val tlogUri = Uri.parse(encodedUri)
-                val allEventsCount = cursor.getInt(cursor.getColumnIndex(TLogMetadata.COL_TLOG_ALL_EVENTS_COUNT))
-                val positionEventsCount = cursor.getInt(cursor.getColumnIndex(TLogMetadata.COL_TLOG_POSITION_EVENTS_COUNT))
-                val metadata = TLogMetadata(metadataId, tlogUri, sessionId, allEventsCount, positionEventsCount)
-
-                result.add(Pair.create(sessionData, metadata))
-            }while(cursor.moveToNext())
-        }
-
-        cursor.close()
-        return result
-    }
 }
